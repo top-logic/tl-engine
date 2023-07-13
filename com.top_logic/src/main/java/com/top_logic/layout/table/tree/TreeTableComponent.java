221f3ac03cd552bc6083c18c33499b2a4c0a1b8c
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-07-13 15:37:12 +0200

    Ticket #27448: Improved documentation of table configuration providers.

--- com.top_logic/src/main/java/com/top_logic/layout/table/tree/TreeTableComponent.java
+++ com.top_logic/src/main/java/com/top_logic/layout/table/tree/TreeTableComponent.java
@@ -145,8 +145,7 @@ public class TreeTableComponent extends BoundComponent
 		String TABLE = "table";
 
 		/**
-		 * The name of the property defining the {@link ComponentTableConfigProvider} that is used
-		 * to adapt the {@link TableConfiguration}.
+		 * @see #getComponentTableConfigProvider()
 		 */
 		String COMPONENT_TABLE_CONFIG_PROVIDER_NAME = "componentTableConfigProvider";
 
@@ -220,8 +219,8 @@ public class TreeTableComponent extends BoundComponent
 		TreeTableConfig getTable();
 
 		/**
-		 * Returns the factory to create programmatic {@link TableConfigurationProvider} for the
-		 * configured {@link TableComponent}.
+		 * {@link TableConfigurationProvider} for dynamically changing the
+		 * {@link TableConfiguration}.
 		 */
 		@InstanceFormat
 		@InstanceDefault(TableComponentTableConfigProvider.class)

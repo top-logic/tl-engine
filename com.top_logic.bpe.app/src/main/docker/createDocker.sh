310f87f66c9d160dc248703ea1964fdd0bc4e484
Marc Siebenhaar <marc.siebenhaar@top-logic.com>
2023-07-26 10:33:09 +0200

    Ticket #27474: On CWS_27474: Added Oracle setup. Added http port parameter.

--- com.top_logic.bpe.app/src/main/docker/createDocker.sh
+++ com.top_logic.bpe.app/src/main/docker/createDocker.sh
@@ -5,15 +5,18 @@ error_param(){
 echo -e "Usage:\t./createDocker.sh [OPTION]...
 Parameter:
 \t-n [AppContextName]
-\t-d [DatabaseType]\tOptions: h2, mysql, postgre, mssql
+\t-d [DatabaseType]\tOptions: h2, mysql, postgre, mssql, oracle
+
 Optional additional database parameter:
 \t-l [JDBC URL]\t\tURL format dependent on the choosen database. Please refer to the specific documentations. Overrides hostname, port and scheme.
-\t-h [ip|hostname]\tDefault: [IP of this machine], H2 default:/var/lib/tomcat9/work/
+\t-h [ip|hostname]\tDefault: [IP of this machine] - H2 default:/var/lib/tomcat9/work/
 \t-o [port]\t\tDefault: DB dependent default port
-\t-s [DB scheme]\t\tDefault: [AppContextName]
+\t-s [DB scheme]\t\tDefault: [AppContextName] - In case of Oracle this is the prefixed (/)SERVICE or (:)SID name. Example: :SIDNAME or /SERVICENAME.
 \t-u [DB username]\tDefault: user
 \t-p [DB password]\tDefault: passwd
+
 Start local parameter:
+\t-w [HTTP PORT]\t\tDefault: 8080 - The http port where the app should be accessible
 \t-r\n";
 exit 0;
 }
@@ -36,11 +39,11 @@ do
        s) DB_SCHEME="$OPTARG";;
        u) DB_USER="$OPTARG";;
        p) DB_PASSWD="$OPTARG";;
+	   w) HTTP_PORT="$OPTARG";;
        r) START_LOCAL="true";;
        *) error_param;;
    esac
 done
-[ -z "$DATABASE" ] && error_param
 
 config_h2(){
   [ -z "$DB_SCHEME" ] && DB_SCHEME=$CONTEXT
@@ -72,8 +75,8 @@ config_mssql(){
   [ -z "$DB_HOST" ] && DB_HOST=$LOCAL_IP
   [ -z "$DB_PORT" ] && DB_PORT=1433
   [ -z "$DB_URL" ] && DB_URL=";serverName=$DB_HOST;port=$DB_PORT;databaseName=$DB_SCHEME;encrypt=false;"
-# Download v12.2.0 from https://learn.microsoft.com/en-us/sql/connect/jdbc/download-microsoft-jdbc-driver-for-sql-server?view=sql-server-ver16
 # HowTo https://learn.microsoft.com/en-us/sql/connect/jdbc/using-the-jdbc-driver?view=sql-server-ver16#make-a-simple-connection-to-a-database
+# Download v12.2.0 from https://learn.microsoft.com/en-us/sql/connect/jdbc/download-microsoft-jdbc-driver-for-sql-server?view=sql-server-ver16
   cd $BUILD_PATH/
   wget https://go.microsoft.com/fwlink/?linkid=2222954 -O mssql.tar.gz
   tar -xvzf mssql.tar.gz
@@ -98,8 +101,18 @@ config_postgre(){
 }
 
 config_oracle(){
-echo "TODO"
-exit 0
+  [ -z "$DB_SCHEME" ] && DB_SCHEME=$CONTEXT
+  [ -z "$DB_USER" ] && DB_USER="user"
+  [ -z "$DB_PASSWD" ] && DB_PASSWD="passwd"
+  [ -z "$DB_HOST" ] && DB_HOST=$LOCAL_IP
+  [ -z "$DB_PORT" ] && DB_PORT=1521
+  [ -z "$DB_URL" ] && DB_URL="$DB_HOST:$DB_PORT$(sed 's/\//\\\//g' <<<$DB_SCHEME)"
+# HowTo https://docs.oracle.com/en/cloud/paas/autonomous-database/dedicated/adbbz/#articletitle
+# Download https://www.oracle.com/database/technologies/appdev/jdbc-downloads.html
+  wget https://download.oracle.com/otn-pub/otn_software/jdbc/232-DeveloperRel/ojdbc11.jar -O ojdbc11.jar
+  sed -i -e "s/{dbLibrary}/COPY --chown=root .\/ojdbc11.jar \/usr\/share\/java\//g" $BUILD_PATH/Dockerfile
+  sed -i -e 's/{dbDriver}/oracle.jdbc.driver.OracleDriver/g' $BUILD_PATH/context.xml
+  sed -i -e "s/{dbURL}/jdbc:oracle:thin:@$DB_URL/g" $BUILD_PATH/context.xml
 }
 
 LOCAL_IP=$(hostname -I | awk '{print $1}')
@@ -123,6 +136,8 @@ docker build -t $CONTEXT $BUILD_PATH/
 
 if [[ "$START_LOCAL" == "true" ]]; then
 	docker rm -f $CONTEXT || echo "Nothing to delete"
-	docker run -tdi -p 8080:8080 --restart=unless-stopped --name=$CONTEXT --hostname=$CONTEXT $CONTEXT && docker logs -f $CONTEXT
+	sleep 5
+	[ -z "$HTTP_PORT" ] && HTTP_PORT=8080
+	docker run -tdi -p $HTTP_PORT:8080 --restart=unless-stopped --name=$CONTEXT --hostname=$CONTEXT $CONTEXT && docker logs -f $CONTEXT
 fi
 

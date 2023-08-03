edb747f4f118cd670826088f165ce8adfc81836d
Daniel Busche <daniel.busche@top-logic.com>
2023-08-03 12:47:46 +0200

    Ticket #27271: Added possibility to configure commands for foreign objects.

--- com.top_logic.layout.formeditor/src/main/java/com/top_logic/layout/formeditor/parts/DeactivatedCommandModel.java
+++ com.top_logic.layout.formeditor/src/main/java/com/top_logic/layout/formeditor/parts/DeactivatedCommandModel.java
@@ -5,6 +5,10 @@
  */
 package com.top_logic.layout.formeditor.parts;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.top_logic.event.infoservice.InfoService;
 import com.top_logic.layout.DisplayContext;
 import com.top_logic.layout.basic.CommandModel;
@@ -30,6 +34,18 @@ class DeactivatedCommandModel extends CommandModelAdapter {
 	public HandlerResult executeCommand(DisplayContext context) {
 		InfoService.showInfo(I18NConstants.COMMAND_MODEL_DEACTIVATED_IN_DESIGN_MODE);
 		return HandlerResult.DEFAULT_RESULT;
+	}
+
+	/**
+	 * Creates a {@link List} of {@link CommandModel}s that shows the info that executing is not
+	 * possible in design mode.
+	 * 
+	 * @param models
+	 *        The {@link CommandModel}s to deactivate. Label and image of the returned commands base
+	 *        on these model.
+	 */
+	public static List<CommandModel> deactivateCommands(Collection<? extends CommandModel> models) {
+		return models.stream().map(DeactivatedCommandModel::new).collect(Collectors.toList());
 	}
 
 }

c2b9eae2bf9115cd06f14cc27356b84ea748d649
Daniel Busche <daniel.busche@top-logic.com>
2023-08-08 15:45:38 +0200

    Ticket #26429: Added missing labels for form elements.

---
+++ com.top_logic/src/main/java/com/top_logic/model/form/implementation/I18NConstants.java
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: 2023 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.model.form.implementation;
+
+import com.top_logic.basic.util.ResKey;
+import com.top_logic.layout.I18NConstantsBase;
+
+/**
+ * Internationalization constants for this package.
+ * 
+ * @author <a href="mailto:bhu@top-logic.com">Bernhard Haumacher</a>
+ */
+public class I18NConstants extends I18NConstantsBase {
+
+	/**
+	 * @en Form definition
+	 */
+	public static ResKey FORM_DEFINITION_LABEL;
+
+	static {
+		initConstants(I18NConstants.class);
+	}
+
+}

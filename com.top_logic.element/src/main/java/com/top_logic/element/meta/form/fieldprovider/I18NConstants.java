f07b422b9d063c31334ba100d85f3f650c4e7081
Daniel Busche <daniel.busche@top-logic.com>
2023-08-09 16:27:36 +0200

    Ticket #25853: Replaced deprecated dialog.

--- com.top_logic.element/src/main/java/com/top_logic/element/meta/form/fieldprovider/I18NConstants.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/meta/form/fieldprovider/I18NConstants.java
@@ -8,7 +8,6 @@ package com.top_logic.element.meta.form.fieldprovider;
 import com.top_logic.basic.util.ResKey;
 import com.top_logic.basic.util.ResKey1;
 import com.top_logic.layout.I18NConstantsBase;
-import com.top_logic.layout.ResPrefix;
 
 /**
  * Internationalization constants for this package.
@@ -28,7 +27,17 @@ public class I18NConstants extends I18NConstantsBase {
 
 	public static ResKey COMPOSITE_FIELD_INNER_FIELD_WARNING;
 
-	public static ResPrefix CREATE_COMPOSITION_ROW;
+	/** @en Create new element */
+	public static ResKey CREATE_COMPOSITION_ROW_DIALOG_HEADER;
+
+	/** @en New element */
+	public static ResKey CREATE_COMPOSITION_ROW_DIALOG_TITLE;
+
+	/** @en Select the type of object you want to create */
+	public static ResKey CREATE_COMPOSITION_ROW_DIALOG_MESSAGE;
+
+	/** @en Type selection */
+	public static ResKey CREATE_COMPOSITION_ROW_DIALOG_FIELD_LABEL;
 
 	static {
         initConstants(I18NConstants.class);

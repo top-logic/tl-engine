a0db6018233b17667236628096149ef548295ac4
Simon Haneke <simon.haneke@top-logic.com>
2023-08-21 15:02:36 +0200

    Ticket #27480: Initial creation of project.
    Created the project for a JMS IBM implementation.
    Implemented basic framework of a JMS Service.

---
+++ tl-service-jms/src/test/java/com/top_logic/services/jms/TestApp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Business Operation Systems GmbH. All Rights Reserved
+ */
+package com.top_logic.services.jms;
+
+import junit.framework.TestCase;
+
+/**
+ * Test case for the app <code>TL Service JMS</code>. 
+ */
+public class TestApp extends TestCase {
+	
+	public void testIt() {
+		// Add some test.
+	}
+	
+}

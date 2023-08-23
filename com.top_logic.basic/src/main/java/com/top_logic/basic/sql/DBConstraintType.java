6f8834a091198a79d6be519e93fe050a7d4932cb
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-08-23 09:30:19 +0200

    Ticket #27514: Added extraction of foreign key constraints.

--- com.top_logic.basic/src/main/java/com/top_logic/basic/sql/DBConstraintType.java
+++ com.top_logic.basic/src/main/java/com/top_logic/basic/sql/DBConstraintType.java
@@ -5,6 +5,7 @@
  */
 package com.top_logic.basic.sql;
 
+import java.sql.DatabaseMetaData;
 
 /**
  * Actions for foreign key constraint.
@@ -15,18 +16,24 @@ public enum DBConstraintType {
 	
 	/**
 	 * The action is rejected, if the result would violate the constraint.
+	 * 
+	 * @see DatabaseMetaData#importedKeyRestrict
 	 */
 	RESTRICT,
 	
 	/**
-	 * If the target is deleted, the referring row is deleted as well.
-	 * If the target is updated, the referring row is updated as well.
+	 * If the target is deleted, the referring row is deleted as well. If the target is updated, the
+	 * referring row is updated as well.
+	 * 
+	 * @see DatabaseMetaData#importedKeyCascade
 	 */
 	CASCADE,
 	
 	/**
 	 * If the target is deleted, the reference is cleared. If the target is updated, the reference
 	 * is cleared.
+	 * 
+	 * @see DatabaseMetaData#importedKeySetNull
 	 */
 	CLEAR;
 	

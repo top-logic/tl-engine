003e9b37e9927a8bb503408a24e2e9f9be22e690
Simon Haneke <simon.haneke@top-logic.com>
2023-08-23 17:11:57 +0200

    Ticket #27480: Added XML and Binary message Handling.
    It is now possible to send XML, binary or string messages.
    Improved Error handling.
    Added catching of not possible to establish connection to server and of
    not startet Service.

--- tl-service-jms/src/main/java/com/top_logic/services/jms/Consumer.java
+++ tl-service-jms/src/main/java/com/top_logic/services/jms/Consumer.java
@@ -5,15 +5,27 @@ import javax.jms.JMSException;
 
 import com.top_logic.services.jms.JMSService.TargetQueueConfig;
 
+/**
+ * Class for a jms consumer (fetches messages from a queue)
+ */
 public class Consumer extends JMSClient {
 
 	private JMSConsumer _consumer;
 
+	/**
+	 * @param config
+	 *        The config for the connection to the queue
+	 * @throws JMSException
+	 *         Exception if something is not jms conform
+	 */
 	public Consumer(TargetQueueConfig config) throws JMSException {
 		super(config);
 		_consumer = getContext().createConsumer(getDestination());
 	}
 
+	/**
+	 * Fetches a message from the given queue
+	 */
 	public void receive() {
 		String message = _consumer.receiveBody(String.class, 15000);
 		System.out.println("\nReceived message:\n" + message);

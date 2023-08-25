830172651d1369a86cf7c6069f55209f672db2e4
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-08-25 08:59:58 +0200

    Ticket #27514: Added annotations for specifying a DB binding.
    * Add corresponding annotations while translating the DB schema to a
    model.

---
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/ddlimporter/api/ExportRow.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2023 Business Operation Systems GmbH. All Rights Reserved.
+ */
+package com.top_logic.element.model.ddlimporter.api;
+
+/**
+ * Write interface to a table row being exported.
+ *
+ * @author <a href="mailto:bhu@top-logic.com">Bernhard Haumacher</a>
+ * 
+ * @see ImportRow
+ */
+public interface ExportRow {
+
+	/**
+	 * Updates the value of the column with the given name in the row currently being exported.
+	 *
+	 * @param column
+	 *        Name of the column to write.
+	 * @param value
+	 *        Value to be written to the database.
+	 */
+	void setValue(String column, Object value);
+
+}

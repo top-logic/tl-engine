374efe630cf541d65910caa6ec26cab279cdc664
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-08-25 12:41:15 +0200

    Ticket #27514: Added reasonable property order.

--- com.top_logic.element/src/main/java/com/top_logic/element/model/jdbcBinding/api/annotate/TLColumnBinding.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/jdbcBinding/api/annotate/TLColumnBinding.java
@@ -5,7 +5,9 @@ package com.top_logic.element.model.jdbcBinding.api.annotate;
 
 import com.top_logic.basic.config.PolymorphicConfiguration;
 import com.top_logic.basic.config.annotation.Mandatory;
+import com.top_logic.basic.config.annotation.Name;
 import com.top_logic.basic.config.annotation.TagName;
+import com.top_logic.basic.config.order.DisplayOrder;
 import com.top_logic.element.model.jdbcBinding.api.ColumnFormat;
 import com.top_logic.element.model.jdbcBinding.api.ColumnParser;
 import com.top_logic.model.TLProperty;
@@ -23,11 +25,32 @@ import com.top_logic.model.annotate.TLAttributeAnnotation;
  * @author <a href="mailto:bhu@top-logic.com">Bernhard Haumacher</a>
  */
 @TagName("column-binding")
+@DisplayOrder({
+	TLColumnBinding.NAME,
+	TLColumnBinding.PARSER,
+	TLColumnBinding.FORMAT,
+})
 public interface TLColumnBinding extends TLAttributeAnnotation {
+
+	/**
+	 * @see #getName()
+	 */
+	String NAME = "name";
+
+	/**
+	 * @see #getParser()
+	 */
+	String PARSER = "parser";
+
+	/**
+	 * @see #getFormat()
+	 */
+	String FORMAT = "format";
 
 	/**
 	 * The name of the column.
 	 */
+	@Name(NAME)
 	@Mandatory
 	String getName();
 
@@ -40,12 +63,14 @@ public interface TLColumnBinding extends TLAttributeAnnotation {
 	 * An optional value conversion that maps the column value to an application value during
 	 * import.
 	 */
+	@Name(PARSER)
 	PolymorphicConfiguration<? extends ColumnParser> getParser();
 
 	/**
 	 * An optional value conversion that maps the property's application value to a column value
 	 * during export.
 	 */
+	@Name(FORMAT)
 	PolymorphicConfiguration<? extends ColumnFormat> getFormat();
 
 }

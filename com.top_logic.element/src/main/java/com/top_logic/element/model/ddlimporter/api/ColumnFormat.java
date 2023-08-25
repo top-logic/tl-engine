2fdf64013783ab69d1b5ef13d923e55b45227c25
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-08-25 08:59:58 +0200

    Ticket #27514: Added annotations for specifying a DB binding.
    * Add corresponding annotations while translating the DB schema to a
    model.

---
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/ddlimporter/api/ColumnFormat.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023 Business Operation Systems GmbH. All Rights Reserved.
+ */
+package com.top_logic.element.model.ddlimporter.api;
+
+import com.top_logic.model.TLObject;
+import com.top_logic.model.TLProperty;
+
+/**
+ * Callback that formats an application value to be stored in a DB column.
+ * 
+ * @see ColumnParser
+ *
+ * @author <a href="mailto:bhu@top-logic.com">Bernhard Haumacher</a>
+ */
+public interface ColumnFormat {
+
+	/**
+	 * Converts the given value retrieved from a {@link TLProperty} to be sent to a JDBC connection.
+	 *
+	 * @param applicationValue
+	 *        A property value of a {@link TLObject}.
+	 * @return The corresponding value to be exported to a database.
+	 */
+	Object getColumnValue(Object applicationValue);
+
+}
---
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/ddlimporter/api/annotate/TLTableBinding.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023 Business Operation Systems GmbH. All Rights Reserved.
+ */
+package com.top_logic.element.model.ddlimporter.api.annotate;
+
+import java.util.List;
+
+import com.top_logic.basic.config.CommaSeparatedStrings;
+import com.top_logic.basic.config.PolymorphicConfiguration;
+import com.top_logic.basic.config.annotation.Format;
+import com.top_logic.basic.config.annotation.Mandatory;
+import com.top_logic.basic.config.annotation.TagName;
+import com.top_logic.element.model.ddlimporter.api.RowReader;
+import com.top_logic.element.model.ddlimporter.api.RowWriter;
+import com.top_logic.element.model.ddlimporter.api.TypeSelector;
+import com.top_logic.model.config.TLTypeAnnotation;
+
+/**
+ * {@link TLTypeAnnotation} specifying the table to which the annotated type is bound.
+ * 
+ * <p>
+ * During import, objects of the annotated type are created from rows of the specified table. During
+ * export, objects of the annotated type are written to rows of the specified table.
+ * </p>
+ *
+ * @author <a href="mailto:bhu@top-logic.com">Bernhard Haumacher</a>
+ */
+@TagName("table-binding")
+public interface TLTableBinding extends TLTypeAnnotation {
+
+	/**
+	 * The name of the table.
+	 */
+	@Mandatory
+	String getName();
+
+	/**
+	 * @see #getName()
+	 */
+	void setName(String value);
+
+	/**
+	 * Specifies the primary key of the table that is used to reference objects through
+	 * {@link TLForeignKeyBinding foreign keys}.
+	 */
+	@Format(CommaSeparatedStrings.class)
+	List<String> getPrimaryKey();
+
+	/**
+	 * Optional type lookup for a polymorphic binding.
+	 * 
+	 * <p>
+	 * The type selector function computes a sub-type of the annotated type that should be used when
+	 * importing an object from a certain row.
+	 * </p>
+	 */
+	PolymorphicConfiguration<? extends TypeSelector> getTypeSelector();
+
+	/**
+	 * Optional post-processing of an object after read from a table row.
+	 */
+	PolymorphicConfiguration<? extends RowReader> getRowReader();
+
+	/**
+	 * Optional post-processing of a table row after exporting an object.
+	 */
+	PolymorphicConfiguration<? extends RowWriter> getRowWriter();
+
+}

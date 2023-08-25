955459901ea2d9051ca1244df97154018a92b72e
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-08-25 08:59:58 +0200

    Ticket #27514: Added annotations for specifying a DB binding.
    * Add corresponding annotations while translating the DB schema to a
    model.

---
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/ddlimporter/api/annotate/TLForeignKeyBinding.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023 Business Operation Systems GmbH. All Rights Reserved.
+ */
+package com.top_logic.element.model.ddlimporter.api.annotate;
+
+import java.util.List;
+
+import com.top_logic.basic.config.CommaSeparatedStrings;
+import com.top_logic.basic.config.annotation.Format;
+import com.top_logic.basic.config.annotation.Mandatory;
+import com.top_logic.basic.config.annotation.TagName;
+import com.top_logic.model.TLReference;
+import com.top_logic.model.annotate.TLAttributeAnnotation;
+
+/**
+ * {@link TLAttributeAnnotation} for a {@link TLReference} that identifies the target objects by the
+ * values in the given columns.
+ * 
+ * <p>
+ * The target object is found by comparing the values in the given columns with the values of the
+ * {@link TLTableBinding#getPrimaryKey() primary key} properties of the target table.
+ * </p>
+ *
+ * @author <a href="mailto:bhu@top-logic.com">Bernhard Haumacher</a>
+ */
+@TagName("foreign-key-binding")
+public interface TLForeignKeyBinding extends TLAttributeAnnotation {
+
+	/**
+	 * The columns whose values are use to identify the target object of the annotated
+	 * {@link TLReference}.
+	 */
+	@Mandatory
+	@Format(CommaSeparatedStrings.class)
+	List<String> getColumns();
+
+	/**
+	 * @see #getColumns()
+	 */
+	void setColumns(List<String> value);
+
+}

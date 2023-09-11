ee21c4382f3245dc480ece8d907898c7c1965696
Sven Förster <sven.foerster@top-logic.com>
2023-09-11 16:05:11 +0200

    Ticket #27195: Fixed copyright header.

--- com.top_logic/src/main/java/com/top_logic/model/annotate/TLModuleDisplayGroup.java
+++ com.top_logic/src/main/java/com/top_logic/model/annotate/TLModuleDisplayGroup.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2023 Business Operation Systems GmbH. All Rights Reserved
+ * SPDX-FileCopyrightText: 2023 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
  */
 package com.top_logic.model.annotate;
 

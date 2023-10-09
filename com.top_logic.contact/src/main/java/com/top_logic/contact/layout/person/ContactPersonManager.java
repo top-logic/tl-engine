f0b62bd5e384081a4b558cb2e3c1655aba86ab62
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-10-09 17:04:20 +0200

    Ticket #27517: Create correponding contacts when creating accounts.

---
+++ com.top_logic.contact/src/main/java/com/top_logic/contact/layout/person/ContactPersonManager.java
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2023 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.contact.layout.person;
+
+import com.top_logic.basic.config.InstantiationContext;
+import com.top_logic.contact.business.ContactFactory;
+import com.top_logic.contact.business.PersonContact;
+import com.top_logic.knowledge.wrap.person.Person;
+import com.top_logic.knowledge.wrap.person.PersonManager;
+
+/**
+ * {@link PersonManager} that initializes new accounts with contact information.
+ */
+public class ContactPersonManager extends PersonManager {
+
+	/**
+	 * Creates a {@link ContactPersonManager}.
+	 */
+	public ContactPersonManager(InstantiationContext context, Config config) {
+		super(context, config);
+	}
+
+	@Override
+	public void initUser(Person account) {
+		String loginName = account.getName();
+		PersonContact user = ContactFactory.getInstance().createNewPersonContact(loginName, loginName);
+		account.setUser(user);
+	}
+
+}

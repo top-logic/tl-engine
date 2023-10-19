2dd4fd6324d6b8d8feb647b918c8def1543b205e
Simon Haneke <simon.haneke@top-logic.com>
2023-10-19 08:24:23 +0200

    Ticket #27480: Commit before restructuring.
    Added support for ActiveMQ. Changed structure to add different MQ
    Systems as plugins.

---
+++ tl-service-jms-activemq/src/test/java/com/top_logic/services/jms/activemq/TestApp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Simon Haneke. All Rights Reserved
+ */
+package com.top_logic.services.jms.activemq;
+
+import junit.framework.TestCase;
+
+/**
+ * Test case for the app <code>Tl Service Jms Activemq</code>. 
+ */
+public class TestApp extends TestCase {
+	
+	public void testIt() {
+		// Add some test.
+	}
+	
+}

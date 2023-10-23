96cee8aaf0e0e758efe43f8f4d6c0a807c03fabf
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-10-23 12:47:38 +0200

    Ticket #27610: Added PDF renderer for ResKey (I18N string fields).
    The renderer exports only the value in the users locale. The HTML
    structure currently generated for showing the value in all locales is
    not suitable for PDF rendering.

---
+++ com.top_logic.element/src/main/java/com/top_logic/element/i18n/I18NPDFRenderer.java
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: 2023 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.element.i18n;
+
+import java.io.IOException;
+
+import com.top_logic.basic.util.ResKey;
+import com.top_logic.basic.xml.TagWriter;
+import com.top_logic.layout.DisplayContext;
+import com.top_logic.tool.export.pdf.PDFRenderer;
+
+/**
+ * {@link PDFRenderer} for {@link ResKey}s.
+ */
+public class I18NPDFRenderer implements PDFRenderer {
+
+	@Override
+	public void write(DisplayContext context, TagWriter out, Object contextObject, Object value) throws IOException {
+		if (value != null) {
+			out.writeText(context.getResources().getString((ResKey) value));
+		}
+	}
+
+}

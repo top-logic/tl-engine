bdd3a37447a98d810c0df9cca3ee92519f85110c
Simon Haneke <simon.haneke@top-logic.com>
2023-10-23 16:47:24 +0200

    Ticket #27480: Reworked structure of the JMSService.

--- tl-service-jms/src/main/java/com/top_logic/services/jms/JMSService.java
+++ tl-service-jms/src/main/java/com/top_logic/services/jms/JMSService.java
@@ -6,24 +6,24 @@ package com.top_logic.services.jms;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.naming.NamingException;
+
 import com.top_logic.basic.Logger;
 import com.top_logic.basic.col.TupleFactory.Pair;
 import com.top_logic.basic.config.InstantiationContext;
 import com.top_logic.basic.config.NamedConfigMandatory;
 import com.top_logic.basic.config.PolymorphicConfiguration;
-import com.top_logic.basic.config.annotation.Encrypted;
 import com.top_logic.basic.config.annotation.Key;
 import com.top_logic.basic.config.annotation.Mandatory;
 import com.top_logic.basic.config.annotation.Name;
 import com.top_logic.basic.config.annotation.defaults.ImplementationClassDefault;
+import com.top_logic.basic.config.misc.TypedConfigUtil;
 import com.top_logic.basic.config.order.DisplayOrder;
 import com.top_logic.basic.module.ConfiguredManagedClass;
 import com.top_logic.basic.module.TypedRuntimeModule;
 import com.top_logic.event.infoservice.InfoService;
 
-import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSException;
-import jakarta.jms.Message;
 
 /**
  * The TopLogic Service to set the config for a connection and establish this connection to a JMS
@@ -52,109 +52,30 @@ public class JMSService extends ConfiguredManagedClass<JMSService.Config> {
 	 * connection.
 	 */
 	@DisplayOrder({ DestinationConfig.NAME_ATTRIBUTE,
-		DestinationConfig.DEST_NAME,
-		DestinationConfig.TYPE,
-		DestinationConfig.USER,
-		DestinationConfig.PASSWORD,
-		DestinationConfig.MQ_SYSTEM_CONFIGURATOR,
-		DestinationConfig.MESSAGE_PROCESSOR })
+		DestinationConfig.MQ_SYSTEM_CLIENT })
 	public interface DestinationConfig extends NamedConfigMandatory {
-
 		/**
-		 * Configuration name for {@link #getUser()}.
+		 * Configuration name for {@link #getMQSystemClient()}
 		 */
-		String USER = "user";
-
-		/**
-		 * Configuration name for {@link #getPassword()}.
-		 */
-		String PASSWORD = "password";
-
-		/**
-		 * Configuration name for {@link #getDestName()}.
-		 */
-		String DEST_NAME = "dest-name";
-
-		/**
-		 * Configuration name for {@link #getType()}
-		 */
-		String TYPE = "type";
-
-		/**
-		 * Configuration name for {@link #getMQSystemConfigurator()}
-		 */
-		String MQ_SYSTEM_CONFIGURATOR = "mq-system-configurator";
-
-		/**
-		 * Configuration name for {@link #getProcessor()}
-		 */
-		String MESSAGE_PROCESSOR = "message-processor";
-
-		/**
-		 * The user name to log in to the message queue server.
-		 */
-		@Name(USER)
-		String getUser();
-
-		/**
-		 * The password to the given user name.
-		 */
-		@Encrypted
-		@Name(PASSWORD)
-		String getPassword();
-
-		/**
-		 * The name that is the destination of the connection.
-		 */
-		@Mandatory
-		@Name(DEST_NAME)
-		String getDestName();
-
-		/**
-		 * The type the destination of the connection has.
-		 */
-		@Name(TYPE)
-		Type getType();
+		String MQ_SYSTEM_CLIENT = "mq-system-client";
 
 		/**
 		 * The configurator for the Message Queue System that is being used.
 		 */
 		@Mandatory
-		@Name(MQ_SYSTEM_CONFIGURATOR)
-		@ImplementationClassDefault(JNDIMQConfigurator.class)
-		PolymorphicConfiguration<MQSystemConfigurator> getMQSystemConfigurator();
-
-		/**
-		 * The config for a processor that processes messages.
-		 */
-		@Name(MESSAGE_PROCESSOR)
-		PolymorphicConfiguration<MessageProcessor> getProcessor();
-	}
-
-	/**
-	 * The type of the destination, that can be a queue or a topic.
-	 */
-	public enum Type {
-		/**
-		 * A queue is a point-to-point connection between a producer and a consumer.
-		 */
-		QUEUE,
-
-		/**
-		 * A topic is a publish/subscribe connection so multiple subscribed consumers can receive
-		 * messages simultaneously from a topic.
-		 */
-		TOPIC;
+		@Name(MQ_SYSTEM_CLIENT)
+		@ImplementationClassDefault(JNDIMQClient.class)
+		PolymorphicConfiguration<MQSystemClient> getMQSystemClient();
 	}
 
 	/**
 	 * The configuration for the System behind the Message Queue that runs the Queue Manager.
 	 */
-	public interface MQSystemConfigurator {
+	public interface MQSystemClient {
 		/**
 		 * 
 		 */
-		public ConnectionFactory setupMQConnection(String un, String pw) throws JMSException;
+		public void setupMQConnection() throws JMSException, NamingException;
 
 		/**
 		 * 
@@ -162,23 +83,20 @@ public class JMSService extends ConfiguredManagedClass<JMSService.Config> {
 		default String getCharsetProperty() {
 			return "charset";
 		}
-	}
 
-	/**
-	 * A {@link MessageProcessor} provides a method to process messages that where received by a
-	 * {@link Consumer}.
-	 * 
-	 * Implementations of {@link MessageProcessor} typically contain a config to set a custom
-	 * processor for received messages.
-	 */
-	public interface MessageProcessor {
 		/**
-		 * Processes the given {@link Message}.
+		 * A map of all created producer configurations for the MQ System.
 		 * 
-		 * @param message
-		 *        The {@link Message} received by the {@link Consumer}.
+		 * @return a map of String name to {@link Producer.Config}
 		 */
-		public void processMessage(Message message);
+		public Map<String, Producer.Config<?>> getProducerConfigs();
+
+		/**
+		 * A map of all created consumer configurations for the MQ System.
+		 * 
+		 * @return a map of String name to {@link Consumer.Config}
+		 */
+		public Map<String, Consumer.Config<?>> getConsumerConfigs();
 	}
 
 	private Map<String, Producer> _producers = new HashMap<>();
@@ -201,18 +119,27 @@ public class JMSService extends ConfiguredManagedClass<JMSService.Config> {
 	protected void startUp() {
 		super.startUp();
 		for (DestinationConfig config : getConfig().getDestinationConfigs().values()) {
+			JMSClient mqClient = (JMSClient) TypedConfigUtil.createInstance(config.getMQSystemClient());
 			try {
-				Producer prod = new Producer(config);
-				_producers.put(config.getName(), prod);
-				if (config.getProcessor() != null) {
-					Consumer cons = new Consumer(config);
+				mqClient.setupMQConnection();
 
+				for (Producer.Config<?> pconfig : mqClient.getProducerConfigs().values()) {
+					Producer prod = TypedConfigUtil.createInstance(pconfig);
+					prod.setup(mqClient);
+					_producers.put(pconfig.getName(), prod);
+				}
+				for (Consumer.Config<?> cconfig : mqClient.getConsumerConfigs().values()) {
+					Consumer cons = TypedConfigUtil.createInstance(cconfig);
+					cons.setup(mqClient);
 					Thread consThread = new Thread(() -> cons.receive());
-					consThread.setName("JMS Consumer - " + config.getName());
+					consThread.setName("JMS Consumer - " + cconfig.getName());
 					consThread.start();
-					_consumers.put(config.getName(), new Pair<>(cons, consThread));
+					_consumers.put(cconfig.getName(), new Pair<>(cons, consThread));
 				}
 			} catch (JMSException ex) {
+				InfoService.logError(I18NConstants.ERROR_ESTABLISH_CONNECTION__NAME.fill(config.getName()),
+					ex.getMessage(), ex, JMSService.class);
+			} catch (NamingException ex) {
 				InfoService.logError(I18NConstants.ERROR_ESTABLISH_CONNECTION__NAME.fill(config.getName()),
 					ex.getMessage(), ex, JMSService.class);
 			} catch (RuntimeException ex) {
@@ -224,15 +151,17 @@ public class JMSService extends ConfiguredManagedClass<JMSService.Config> {
 
 	@Override
 	protected void shutDown() {
-		for (Producer prod : _producers.values()) {
-			prod.close();
-		}
+		_producers.clear();
+		_producers = null;
 		for (Pair<Consumer, Thread> pair : _consumers.values()) {
 			Consumer cons = pair.getFirst();
 			Thread thread = pair.getSecond();
 			thread.interrupt();
 			Logger.info("Interrupted consumer thread " + thread.getName() + ".", JMSService.class);
+			cons.close();
 		}
+		_consumers.clear();
+		_consumers = null;
 		super.shutDown();
 	}
 

3798def18e9e80db92b22e62ed4ba38d39c72371
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-10-26 18:33:44 +0200

    Ticket #27637: Enhanced documentation of model annotations.
    Those annotations now show tooltips for their enumeration options.

--- com.top_logic/src/main/java/com/top_logic/model/config/annotation/MainProperties.java
+++ com.top_logic/src/main/java/com/top_logic/model/config/annotation/MainProperties.java
@@ -10,6 +10,7 @@ import java.util.List;
 import com.top_logic.basic.annotation.InApp;
 import com.top_logic.basic.config.CommaSeparatedStrings;
 import com.top_logic.basic.config.annotation.Format;
+import com.top_logic.basic.config.annotation.Label;
 import com.top_logic.basic.config.annotation.Name;
 import com.top_logic.basic.config.annotation.TagName;
 import com.top_logic.basic.util.InAppClassifierConstants;
@@ -26,10 +27,11 @@ import com.top_logic.model.annotate.TargetType;
 import com.top_logic.model.config.TLTypeAnnotation;
 
 /**
- * {@link TLAnnotation} for defining the "main properties" of a {@link TLClass}.
+ * {@link TLAnnotation} for defining the default columns of a {@link TLClass} when instances are
+ * displayed in a table.
  * 
  * <p>
- * Alternatively to defining the main properties as type annotation, the can be given in the
+ * Alternatively to defining the default columns as type annotation, they can be configured in the
  * application configuration for a certain module, see
  * <code>com.top_logic.element.model.DynamicModelService.Config.ModuleSetting#getMainProperties()</code>.
  * </p>
@@ -38,6 +40,7 @@ import com.top_logic.model.config.TLTypeAnnotation;
  * 
  * @author <a href="mailto:jst@top-logic.com">Jan Stolzenburg</a>
  */
+@Label("Default columns")
 @TagName("main-properties")
 @TargetType(value = { TLTypeKind.REF, TLTypeKind.COMPOSITION })
 @InApp(classifiers = { InAppClassifierConstants.FORM_RELEVANT })
@@ -47,10 +50,8 @@ public interface MainProperties extends TLAttributeAnnotation, TLTypeAnnotation 
 	String PROPERTIES = "properties";
 
 	/**
-	 * The "main properties" of the type.
-	 * <p>
-	 * They are used as default columns of tables, in the order given here.
-	 * </p>
+	 * The attributes of the annotated type that should be displayed as columns when instances of
+	 * the annotated type are presented as table.
 	 */
 	@Name(PROPERTIES)
 	@Options(fun = PartNamesOptionProvider.class, mapping = ColumnOptionMapping.class)

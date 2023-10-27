03d920d72a84c0969052b38d5f2e1de73ad9c0a8
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-10-27 11:16:44 +0200

    Ticket #27477: Added TL-Script function for creating transient objects.

--- com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/CreateTransientObject.java
+++ com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/CreateTransientObject.java
@@ -9,45 +9,45 @@ import com.top_logic.basic.config.ConfigurationException;
 import com.top_logic.basic.config.InstantiationContext;
 import com.top_logic.model.TLClass;
 import com.top_logic.model.TLObject;
+import com.top_logic.model.impl.TransientObjectFactory;
 import com.top_logic.model.search.expr.config.dom.Expr;
 import com.top_logic.model.search.expr.config.operations.AbstractSimpleMethodBuilder;
-import com.top_logic.util.model.ModelService;
 
 /**
- * {@link SearchExpression} creating a new object of a given {@link TLClass} type.
+ * {@link SearchExpression} creating a new transient object of a given {@link TLClass} type.
  *
  * @author <a href="mailto:bhu@top-logic.com">Bernhard Haumacher</a>
  */
-public class CreateObject extends AbstractObjectCreation {
+public class CreateTransientObject extends AbstractObjectCreation {
 
 	/**
-	 * Creates a {@link CreateObject}.
+	 * Creates a {@link CreateTransientObject}.
 	 *
 	 * @param self
 	 *        The expression evaluating to the type to instantiate (usually a model type literal).
 	 * @param args
 	 *        The optional create context (at most a single argument).
 	 */
-	CreateObject(String name, SearchExpression self, SearchExpression[] args) {
+	CreateTransientObject(String name, SearchExpression self, SearchExpression[] args) {
 		super(name, self, args);
 	}
 
 	@Override
 	public GenericMethod copy(SearchExpression self, SearchExpression[] arguments) {
-		return new CreateObject(getName(), self, arguments);
+		return new CreateTransientObject(getName(), self, arguments);
 	}
 
 	@Override
 	protected Object eval(Object self, Object[] arguments, EvalContext definitions) {
 		TLClass type = (TLClass) asStructuredTypeNonNull(self, getSelf());
 		TLObject context = asTLObject(arguments.length > 0 ? arguments[0] : null);
-		return ModelService.getInstance().getFactory().createObject(type, context, null);
+		return TransientObjectFactory.INSTANCE.createObject(type, context);
 	}
 
 	/**
-	 * Builder creating a {@link CreateObject} expression.
+	 * Builder creating a {@link CreateTransientObject} expression.
 	 */
-	public static class Builder extends AbstractSimpleMethodBuilder<CreateObject> {
+	public static class Builder extends AbstractSimpleMethodBuilder<CreateTransientObject> {
 		/**
 		 * Creates a {@link Builder}.
 		 */
@@ -56,10 +56,10 @@ public class CreateObject extends AbstractObjectCreation {
 		}
 
 		@Override
-		public CreateObject build(Expr expr, SearchExpression self, SearchExpression[] args)
+		public CreateTransientObject build(Expr expr, SearchExpression self, SearchExpression[] args)
 				throws ConfigurationException {
 			checkMaxArgs(expr, args, 1);
-			return new CreateObject(getName(), self, args);
+			return new CreateTransientObject(getName(), self, args);
 		}
 	}
 

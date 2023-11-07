580a706c7a39c119e22e3428060ba9791cee4c4b
Daniel Busche <daniel.busche@top-logic.com>
2023-11-07 12:27:40 +0100

    Ticket #25644: Removed content already definied in com.top_logic.basic.util.ResKeyUtil.

--- com.top_logic/src/main/java/com/top_logic/util/ResKeyUtil.java
+++ com.top_logic/src/main/java/com/top_logic/util/ResKeyUtil.java
@@ -5,23 +5,16 @@
  */
 package com.top_logic.util;
 
-import static com.top_logic.basic.shared.collection.factory.CollectionFactoryShared.*;
-import static java.util.Objects.*;
-
 import java.lang.annotation.Annotation;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Map.Entry;
 
-import com.top_logic.basic.CollectionUtil;
 import com.top_logic.basic.config.TypedConfiguration;
 import com.top_logic.basic.config.customization.CustomizationContainer.TypeCustomizationConfig;
 import com.top_logic.basic.config.misc.TypedConfigUtil;
 import com.top_logic.basic.util.ResKey;
 import com.top_logic.basic.util.ResKey.Builder;
-import com.top_logic.basic.util.ResourcesModule;
 import com.top_logic.layout.form.values.edit.annotation.ItemDisplay;
 import com.top_logic.layout.form.values.edit.annotation.PropertyEditor;
 import com.top_logic.layout.form.values.edit.editor.InternationalizationEditor;
@@ -32,7 +25,7 @@ import com.top_logic.model.TLObject;
  * 
  * @author <a href="mailto:jst@top-logic.com">Jan Stolzenburg</a>
  */
-public class ResKeyUtil {
+public class ResKeyUtil extends com.top_logic.basic.util.ResKeyUtil {
 
 	/**
 	 * Updates the {@link ResKey} attribute with the given name.
@@ -51,82 +44,6 @@ public class ResKeyUtil {
 		ResKey oldResKey = (ResKey) tlObject.tValueByName(attribute);
 		ResKey newResKey = updateTranslation(oldResKey, locale, newTranlation);
 		tlObject.tUpdateByName(attribute, newResKey);
-	}
-
-	/**
-	 * Creates a new {@link ResKey} where the translation for the given locale is replaced.
-	 * 
-	 * @param resKey
-	 *        Null is treated as a {@link ResKey} without translations.
-	 * @param locale
-	 *        Is not allowed to be null.
-	 * @param newTranslation
-	 *        Is allowed to be null and empty: The value is passed to
-	 *        {@link Builder#add(Locale, String)} without changes.
-	 * @return Never null.
-	 */
-	public static ResKey updateTranslation(ResKey resKey, Locale locale, String newTranslation) {
-		requireNonNull(locale);
-		Map<Locale, String> translations = toMap(resKey);
-		translations.put(locale, newTranslation);
-		return fromMap(translations);
-	}
-
-	/**
-	 * Creates a map with the translations of this {@link ResKey}.
-	 * <p>
-	 * The locales are taken from {@link ResourcesModule#getSupportedLocales()}.
-	 * </p>
-	 * <p>
-	 * If there is no translation for a {@link Locale}, null is stored as value.
-	 * </p>
-	 * 
-	 * @param key
-	 *        If null, an empty {@link Map} is returned.
-	 * @return Never null. A new, mutable and resizable {@link Map}.
-	 */
-	public static Map<Locale, String> toMap(ResKey key) {
-		if (key == null) {
-			return map();
-		}
-		Map<Locale, String> map = map();
-		List<Locale> locales = ResourcesModule.getInstance().getSupportedLocales();
-		for (Locale locale : locales) {
-			/* Don't use the fallback translation: That would make it impossible for callers to
-			 * detect whether a translation is missing. */
-			String translation = translateWithoutFallback(locale, key);
-			map.put(locale, translation);
-		}
-		return map;
-	}
-
-	/**
-	 * Creates a new {@link ResKey} with the given translations.
-	 * 
-	 * @param translations
-	 *        None of the locales must be null. Translations are allowed to be null and empty: These
-	 *        values are passed to {@link Builder#add(Locale, String)} without changes.
-	 * @return Never null.
-	 */
-	public static ResKey fromMap(Map<Locale, String> translations) {
-		ResKey.Builder builder = ResKey.builder();
-		for (Entry<Locale, String> entry : CollectionUtil.nonNull(translations.entrySet())) {
-			Locale locale = requireNonNull(entry.getKey());
-			String translation = entry.getValue();
-			builder.add(locale, translation);
-		}
-		return requireNonNull(builder.build());
-	}
-
-	/**
-	 * @param locale
-	 *        If null, null is returned.
-	 * @param key
-	 *        If null, the empty {@link String} is returned.
-	 * @return Null, if the locale is null.
-	 */
-	private static String translateWithoutFallback(Locale locale, ResKey key) {
-		return Resources.getInstance(locale).getString(key, null);
 	}
 
 	/**

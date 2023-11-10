8b8ea9462476f92744dc321f9f8c05e14f2496ee
Daniel Busche <daniel.busche@top-logic.com>
2023-11-10 16:45:45 +0100

    Ticket #27656: Generalized return type of Control to HTMLFragment.

--- com.top_logic/src/main/java/com/top_logic/layout/form/values/Forms.java
+++ com.top_logic/src/main/java/com/top_logic/layout/form/values/Forms.java
@@ -7,6 +7,7 @@ package com.top_logic.layout.form.values;
 
 import java.io.IOException;
 
+import com.top_logic.base.services.simpleajax.HTMLFragment;
 import com.top_logic.basic.util.ResKey;
 import com.top_logic.basic.xml.TagWriter;
 import com.top_logic.layout.Control;
@@ -35,53 +36,37 @@ public class Forms {
 
 	private static final String CSS_FRM_VALUE = "frmValue";
 
-	public static Control keyValue(FormField field) {
+	public static HTMLFragment keyValue(FormField field) {
 		return keyValues(field,
 			Fragments.label(field),
 			Fragments.value(field),
 			Fragments.error(field));
 	}
 
-	public static Control keyValues(FormMember field, Control label, Control... values) {
+	public static HTMLFragment keyValues(FormMember field, HTMLFragment label, HTMLFragment... values) {
 		return onvisible(field,
 			keyValueContainer(
 				label,
 				valueContainer(values)));
 	}
 
-	public static Control subGroup(Control... contents) {
+	public static HTMLFragment subGroup(HTMLFragment... contents) {
 		return span(CSS_FRM_SUB_GROUP, contents);
 	}
 
-	public static Control keyValueContainer(Control... contents) {
+	public static HTMLFragment keyValueContainer(HTMLFragment... contents) {
 		return span(CSS_FRM_KEY_VALUE, contents);
 	}
 
-	public static Control valueContainer(Control... contents) {
+	public static HTMLFragment valueContainer(HTMLFragment... contents) {
 		return span(CSS_FRM_VALUE, contents);
 	}
 
-	/**
-	 * @deprecated Use {@link Fragments#value(FormMember)}
-	 */
-	@Deprecated
-	public static Control value(FormMember field) {
-		return Fragments.value(field);
-	}
-
-	/**
-	 * @deprecated Use {@link Fragments#error(FormMember)}
-	 */
-	@Deprecated
-	public static Control error(FormField field) {
-		return Fragments.error(field);
-	}
-
-	public static OnVisibleControl div(FormMember model, String cssClass, Control... contents) {
+	public static OnVisibleControl div(FormMember model, String cssClass, HTMLFragment... contents) {
 		return onvisible(model, div(cssClass, contents));
 	}
 
-	public static OnVisibleControl span(FormMember model, String cssClass, Control... contents) {
+	public static OnVisibleControl span(FormMember model, String cssClass, HTMLFragment... contents) {
 		return onvisible(model, span(cssClass, contents));
 	}
 
@@ -94,46 +79,38 @@ public class Forms {
 		};
 	}
 
-	public static OnVisibleControl onvisible(FormMember model, Control... contents) {
+	public static OnVisibleControl onvisible(FormMember model, HTMLFragment... contents) {
 		OnVisibleControl result = new OnVisibleControl(model);
 		contents(result, contents);
 		return result;
 	}
 
-	public static BlockControl span(String cssClass, Control... contents) {
+	public static BlockControl span(String cssClass, HTMLFragment... contents) {
 		BlockControl result = block(contents);
 		result.setRenderer(DefaultSimpleCompositeControlRenderer.spanWithCSSClass(cssClass));
 		return result;
 	}
 
-	public static BlockControl floatingGroup(Control... contents) {
+	public static BlockControl floatingGroup(HTMLFragment... contents) {
 		return div(CSS_FRM_GROUP, contents);
 	}
 
-	public static BlockControl div(String cssClass, Control... contents) {
+	public static BlockControl div(String cssClass, HTMLFragment... contents) {
 		BlockControl result = block(contents);
 		result.setRenderer(DefaultSimpleCompositeControlRenderer.divWithCSSClass(cssClass));
 		return result;
 	}
 
-	public static BlockControl block(Control... contents) {
+	public static BlockControl block(HTMLFragment... contents) {
 		BlockControl result = new BlockControl();
 		contents(result, contents);
 		return result;
 	}
 
-	private static void contents(AbstractCompositeControl result, Control... contents) {
-		for (Control control : contents) {
-			result.addChild(control);
+	private static void contents(AbstractCompositeControl result, HTMLFragment... contents) {
+		for (HTMLFragment content : contents) {
+			result.addChild(content);
 		}
-	}
-
-	/**
-	 * @deprecated Use {@link Fragments#label(FormMember)}
-	 */
-	@Deprecated
-	public static Control label(FormMember member) {
-		return Fragments.label(member);
 	}
 
 }

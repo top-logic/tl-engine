ed66aeeec81b5f27b61f0f893c5f8d620e1a8a78
Jan Stolzenburg <jan.stolzenburg@top-logic.com>
2023-11-13 15:04:35 +0100

    Ticket #27604: Generalize the AbstractGridCreateHandler. It should be possible to specify the algorithm for calculating the potential types of the new object. Use the existing interface CreateTypeOptions for that. This change makes the AbstractGridCreateHandler better suited for trees where it is registered as the single handler for all levels of the tree.

--- com.top_logic.element/src/main/java/com/top_logic/element/layout/grid/AbstractGridCreateHandler.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/layout/grid/AbstractGridCreateHandler.java
@@ -5,9 +5,10 @@
  */
 package com.top_logic.element.layout.grid;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.top_logic.basic.CalledByReflection;
 import com.top_logic.basic.config.InstantiationContext;
@@ -15,7 +16,7 @@ import com.top_logic.basic.config.PolymorphicConfiguration;
 import com.top_logic.basic.config.annotation.Name;
 import com.top_logic.basic.config.annotation.defaults.ImplementationClassDefault;
 import com.top_logic.basic.config.annotation.defaults.ItemDefault;
-import com.top_logic.basic.util.ResKey;
+import com.top_logic.element.layout.create.CreateTypeOptions;
 import com.top_logic.element.meta.schema.ElementSchema;
 import com.top_logic.layout.ContextPosition;
 import com.top_logic.layout.DisplayContext;
@@ -26,13 +27,11 @@ import com.top_logic.layout.form.component.AbstractApplyCommandHandler;
 import com.top_logic.layout.form.component.CreateFunction;
 import com.top_logic.layout.form.model.FormContext;
 import com.top_logic.layout.messagebox.SimpleSelectDialog;
-import com.top_logic.layout.provider.MetaLabelProvider;
 import com.top_logic.layout.provider.label.TLTypeLabelProvider;
 import com.top_logic.mig.html.layout.LayoutComponent;
 import com.top_logic.model.TLClass;
 import com.top_logic.model.TLObject;
 import com.top_logic.model.TLType;
-import com.top_logic.model.util.TLModelUtil;
 import com.top_logic.tool.boundsec.CommandHandler;
 import com.top_logic.tool.boundsec.HandlerResult;
 import com.top_logic.tool.boundsec.conditional.CommandStep;
@@ -43,7 +42,7 @@ import com.top_logic.tool.boundsec.conditional.Success;
 /**
  * Base class for {@link CommandHandler}s starting a create operation in a {@link GridComponent}.
  * 
- * @see #getCreateType()
+ * @see #getTypeOptions()
  * @see #getCreateOperation()
  *
  * @author <a href="mailto:bhu@top-logic.com">Bernhard Haumacher</a>
@@ -89,23 +88,21 @@ public abstract class AbstractGridCreateHandler extends PreconditionCommandHandl
 	protected CommandStep prepare(LayoutComponent component, Object model, Map<String, Object> arguments) {
 		GridComponent grid = (GridComponent) component;
 
-		TLClass createType = getCreateType();
-		boolean isAbstract = createType.isAbstract();
-		if (isAbstract || openTypeChooser()) {
-			Set<TLClass> concreteSpecializations = TLModelUtil.getConcreteSpecializations(createType);
-			if (concreteSpecializations.isEmpty()) {
-				String typeName = MetaLabelProvider.INSTANCE.getLabel(createType);
-				ResKey errorKey = I18NConstants.ABSTRACT_TYPE_WITHOUT_SUBTYPES__TYPE.fill(typeName);
-				return new Failure(errorKey);
-			} else if (concreteSpecializations.size() == 1) {
-				return successSingleOption(grid, model, concreteSpecializations.iterator().next());
-			} else {
-				return successMultipleOptions(grid, model, arguments, concreteSpecializations,
-					isAbstract ? null : createType);
-			}
-		} else {
-			return successSingleOption(grid, model, createType);
+		List<TLClass> possibleTypes = getTypeOptions().getPossibleTypes(model);
+		if (possibleTypes.isEmpty()) {
+			return new Failure(I18NConstants.NO_NEW_OBJECT_FOR_THIS_CONTEXT__CONTEXT.fill(model));
 		}
+		if (possibleTypes.size() == 1) {
+			return successSingleOption(grid, model, possibleTypes.get(0));
+		}
+		TLClass defaultType = getTypeOptions().getDefaultType(model, possibleTypes);
+		if (openTypeChooser()) {
+			return successMultipleOptions(grid, model, arguments, possibleTypes, defaultType);
+		}
+		if (defaultType != null) {
+			return successSingleOption(grid, model, defaultType);
+		}
+		return successMultipleOptions(grid, model, arguments, possibleTypes, defaultType);
 	}
 
 	/**
@@ -121,7 +118,8 @@ public abstract class AbstractGridCreateHandler extends PreconditionCommandHandl
 	}
 
 	/**
-	 * Whether to open a sub-type chooser, even if {@link #getCreateType()} is concrete.
+	 * Whether to open a sub-type chooser, even if
+	 * {@link CreateTypeOptions#getDefaultType(Object, List) the default type} is concrete.
 	 */
 	protected boolean openTypeChooser() {
 		return config().getTypeChooser();
@@ -132,9 +130,13 @@ public abstract class AbstractGridCreateHandler extends PreconditionCommandHandl
 	}
 
 	/**
-	 * The type to instantiate.
+	 * Computes which types the user can create, based on the context.
+	 * <p>
+	 * The context is the {@link #getTargetModel(LayoutComponent, Map) target model} of this
+	 * command.
+	 * </p>
 	 */
-	protected abstract TLClass getCreateType();
+	protected abstract CreateTypeOptions getTypeOptions();
 
 	/**
 	 * The operation that finally performs the object creation.
@@ -157,7 +159,7 @@ public abstract class AbstractGridCreateHandler extends PreconditionCommandHandl
 	}
 
 	private Success successMultipleOptions(GridComponent grid, Object model, Map<String, Object> arguments,
-			Set<TLClass> concreteTypes, TLClass defaultOption) {
+			Collection<TLClass> concreteTypes, TLClass defaultOption) {
 		assert !concreteTypes.isEmpty() : "Concretes types must not be empty";
 		return new Success() {
 
@@ -182,7 +184,7 @@ public abstract class AbstractGridCreateHandler extends PreconditionCommandHandl
 			}
 
 			private void openSelectDialog(DisplayContext aContext, HandlerResult suspended,
-					Set<TLClass> concreteSpecializations) {
+					Collection<TLClass> concreteSpecializations) {
 				new SimpleSelectDialog<>(I18NConstants.SELECT_CONCRETE_TYPE_DIALOG, concreteSpecializations)
 					.setDefaultOption(defaultOption)
 					.setLabels(new TLTypeLabelProvider())

dfebad09835b7bd6e05e55e5c7acae6ad9f73982
Sven Förster <sven.foerster@top-logic.com>
2023-11-22 17:37:01 +0100

    Ticket #27640: Adjusted comments of the ElementVisibilityEventHandler.

--- com.top_logic.graph.diagramjs.client/src/main/java/com/top_logic/graph/diagramjs/client/service/event/ElementVisibilityEventHandler.java
+++ com.top_logic.graph.diagramjs.client/src/main/java/com/top_logic/graph/diagramjs/client/service/event/ElementVisibilityEventHandler.java
@@ -8,7 +8,6 @@ package com.top_logic.graph.diagramjs.client.service.event;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
 
 import com.top_logic.ajax.client.compat.AJAX;
@@ -20,7 +19,8 @@ import com.top_logic.graph.diagramjs.client.ajax.VisibilityCommandArguments;
 import com.top_logic.graph.diagramjs.util.DiagramJSGraphControlCommon;
 
 /**
- * Handler that is executed when the user toggles a diagram elements visibility.
+ * Handler that is executed to inform the server that on the clients diagram a collection of diagram
+ * parts have changed their visibility.
  * 
  * @author <a href="mailto:sven.foerster@top-logic.com">Sven Förster</a>
  */
@@ -39,26 +39,29 @@ public final class ElementVisibilityEventHandler implements EventHandler {
 	public void call(Event event) {
 		VisibilityCommandArguments arguments = JavaScriptObject.createObject().cast();
 
-		GWT.debugger();
-
-		Base[] elements = getElement(event);
-		List<String> ids = new ArrayList<>();
-		for(int i = 0; i < elements.length; i++) {
-			ids.add(getID((DefaultSharedObject) elements[i].getSharedGraphPart()));
-		}
 		arguments.setVisibility(getVisibility(event));
-		arguments.setIDs(ids.toArray(new String[elements.length]));
+		arguments.setIDs(getGraphPartIDs(getElements(event)));
 		arguments.setControlID(_controlID);
 		arguments.setControlCommand(DiagramJSGraphControlCommon.ELEMENT_VISIBILITY_COMMAND);
 
 		AJAX.execute("dispatchControlCommand", arguments);
 	}
 
+	private String[] getGraphPartIDs(Base[] elements) {
+		List<String> ids = new ArrayList<>();
+
+		for (int i = 0; i < elements.length; i++) {
+			ids.add(getID((DefaultSharedObject) elements[i].getSharedGraphPart()));
+		}
+
+		return ids.toArray(new String[elements.length]);
+	}
+
 	private String getID(DefaultSharedObject sharedGraphPart) {
 		return sharedGraphPart.data().id();
 	}
 
-	private native Base[] getElement(Event event) /*-{
+	private native Base[] getElements(Event event) /*-{
 		return event.elements;
 	}-*/;
 

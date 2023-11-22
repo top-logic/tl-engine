68951ba20ac3753a5855846c9961080eb70b73e4
Sven Förster <sven.foerster@top-logic.com>
2023-11-22 18:37:45 +0100

    Ticket #27640: Renamed handler of diagram element visibility changes.

--- com.top_logic.graph.diagramjs.server/src/main/java/com/top_logic/graph/diagramjs/server/handler/ShowHiddenElementsHandler.java
+++ com.top_logic.graph.diagramjs.server/src/main/java/com/top_logic/graph/diagramjs/server/handler/ShowHiddenElementsHandler.java
@@ -6,6 +6,7 @@
 package com.top_logic.graph.diagramjs.server.handler;
 
 import com.top_logic.basic.config.InstantiationContext;
+import com.top_logic.graph.common.model.GraphPart;
 import com.top_logic.graph.diagramjs.server.DiagramJSGraphComponent;
 import com.top_logic.graph.diagramjs.server.commands.ShowAllElements;
 import com.top_logic.layout.DisplayContext;
@@ -13,9 +14,12 @@ import com.top_logic.mig.html.layout.LayoutComponent;
 import com.top_logic.tool.boundsec.commandhandlers.ToggleCommandHandler;
 
 /**
- * Shows the diagrams hidden elements.
+ * Shows the diagrams {@link GraphPart#isVisible() hidden} elements.
  * 
  * In contrast to the {@link ShowAllElements}, these changes are not permanent.
+ * 
+ * @see DiagramJSGraphComponent#showHiddenElements()
+ * @see GraphPart#isVisible()
  * 
  * @author <a href="mailto:sven.foerster@top-logic.com">Sven Förster</a>
  */

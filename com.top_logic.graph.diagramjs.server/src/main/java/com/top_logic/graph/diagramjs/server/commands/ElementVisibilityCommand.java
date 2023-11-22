f2547906da75c621b3f01de1884a5c84b1078579
Sven Förster <sven.foerster@top-logic.com>
2023-11-22 18:30:53 +0100

    Ticket #27640: Renamed handler that is executed when elements have changed their visibility in the client diagram.

--- com.top_logic.graph.diagramjs.server/src/main/java/com/top_logic/graph/diagramjs/server/commands/ElementVisibilityCommand.java
+++ com.top_logic.graph.diagramjs.server/src/main/java/com/top_logic/graph/diagramjs/server/commands/ElementVisibilityCommand.java
@@ -13,6 +13,7 @@ import java.util.Set;
 import com.top_logic.basic.util.ResKey;
 import com.top_logic.graph.diagramjs.server.DiagramJSGraphControl;
 import com.top_logic.graph.diagramjs.server.I18NConstants;
+import com.top_logic.graph.diagramjs.server.handler.ElementsVisibilityHandler;
 import com.top_logic.graph.diagramjs.util.DiagramJSGraphControlCommon;
 import com.top_logic.layout.Control;
 import com.top_logic.layout.DisplayContext;
@@ -20,44 +21,54 @@ import com.top_logic.layout.basic.ControlCommand;
 import com.top_logic.tool.boundsec.HandlerResult;
 
 /**
- * A {@link ControlCommand} executed when the diagram element has changed its visibility.
+ * A {@link ControlCommand} executed when elements on the client diagram have changed their
+ * visibility.
+ * 
+ * @see ElementsVisibilityHandler
  * 
  * @author <a href="mailto:sven.foerster@top-logic.com">Sven Förster</a>
  */
-public class ToggleElementVisibilityCommand extends ControlCommand {
+public class ElementVisibilityCommand extends ControlCommand {
 
 	/**
-	 * Singleton instance of {@link ToggleElementVisibilityCommand}.
+	 * Singleton instance of {@link ElementVisibilityCommand}.
 	 */
-	public static final ToggleElementVisibilityCommand INSTANCE = new ToggleElementVisibilityCommand();
+	public static final ElementVisibilityCommand INSTANCE = new ElementVisibilityCommand();
 
-	private ToggleElementVisibilityCommand() {
+	private ElementVisibilityCommand() {
 		super(DiagramJSGraphControlCommon.ELEMENT_VISIBILITY_COMMAND);
 	}
 
 	@Override
 	public ResKey getI18NKey() {
-		return I18NConstants.TOGGLE_ELEMENT_VISIBILITY_COMMAND;
+		return I18NConstants.ELEMENT_VISIBILITY_COMMAND;
 	}
 
 	@Override
 	protected HandlerResult execute(DisplayContext commandContext, Control control, Map<String, Object> arguments) {
 		DiagramJSGraphControl graphControl = (DiagramJSGraphControl) control;
-		boolean isVisible = (Boolean) arguments.get("visibility");
-		graphControl.setElementsVisibility(getGraphPartModels(arguments, graphControl), isVisible);
+		graphControl.setElementsVisibility(getGraphPartModels(arguments, graphControl), getVisibility(arguments));
 
 		return HandlerResult.DEFAULT_RESULT;
 	}
 
+	private boolean getVisibility(Map<String, Object> arguments) {
+		return (Boolean) arguments.get("visibility");
+	}
+
 	private Set<Object> getGraphPartModels(Map<String, Object> arguments, DiagramJSGraphControl graphControl) {
-		List<String> ids = (List<String>) arguments.get("ids");
 		Set<Object> graphPartModels = new HashSet<>();
 
-		for (int i = 0; i < ids.size(); i++) {
-			graphPartModels.add(DeleteGraphPartCommand.getGraphPart(ids.get(i), graphControl).getTag());
+		for (String id : getElementIDs(arguments)) {
+			graphPartModels.add(DeleteGraphPartCommand.getGraphPart(id, graphControl).getTag());
 		}
 
 		return graphPartModels;
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<String> getElementIDs(Map<String, Object> arguments) {
+		return (List<String>) arguments.get("ids");
 	}
 
 }

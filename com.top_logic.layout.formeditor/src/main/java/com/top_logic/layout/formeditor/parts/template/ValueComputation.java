a5c99bf2242fe798236d3171426094cfe5267f38
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-11-27 16:56:42 +0100

    Ticket #27091: Generalized structure of HTML templates with in forms.
    A HTML template within a form can now define its own variables. There is
    no longer a single value "items" that is being rendered.

--- com.top_logic.layout.formeditor/src/main/java/com/top_logic/layout/formeditor/parts/template/ValueComputation.java
+++ com.top_logic.layout.formeditor/src/main/java/com/top_logic/layout/formeditor/parts/template/ValueComputation.java
@@ -13,13 +13,8 @@ import com.top_logic.basic.config.annotation.Name;
 import com.top_logic.basic.config.annotation.TagName;
 import com.top_logic.basic.config.order.DisplayOrder;
 import com.top_logic.layout.DisplayContext;
-import com.top_logic.layout.ModelSpec;
-import com.top_logic.layout.channel.ComponentChannel;
-import com.top_logic.layout.channel.ComponentChannel.ChannelListener;
-import com.top_logic.layout.channel.linking.impl.ChannelLinking;
 import com.top_logic.mig.html.layout.LayoutComponent;
 import com.top_logic.model.search.expr.config.dom.Expr;
-import com.top_logic.model.search.expr.query.Args;
 import com.top_logic.model.search.expr.query.QueryExecutor;
 
 /**
@@ -27,30 +22,17 @@ import com.top_logic.model.search.expr.query.QueryExecutor;
  * function.
  */
 public class ValueComputation extends AbstractConfiguredInstance<ValueComputation.Config>
-		implements VariableDefinition {
+		implements VariableDefinition<ValueComputation.Config> {
 
 	/**
 	 * Definition of a local template variable.
 	 */
 	@DisplayOrder({
 		Config.NAME,
-		Config.INPUT,
 		Config.FUNCTION,
 	})
 	@TagName("value-computation")
 	public interface Config extends VariableDefinition.Config<ValueComputation> {
-
-		/**
-		 * @see #getInput()
-		 */
-		String INPUT = "input";
-
-		/**
-		 * Source of an additional input to the specified {@link #getFunction()}.
-		 */
-		@Name(INPUT)
-		ModelSpec getInput();
-
 		/**
 		 * @see #getFunction()
 		 */
@@ -68,8 +50,6 @@ public class ValueComputation extends AbstractConfiguredInstance<ValueComputatio
 
 	private QueryExecutor _function;
 
-	private ChannelLinking _model;
-
 	/**
 	 * Creates a {@link ValueComputation} from configuration.
 	 * 
@@ -83,86 +63,10 @@ public class ValueComputation extends AbstractConfiguredInstance<ValueComputatio
 		super(context, config);
 
 		_function = QueryExecutor.compile(config.getFunction());
-		_model = context.getInstance(config.getInput());
 	}
 
 	@Override
 	public EvalResult eval(DisplayContext displayContext, LayoutComponent component, Object model) {
-		if (_model == null) {
-			return new ResultValue(_function.execute(displayContext, null, model));
-		}
-
-		ComponentChannel channel = _model.resolveChannel(null, component);
-
-		return new DynamicEvalResult(_function, model, channel);
+		return new ResultValue(_function.execute(displayContext, null, model));
 	}
-
-	private static class DynamicEvalResult implements EvalResult, ChannelListener {
-
-		private QueryExecutor _function;
-
-		private Object _model;
-
-		private ComponentChannel _channel;
-
-		private InvalidateListener _listener;
-
-		/**
-		 * Creates a {@link DynamicEvalResult}.
-		 */
-		public DynamicEvalResult(QueryExecutor function, Object model, ComponentChannel channel) {
-			_function = function;
-			_model = model;
-			_channel = channel;
-		}
-
-		@Override
-		public Object getValue(DisplayContext displayContext) {
-			Object input = _channel.get();
-			return _function.executeWith(displayContext, null, Args.some(_model, input));
-		}
-
-		@Override
-		public boolean addInvalidateListener(InvalidateListener listener) {
-			if (_listener == listener) {
-				return false;
-			}
-
-			if (_listener != null) {
-				throw new IllegalStateException("Only a single listener supported.");
-			}
-
-			_listener = listener;
-			_channel.addListener(this);
-
-			return true;
-		}
-
-		@Override
-		public boolean removeInvalidateListener(InvalidateListener listener) {
-			if (_listener == listener) {
-				detach();
-				return true;
-			}
-			return false;
-		}
-
-		@Override
-		public void handleNewValue(ComponentChannel sender, Object oldValue, Object newValue) {
-			if (_listener != null) {
-				_listener.handleValueInvalidation(this);
-			}
-
-			detach();
-		}
-
-		private void detach() {
-			if (_listener != null) {
-				_listener = null;
-				_channel.removeListener(this);
-			}
-		}
-
-	}
-
 }

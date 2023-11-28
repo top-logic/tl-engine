3528f27ea4b9f01acc5eaf498713c2234d96019b
Petar Janosevic <petar.janosevic@top-logic.com>
2023-11-28 10:02:40 +0100

    Ticket #25539: The LayoutComponentLabelProvider has been added again. Without it, there are labels that use the technical name and not the correct label name.

---
+++ com.top_logic/src/main/java/com/top_logic/mig/html/layout/LayoutComponentLabelProvider.java
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: 2019 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.mig.html.layout;
+
+import com.top_logic.layout.LabelProvider;
+import com.top_logic.layout.provider.label.NullSafeLabelProvider;
+import com.top_logic.util.Resources;
+
+/**
+ * A {@link LabelProvider} for {@link LayoutComponent}s.
+ * 
+ * @author <a href="mailto:jst@top-logic.com">Jan Stolzenburg</a>
+ */
+public class LayoutComponentLabelProvider extends NullSafeLabelProvider {
+
+	/** Returns the {@link LayoutComponentLabelProvider} instance. */
+	public static final LayoutComponentLabelProvider INSTANCE = new LayoutComponentLabelProvider();
+
+	@Override
+	public String getLabelNullSafe(Object object) {
+		LayoutComponent component = (LayoutComponent) object;
+		return Resources.getInstance().getString(component.getTitleKey());
+	}
+
+}

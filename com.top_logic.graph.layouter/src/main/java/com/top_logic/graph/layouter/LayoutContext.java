2d0c5d49f278aa41f85ea691437ef8273f6f44e2
Sven Förster <sven.foerster@top-logic.com>
2023-11-29 19:43:27 +0100

    Ticket #27640: Hide only inheritance edges when technical generalizations should not be displayed, references are not considered.

--- com.top_logic.graph.layouter/src/main/java/com/top_logic/graph/layouter/LayoutContext.java
+++ com.top_logic.graph.layouter/src/main/java/com/top_logic/graph/layouter/LayoutContext.java
@@ -6,10 +6,9 @@
 package com.top_logic.graph.layouter;
 
 import java.util.Collection;
-import java.util.Collections;
 
 import com.top_logic.layout.LabelProvider;
-import com.top_logic.layout.provider.MetaResourceProvider;
+import com.top_logic.model.TLType;
 
 /**
  * Context for graph layouting.
@@ -24,21 +23,17 @@ public class LayoutContext {
 
 	private final Collection<Object> _hiddenElements;
 
-	/**
-	 * Creates a {@link LayoutContext} with the {@link MetaResourceProvider} and
-	 * {@link LayoutDirection} vertical from sink as default.
-	 */
-	public LayoutContext() {
-		this(LayoutDirection.VERTICAL_FROM_SINK, MetaResourceProvider.INSTANCE, Collections.emptySet());
-	}
+	private final Collection<TLType> _hiddenGeneralizations;
 
 	/**
 	 * Creates a {@link LayoutContext} with the given arguments.
 	 */
-	public LayoutContext(LayoutDirection direction, LabelProvider labelProvider, Collection<Object> hiddenElements) {
+	public LayoutContext(LayoutDirection direction, LabelProvider labelProvider, Collection<Object> hiddenElements,
+			Collection<TLType> hiddenGeneralizations) {
 		_direction = direction;
 		_labelProvider = labelProvider;
 		_hiddenElements = hiddenElements;
+		_hiddenGeneralizations = hiddenGeneralizations;
 	}
 
 	/**
@@ -60,6 +55,17 @@ public class LayoutContext {
 	 */
 	public Collection<Object> getHiddenElements() {
 		return _hiddenElements;
+	}
+
+	/**
+	 * Collection of objects that should be hidden as generalizations.
+	 * 
+	 * <p>
+	 * References to this set of types are still displayed.
+	 * </p>
+	 */
+	public Collection<TLType> getHiddenGeneralizations() {
+		return _hiddenGeneralizations;
 	}
 
 }

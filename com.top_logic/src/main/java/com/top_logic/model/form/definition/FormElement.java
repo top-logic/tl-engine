4af150e4a06b1259fe847ea20ba2293aa14224d1
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2023-11-29 14:18:31 +0100

    Ticket #27091: Hide technical property for a form definition.

--- com.top_logic/src/main/java/com/top_logic/model/form/definition/FormElement.java
+++ com.top_logic/src/main/java/com/top_logic/model/form/definition/FormElement.java
@@ -37,6 +37,7 @@ public interface FormElement<T extends FormElementTemplateProvider>
 	 * The context in which this {@link FormElement} is defined.
 	 */
 	@Container
+	@Hidden
 	@Name(FORM_CONTEXT_DEFINITION)
 	FormContextDefinition getFormContextDefinition();
 

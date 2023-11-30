2677fe99c892ac196c962c982656c39bebefc92b
Daniel Busche <daniel.busche@top-logic.com>
2023-11-30 15:46:54 +0100

    Ticket #27656: Added factory to create MultiLineText for a given number of rows and columns.

--- com.top_logic/src/main/java/com/top_logic/layout/form/control/TextInputControl.java
+++ com.top_logic/src/main/java/com/top_logic/layout/form/control/TextInputControl.java
@@ -136,7 +136,7 @@ public class TextInputControl extends AbstractFormFieldControl implements WithPl
      * Undo {@link #setColumns(int) setting the columns property}.
      */
     public void resetColumns() {
-		setColumns(0);
+		setColumns(NO_COLUMNS);
     }
     
     /**

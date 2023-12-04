02553f4443037913cfed8d29dfc60ca87f374b4f
Simon Haneke <simon.haneke@top-logic.com>
2023-12-04 11:45:55 +0100

    Ticket #27480: Added Help Page for JMS.

--- tl-service-jms-activemq/src/test/java/com/top_logic/services/jms/activemq/TestApp.java
+++ tl-service-jms-activemq/src/test/java/com/top_logic/services/jms/activemq/TestApp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Simon Haneke. All Rights Reserved
+ * Copyright (c) 2023 Business Operation Systems GmbH. All Rights Reserved.
  */
 package com.top_logic.services.jms.activemq;
 

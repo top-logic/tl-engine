22fde7a05d152e4a600aa415aa52fdeb1b003ba8
Daniel Busche <daniel.busche@top-logic.com>
2023-12-12 15:45:14 +0100

    Ticket #27517: Extracted command to reset start page to extra file.

--- com.top_logic.contact/src/main/java/com/top_logic/contact/layout/person/related/EditCurrentPersonComponent.java
+++ com.top_logic.contact/src/main/java/com/top_logic/contact/layout/person/related/EditCurrentPersonComponent.java
@@ -11,26 +11,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import com.top_logic.basic.CalledByReflection;
 import com.top_logic.basic.col.factory.CollectionFactory;
 import com.top_logic.basic.config.ConfigurationException;
-import com.top_logic.basic.config.ConfigurationItem;
 import com.top_logic.basic.config.InstantiationContext;
 import com.top_logic.basic.config.PolymorphicConfiguration;
 import com.top_logic.basic.config.TypedConfiguration;
-import com.top_logic.basic.config.annotation.defaults.FormattedDefault;
-import com.top_logic.basic.config.annotation.defaults.ListDefault;
 import com.top_logic.basic.translation.TranslationService;
 import com.top_logic.knowledge.wrap.person.Homepage;
-import com.top_logic.knowledge.wrap.person.NoStartPageAutomatismExecutability;
 import com.top_logic.knowledge.wrap.person.Person;
 import com.top_logic.knowledge.wrap.person.PersonalConfiguration;
 import com.top_logic.knowledge.wrap.person.PersonalConfigurationWrapper;
 import com.top_logic.layout.Accessor;
-import com.top_logic.layout.DisplayContext;
 import com.top_logic.layout.SimpleAccessor;
 import com.top_logic.layout.form.FormField;
 import com.top_logic.layout.form.model.BooleanField;
@@ -47,17 +41,10 @@ import com.top_logic.layout.table.model.TableConfigurationFactory;
 import com.top_logic.mig.html.layout.CommandRegistry;
 import com.top_logic.mig.html.layout.ComponentName;
 import com.top_logic.mig.html.layout.LayoutComponent;
-import com.top_logic.tool.boundsec.AbstractCommandHandler;
 import com.top_logic.tool.boundsec.BoundCommandGroup;
 import com.top_logic.tool.boundsec.BoundObject;
-import com.top_logic.tool.boundsec.CommandGroupReference;
-import com.top_logic.tool.boundsec.CommandHandler;
-import com.top_logic.tool.boundsec.DefaultHandlerResult;
-import com.top_logic.tool.boundsec.HandlerResult;
 import com.top_logic.tool.boundsec.compound.CompoundSecurityLayout;
 import com.top_logic.tool.boundsec.simple.SimpleBoundCommandGroup;
-import com.top_logic.tool.execution.ExecutabilityRule;
-import com.top_logic.tool.execution.InViewModeExecutable;
 import com.top_logic.util.Resources;
 import com.top_logic.util.TLContextManager;
 
@@ -241,54 +228,6 @@ public class EditCurrentPersonComponent extends EditRelatedPersonComponent {
 
 	private PersonalConfiguration getPersonalConfig() {
 		return PersonalConfiguration.getPersonalConfiguration();
-	}
-
-	/** A {@link CommandHandler} for resetting the start page of the current user. */
-	public static class ResetStartPageCommandHandler extends AbstractCommandHandler {
-
-		/** {@link ConfigurationItem} of the {@link ResetStartPageCommandHandler}. */
-		public interface Config extends AbstractCommandHandler.Config {
-
-			@Override
-			@ListDefault({ InViewModeExecutable.class, NoStartPageAutomatismExecutability.class })
-			List<PolymorphicConfiguration<? extends ExecutabilityRule>> getExecutability();
-
-			@Override
-			@FormattedDefault(SimpleBoundCommandGroup.SYSTEM_NAME)
-			CommandGroupReference getGroup();
-
-		}
-
-		/** The default {@link #getID()} of the command. */
-		public static final String COMMAND_ID = "resetStartPage";
-
-		/**
-		 * Called by the {@link TypedConfiguration} for creating a
-		 * {@link EditCurrentPersonComponent.ResetStartPageCommandHandler}.
-		 * <p>
-		 * <b>Don't call directly.</b> Use
-		 * {@link InstantiationContext#getInstance(PolymorphicConfiguration)} instead.
-		 * </p>
-		 * 
-		 * @param context
-		 *        For error reporting and instantiation of dependent configured objects.
-		 * @param config
-		 *        The configuration for the new instance.
-		 */
-		@CalledByReflection
-		public ResetStartPageCommandHandler(InstantiationContext context, Config config) {
-			super(context, config);
-		}
-
-		@Override
-		public HandlerResult handleCommand(DisplayContext aContext, LayoutComponent aComponent, Object model, Map<String, Object> someArguments) {
-			PersonalConfiguration thePConf = PersonalConfiguration.getPersonalConfiguration();
-			thePConf.removeHomepage(aComponent.getMainLayout());
-			PersonalConfiguration.storePersonalConfiguration();
-			aComponent.invalidate();
-			return DefaultHandlerResult.DEFAULT_RESULT;
-		}
-
 	}
 
 	@Override

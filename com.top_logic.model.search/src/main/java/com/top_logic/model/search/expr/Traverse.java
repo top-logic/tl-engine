b5ecfa5c14584003d4e92776114d6579acf6084c
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-01-05 12:48:30 +0100

    Ticket #27705: Fixed file header.

--- com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/Traverse.java
+++ com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/Traverse.java
@@ -1,7 +1,7 @@
 /*
- * SPDX-FileCopyrightText: 2023 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * SPDX-FileCopyrightText: 2019 (c) Business Operation Systems GmbH <info@top-logic.com>
  * 
- * SPDX-License-Identifier: LicenseRef-BOS-TopLogic-1.0
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
  */
 package com.top_logic.model.search.expr;
 

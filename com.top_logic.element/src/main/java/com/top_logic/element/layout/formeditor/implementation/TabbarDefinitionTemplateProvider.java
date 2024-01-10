9193421dca5b66d0e36905c2e26e64f2c0b97040
Daniel Busche <daniel.busche@top-logic.com>
2024-01-10 13:47:10 +0100

    Ticket #27517: Removed API's for creating design or display template.
    A FormElementTemplateProvider creates a template. The implementation can
    decide whether it is for design or display by inspecting the context.
    Therefore no separate API's are necessary.

--- com.top_logic.element/src/main/java/com/top_logic/element/layout/formeditor/implementation/TabbarDefinitionTemplateProvider.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/layout/formeditor/implementation/TabbarDefinitionTemplateProvider.java
@@ -67,12 +67,7 @@ public class TabbarDefinitionTemplateProvider extends AbstractFormElementProvide
 	}
 
 	@Override
-	public HTMLTemplateFragment createDesignTemplate(FormEditorContext context) {
-		return super.createDesignTemplate(context);
-	}
-
-	@Override
-	public HTMLTemplateFragment createDisplayTemplate(FormEditorContext context) {
+	protected HTMLTemplateFragment createDisplayTemplate(FormEditorContext context) {
 		TabbarDefinition config = getConfig();
 
 		String deckName = config.getUUID();

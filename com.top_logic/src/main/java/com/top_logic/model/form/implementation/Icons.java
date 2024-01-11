d86069cbcc78bd8574d62b2477ce99e2de0b71a0
Daniel Busche <daniel.busche@top-logic.com>
2024-01-11 12:35:42 +0100

    Ticket #27517: Added mechanism to use programmatic FormTemplateProvider without defining an separate tool implementation.

---
+++ com.top_logic/src/main/java/com/top_logic/model/form/implementation/Icons.java
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2024 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.model.form.implementation;
+
+import com.top_logic.layout.basic.IconsBase;
+import com.top_logic.layout.basic.ThemeImage;
+
+/**
+ * Icon constants for this package.
+ *
+ * @see ThemeImage
+ */
+@SuppressWarnings("javadoc")
+public class Icons extends IconsBase {
+
+	public static ThemeImage FORM_EDITOR_TOOL_CODE_ELEMENT;
+
+}

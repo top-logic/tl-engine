5eaa9d5e8c85d749f73b0efd0c61a4be21534fec
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-02-06 14:17:10 +0100

    Ticket #27781: Added script functions pdf2text and parseJson.

--- com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/config/operations/binary/I18NConstants.java
+++ com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/config/operations/binary/I18NConstants.java
@@ -5,6 +5,7 @@
  */
 package com.top_logic.model.search.expr.config.operations.binary;
 
+import com.top_logic.basic.util.ResKey1;
 import com.top_logic.basic.util.ResKey2;
 import com.top_logic.layout.I18NConstantsBase;
 
@@ -17,6 +18,16 @@ public class I18NConstants extends I18NConstantsBase {
 	 * @en Invalid content type "{0}" specified: {1}
 	 */
 	public static ResKey2 ERROR_INVALID_CONTENT_TYPE_SPECIFIED__VALUE_MSG;
+
+	/**
+	 * @en Binary data expected got "{0}".
+	 */
+	public static ResKey1 ERROR_BINARY_DATA_EXPECTED__ACTUAL;
+
+	/**
+	 * @en Conversion failed: {0}
+	 */
+	public static ResKey1 ERROR_CONVERSION_FAILED__MSG;
 
 	static {
 		initConstants(I18NConstants.class);

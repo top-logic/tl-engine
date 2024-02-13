e5befb37291c833c6153cb95e5a4206774ed88d4
Isabell Wittich <isabell.wittich@top-logic.com>
2024-02-13 14:37:00 +0100

    Ticket #27718: Added method for external scripts.

--- com.top_logic.monitoring/src/main/java/com/top_logic/monitoring/data/MonitorByExpression.java
+++ com.top_logic.monitoring/src/main/java/com/top_logic/monitoring/data/MonitorByExpression.java
@@ -5,6 +5,10 @@
  */
 package com.top_logic.monitoring.data;
 
+import java.io.StringReader;
+
+import com.top_logic.basic.Logger;
+import com.top_logic.basic.StringServices;
 import com.top_logic.basic.config.AbstractConfiguredInstance;
 import com.top_logic.basic.config.ConfigurationItem;
 import com.top_logic.basic.config.InstantiationContext;
@@ -13,7 +17,10 @@ import com.top_logic.basic.config.annotation.Label;
 import com.top_logic.basic.config.annotation.Mandatory;
 import com.top_logic.basic.config.annotation.ValueInitializer;
 import com.top_logic.basic.config.constraint.annotation.Constraint;
+import com.top_logic.basic.thread.ThreadContextManager;
 import com.top_logic.model.search.expr.config.dom.Expr;
+import com.top_logic.model.search.expr.parser.ParseException;
+import com.top_logic.model.search.expr.parser.SearchExpressionParser;
 import com.top_logic.model.search.expr.query.QueryExecutor;
 
 /**
@@ -24,7 +31,9 @@ import com.top_logic.model.search.expr.query.QueryExecutor;
 public class MonitorByExpression extends AbstractConfiguredInstance<MonitorByExpression.Config>
 		implements MonitorByExpressionMBean, MBeanElement {
 
-	private QueryExecutor _compile;
+	private QueryExecutor _internalImplemenation;
+
+	private QueryExecutor _externalImplementation;
 
 	/** {@link ConfigurationItem} for the {@link MonitorByExpression}. */
 	public interface Config extends MBeanConfiguration<MonitorByExpression> {
@@ -47,12 +56,46 @@ public class MonitorByExpression extends AbstractConfiguredInstance<MonitorByExp
 	public MonitorByExpression(InstantiationContext context, Config config) {
 		super(context, config);
 
-		_compile = QueryExecutor.compile(config.getImpl());
+		_internalImplemenation = QueryExecutor.compile(config.getImpl());
 	}
 
 	@Override
 	public Object getResult() {
-		return _compile.execute();
+		return _internalImplemenation.execute();
+	}
+
+	@Override
+	public void compileExternalQuery(String script) {
+		Expr stringToExpr = stringToExpr(script);
+
+		if (stringToExpr != null) {
+			ThreadContextManager.inSystemInteraction(MonitorUser.class, () -> {
+				_externalImplementation = QueryExecutor.compile(stringToExpr);
+			});
+		}
+	}
+
+	private Expr stringToExpr(String script) {
+		if (StringServices.isEmpty(script)) {
+			return null;
+		}
+
+		SearchExpressionParser parser = new SearchExpressionParser(new StringReader(script));
+		try {
+			return parser.expr();
+		} catch (ParseException ex) {
+			Logger.error("Failed to create an Expr of: '" + script + "'.", ex, MonitorByExpression.class);
+		}
+		return null;
+	}
+
+	@Override
+	public Object getExternalResult() {
+		if (_externalImplementation != null) {
+			return _externalImplementation.execute();
+		}
+
+		return null;
 	}
 
 }

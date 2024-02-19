64a7d4ae72b57c7c95d669e51d953c0992fb2a12
Jan Stolzenburg <jan.stolzenburg@top-logic.com>
2024-02-19 16:16:32 +0100

    Ticket #27736: Move the automatic migration to `com.top_logic.monitoring`.

--- com.top_logic.monitoring/src/main/java/com/top_logic/monitoring/migration/Ticket27736UpgradeLayout.java
+++ com.top_logic.monitoring/src/main/java/com/top_logic/monitoring/migration/Ticket27736UpgradeLayout.java
@@ -3,7 +3,7 @@
  * 
  * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
  */
-package com.top_logic.layout.migration;
+package com.top_logic.monitoring.migration;
 
 import static com.top_logic.basic.core.xml.DOMUtil.*;
 import static com.top_logic.basic.shared.collection.iterator.IteratorUtilShared.*;

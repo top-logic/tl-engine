8b4a91afcb266d788c8db68856ebc9e87b6f0d15
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-03-13 15:02:16 +0100

    Ticket #27662: Added label provider for singleton assignment links.
    This produces better error messages, when a delete-veto is triggered on
    a singleton assignment.

--- com.top_logic/src/main/java/com/top_logic/model/TLModuleSingleton.java
+++ com.top_logic/src/main/java/com/top_logic/model/TLModuleSingleton.java
@@ -5,12 +5,15 @@
  */
 package com.top_logic.model;
 
+import com.top_logic.layout.LabelProvider;
+import com.top_logic.model.util.TLModelUtil;
+
 /**
  * Singleton assignment link.
  * 
  * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
  */
-public interface TLModuleSingleton extends TLNamed, com.top_logic.model.impl.generated.TLModuleSingletonsBase {
+public interface TLModuleSingleton extends com.top_logic.model.impl.generated.TLModuleSingletonsBase {
 
 	/**
 	 * The name under which the singleton is known in {@link TLModuleSingletons}.
@@ -29,4 +32,26 @@ public interface TLModuleSingleton extends TLNamed, com.top_logic.model.impl.gen
 	 * The actual singleton object.
 	 */
 	TLObject getSingleton();
+
+	/**
+	 * {@link LabelProvider} for {@link TLModuleSingleton}s.
+	 */
+	class Label implements LabelProvider {
+
+		/**
+		 * Singleton {@link TLModuleSingleton.Label} instance.
+		 */
+		public static final TLModuleSingleton.Label INSTANCE = new TLModuleSingleton.Label();
+
+		private Label() {
+			// Singleton constructor.
+		}
+
+		@Override
+		public String getLabel(Object object) {
+			TLModuleSingleton link = (TLModuleSingleton) object;
+			return TLModelUtil.qualifiedName(link.getModule()) + TLModelUtil.QUALIFIED_NAME_PART_SEPARATOR
+				+ link.getName();
+		}
+	}
 }

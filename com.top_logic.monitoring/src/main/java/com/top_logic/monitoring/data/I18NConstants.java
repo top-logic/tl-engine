8bebd464be55443c855cee02bda71af99b0bf2d3
Isabell Wittich <isabell.wittich@top-logic.com>
2024-03-19 12:22:41 +0100

    Ticket #27718: Fixed java doc warnings.

--- com.top_logic.monitoring/src/main/java/com/top_logic/monitoring/data/I18NConstants.java
+++ com.top_logic.monitoring/src/main/java/com/top_logic/monitoring/data/I18NConstants.java
@@ -9,13 +9,15 @@ import com.top_logic.basic.util.ResKey;
 import com.top_logic.layout.I18NConstantsBase;
 
 /**
- * {@link I18NConstantsBase} for this package
+ * {@link I18NConstantsBase} for this package.
  * 
  * @author <a href="mailto:iwi@top-logic.com">Isabell Wittich</a>
  */
-@SuppressWarnings("javadoc")
 public class I18NConstants extends I18NConstantsBase {
 
+	/**
+	 * @en The name of the <i>MBean</i> must follow the pattern "domain:key=property".
+	 */
 	public static ResKey WRONG_PATTERN_MBEAN_NAME;
 
 	static {

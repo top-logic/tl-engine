419201c9a3778844596b63162a57fb7c289a6a0b
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-03-28 09:23:12 +0100

    Ticket #27955: Added reasonable label and tooltip for revisions.

--- com.top_logic/src/main/java/com/top_logic/layout/provider/label/I18NConstants.java
+++ com.top_logic/src/main/java/com/top_logic/layout/provider/label/I18NConstants.java
@@ -6,8 +6,7 @@
 package com.top_logic.layout.provider.label;
 
 import com.top_logic.basic.util.ResKey;
-import com.top_logic.basic.util.ResKey1;
-import com.top_logic.knowledge.service.Revision;
+import com.top_logic.basic.util.ResKey2;
 import com.top_logic.layout.I18NConstantsBase;
 
 /**
@@ -17,12 +16,27 @@ import com.top_logic.layout.I18NConstantsBase;
  */
 public class I18NConstants extends I18NConstantsBase {
 
-	/** Label key for the current {@link Revision}. */
+	/**
+	 * @en Current revision
+	 * @tooltip The current revision represents the current state of the application's data
+	 *          inventory that can be modified. All other revisions represent a stable unmodifyable
+	 *          view of historic data.
+	 */
 	public static ResKey CURRENT_REVISION_LABEL;
 
-	/** Label key for a persistent {@link Revision}. */
-	public static ResKey1 REVISION_LABEL__COMMIT_NUMBER;
-	
+	/**
+	 * @en Initial revision
+	 * @tooltip The initial revision represents the state before the initial system setup. At
+	 *          initial revision, the data inventory of the application is considered empty.
+	 */
+	public static ResKey INITIAL_REVISION_LABEL;
+
+	/**
+	 * @en Revision {0} ({1})
+	 * @tooltip The historic system state at {1}, represented by the technical revision number {0}.
+	 */
+	public static ResKey2 REVISION_LABEL__REV_DATE;
+
 	static {
 		initConstants(I18NConstants.class);
 	}
--- com.top_logic/src/main/java/com/top_logic/layout/provider/label/RevisionLabelProvider.java
+++ com.top_logic/src/main/java/com/top_logic/layout/provider/label/RevisionLabelProvider.java
@@ -5,7 +5,10 @@
  */
 package com.top_logic.layout.provider.label;
 
+import com.top_logic.basic.format.configured.Formatter;
+import com.top_logic.basic.util.ResKey;
 import com.top_logic.knowledge.service.Revision;
+import com.top_logic.layout.AbstractResourceProvider;
 import com.top_logic.layout.LabelProvider;
 import com.top_logic.mig.html.HTMLFormatter;
 import com.top_logic.util.Resources;
@@ -15,7 +18,7 @@ import com.top_logic.util.Resources;
  * 
  * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
  */
-public class RevisionLabelProvider implements LabelProvider {
+public class RevisionLabelProvider extends AbstractResourceProvider {
 
 	@Override
 	public String getLabel(Object object) {
@@ -23,12 +26,32 @@ public class RevisionLabelProvider implements LabelProvider {
 		if (rev == null) {
 			return null;
 		}
-		if (rev.isCurrent()) {
-			return Resources.getInstance().getString(I18NConstants.CURRENT_REVISION_LABEL);
+
+		return Resources.getInstance().getString(resKey(rev));
+	}
+
+	@Override
+	public String getTooltip(Object object) {
+		Revision rev = (Revision) object;
+		if (rev == null) {
+			return null;
 		}
 
-		String commitNumberString = HTMLFormatter.getInstance().formatLong(rev.getCommitNumber());
-		return Resources.getInstance().getString(I18NConstants.REVISION_LABEL__COMMIT_NUMBER.fill(commitNumberString));
+		return Resources.getInstance().getString(resKey(rev).tooltipOptional());
+	}
+
+	private ResKey resKey(Revision rev) {
+		if (rev.isCurrent()) {
+			return I18NConstants.CURRENT_REVISION_LABEL;
+		}
+		if (rev.getCommitNumber() < Revision.FIRST_REV) {
+			return I18NConstants.INITIAL_REVISION_LABEL;
+		}
+
+		Formatter formatter = HTMLFormatter.getInstance();
+		String commitNumberString = formatter.formatLong(rev.getCommitNumber());
+		String date = formatter.formatShortDateTime(rev.resolveDate());
+		return I18NConstants.REVISION_LABEL__REV_DATE.fill(commitNumberString, date);
 	}
 
 }

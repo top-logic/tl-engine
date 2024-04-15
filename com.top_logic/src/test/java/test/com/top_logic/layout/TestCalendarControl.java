0ca6ad746db9c552cbbd73a00a59dd62eb41af31
Daniel Busche <daniel.busche@top-logic.com>
2024-04-15 10:39:45 +0200

    Ticket #27993: Normalize hour in an 24-hour clock.
    When the hour is normalized in the 12-hour clock, the test fails if it
    executed between 12:00 and 23:59.

--- com.top_logic/src/test/java/test/com/top_logic/layout/TestCalendarControl.java
+++ com.top_logic/src/test/java/test/com/top_logic/layout/TestCalendarControl.java
@@ -56,7 +56,7 @@ public class TestCalendarControl extends TestControl {
 		ComplexField dateField = FormFactory.newDateField("dateInputControl", cal.getTime(), false);
 
 		cal.set(Calendar.DAY_OF_MONTH, 5);
-		cal.set(Calendar.HOUR, 0);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
 		cal.set(Calendar.MINUTE, 0);
 		cal.set(Calendar.SECOND, 0);
 		cal.set(Calendar.MILLISECOND, 0);

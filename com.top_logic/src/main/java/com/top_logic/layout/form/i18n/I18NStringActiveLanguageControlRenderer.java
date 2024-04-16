5a064e66bb95bb16df83e1ff8b3b8f6a9fa2c795
Daniel Busche <daniel.busche@top-logic.com>
2024-04-16 14:02:16 +0200

    Ticket #27656: Migration for removal of I18NStringField#isMultiline().

--- com.top_logic/src/main/java/com/top_logic/layout/form/i18n/I18NStringActiveLanguageControlRenderer.java
+++ com.top_logic/src/main/java/com/top_logic/layout/form/i18n/I18NStringActiveLanguageControlRenderer.java
@@ -8,11 +8,12 @@ package com.top_logic.layout.form.i18n;
 import java.io.IOException;
 import java.util.Locale;
 
+import com.top_logic.basic.StringServices;
 import com.top_logic.basic.util.ResKey;
 import com.top_logic.basic.xml.TagWriter;
 import com.top_logic.layout.CompositeControl;
 import com.top_logic.layout.DisplayContext;
-import com.top_logic.layout.form.values.edit.editor.InternationalizationEditor;
+import com.top_logic.layout.form.format.WikiWrite;
 
 /**
  * Control renderer for {@link I18NStringField} fields that renders only the value of the current
@@ -24,7 +25,23 @@ public class I18NStringActiveLanguageControlRenderer extends I18NFieldActiveLang
 
 	/** Instance of this class. */
 	public static final I18NStringActiveLanguageControlRenderer INSTANCE =
-		new I18NStringActiveLanguageControlRenderer();
+		new I18NStringActiveLanguageControlRenderer(false);
+
+	/** Instance of this class which renders the value multiline. */
+	public static final I18NStringActiveLanguageControlRenderer MULTILINE_INSTANCE =
+		new I18NStringActiveLanguageControlRenderer(true);
+
+	private boolean _multiline;
+
+	/**
+	 * Creates a {@link I18NStringActiveLanguageControlRenderer}.
+	 *
+	 * @param multiline
+	 *        Whether the field value must be rendered multiline.
+	 */
+	protected I18NStringActiveLanguageControlRenderer(boolean multiline) {
+		_multiline = multiline;
+	}
 
 	@Override
 	protected String getControlTag(CompositeControl control) {
@@ -34,8 +51,26 @@ public class I18NStringActiveLanguageControlRenderer extends I18NFieldActiveLang
 	@Override
 	protected void writeFieldValue(DisplayContext context, TagWriter out, I18NField<?, ?, ?> i18nField)
 			throws IOException {
-		boolean multiline = ((I18NStringField) i18nField).isMultiline();
-		InternationalizationEditor.writeResKey(context, out, (ResKey) i18nField.getValue(), multiline);
+		writeResKey(context, out, (ResKey) i18nField.getValue());
+	}
+
+	/**
+	 * Renders the translation for the current language of the given {@link ResKey}.
+	 */
+	private void writeResKey(DisplayContext context, TagWriter out, ResKey resKey)
+			throws IOException {
+		if (resKey == null) {
+			return;
+		}
+		String translation = context.getResources().getString(resKey, null);
+		if (StringServices.isEmpty(translation)) {
+			return;
+		}
+		if (_multiline) {
+			WikiWrite.wikiWrite(out, translation);
+		} else {
+			out.writeText(translation);
+		}
 	}
 
 }

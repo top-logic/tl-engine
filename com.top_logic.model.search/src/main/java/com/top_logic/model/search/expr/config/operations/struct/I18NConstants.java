0fab9a677cdfd8faf6cc7b7e7623b2f1108ea347
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-05-03 10:27:36 +0200

    Ticket #28022: Allow to create configurations from JSON objects.

--- com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/config/operations/struct/I18NConstants.java
+++ com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/config/operations/struct/I18NConstants.java
@@ -5,6 +5,7 @@
  */
 package com.top_logic.model.search.expr.config.operations.struct;
 
+import com.top_logic.basic.util.ResKey1;
 import com.top_logic.basic.util.ResKey2;
 import com.top_logic.basic.util.ResKey3;
 import com.top_logic.basic.util.ResKey4;
@@ -21,6 +22,21 @@ public class I18NConstants extends I18NConstantsBase {
 	public static ResKey3 ERROR_ARGUMENT_COUNT_MISMATCH__EXPECTED_ACTUAL_EXPR;
 
 	public static ResKey4 ERROR_DUPLICATE_KEYS__KEY_IDX1_IDX2_EXPR;
+
+	/**
+	 * @en No such class: {0}
+	 */
+	public static ResKey1 ERROR_NO_SUCH_CLASS__NAME;
+
+	/**
+	 * @en Failed to instantiate configuration "{0}": {1}
+	 */
+	public static ResKey2 ERROR_CREATING_CONFIGURATION__TYPE_MSG;
+
+	/**
+	 * @en Failed parse configuration value "{1}" for property "{0}": {2}
+	 */
+	public static ResKey3 ERROR_PARSING_CONFIGURATION_VALUE__PROP_VAL_MSG;
 
 	static {
 		initConstants(I18NConstants.class);

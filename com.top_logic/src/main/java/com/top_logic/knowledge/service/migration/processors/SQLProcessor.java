0286e6496be9c9305128d954656ac8cbf3e61bfd
Daniel Busche <daniel.busche@top-logic.com>
2024-05-14 13:14:00 +0200

    Ticket #27999: Made SQLProcessor available in tl-core.

--- com.top_logic/src/main/java/com/top_logic/knowledge/service/migration/processors/SQLProcessor.java
+++ com.top_logic/src/main/java/com/top_logic/knowledge/service/migration/processors/SQLProcessor.java
@@ -3,7 +3,7 @@
  * 
  * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
  */
-package com.top_logic.element.model.migration;
+package com.top_logic.knowledge.service.migration.processors;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,7 +21,7 @@ import com.top_logic.basic.sql.PooledConnection;
 /**
  * Utility for directly executing SQL generated with {@link SQLFactory}.
  */
-class SQLProcessor {
+public class SQLProcessor {
 
 	private PooledConnection _connection;
 

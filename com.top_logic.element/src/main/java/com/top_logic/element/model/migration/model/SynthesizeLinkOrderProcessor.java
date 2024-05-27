5f529b28f8f61a4329547b58626cbcbb41f703f8
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-05-27 13:32:14 +0200

    Ticket #28036: Generalized SynthesizeLinkOrderProcessor.
    Allow to synthesize order information from arbitrary columns of the
    child object.

--- com.top_logic.element/src/main/java/com/top_logic/element/model/migration/model/SynthesizeLinkOrderProcessor.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/migration/model/SynthesizeLinkOrderProcessor.java
@@ -9,9 +9,15 @@ import static com.top_logic.basic.db.sql.SQLFactory.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import com.top_logic.basic.CalledByReflection;
 import com.top_logic.basic.Log;
+import com.top_logic.basic.col.ComparableComparator;
 import com.top_logic.basic.config.AbstractConfiguredInstance;
 import com.top_logic.basic.config.InstantiationContext;
 import com.top_logic.basic.config.PolymorphicConfiguration;
@@ -63,11 +69,23 @@ public class SynthesizeLinkOrderProcessor extends AbstractConfiguredInstance<Syn
 		String getSourceTable();
 
 		/**
-		 * The order column to fill.
+		 * The order column in the link table to fill.
 		 */
 		@StringDefault(KBBasedMetaAttribute.OWNER_REF_ORDER_ATTR)
 		@Name("order-column")
 		String getOrderColumn();
+
+		/**
+		 * Reference to the scope/parent object.
+		 * 
+		 * <p>
+		 * An order is established by sorting all values of the {@link #getSourceOrderAttribute()}
+		 * for all links originating from the same scope object.
+		 * </p>
+		 */
+		@StringDefault(DBKnowledgeAssociation.REFERENCE_SOURCE_NAME)
+		@Name("scope-ref")
+		String getScopeRef();
 
 		/**
 		 * The reference to the object from which the order information is taken.
@@ -84,10 +102,9 @@ public class SynthesizeLinkOrderProcessor extends AbstractConfiguredInstance<Syn
 		String getSourceOrderAttribute();
 
 		/**
-		 * Factor to multiply the extracted order information with to get the internal ordering
-		 * value.
+		 * Factor to multiply the index with to get the internal ordering value.
 		 */
-		@IntDefault(1)
+		@IntDefault(1024)
 		int getFactor();
 	}
 
@@ -116,6 +133,7 @@ public class SynthesizeLinkOrderProcessor extends AbstractConfiguredInstance<Syn
 			MOAttribute sourceOrderAttr = sourceSpec.getAttribute(config.getSourceOrderAttribute());
 			
 			MOClass linkTable = (MOClass) repository.getType(config.getLinkTable());
+			MOReference scopeRef = (MOReference) linkTable.getAttribute(config.getScopeRef());
 			MOReference sourceRef = (MOReference) linkTable.getAttribute(config.getSourceRef());
 			MOAttribute orderAttr = linkTable.getAttribute(config.getOrderColumn());
 			
@@ -134,34 +152,36 @@ public class SynthesizeLinkOrderProcessor extends AbstractConfiguredInstance<Syn
 				}
 				
 				boolean hasBranches = util.hasBranches();
+				String link = "l";
+				String source = "s";
 				CompiledStatement select = query(
 					select(
 						Util.listWithoutNull(
-							util.branchColumnDefOrNull("l"),
-							columnDef(column("l", BasicTypes.IDENTIFIER_DB_NAME)),
-							columnDef(column("l", BasicTypes.REV_MAX_DB_NAME)),
-							columnDef(column("s", sourceOrderAttr.getDbMapping()[0]
-								.getDBName()))
+							util.branchColumnDefOrNull(link),
+							columnDef(column(link, BasicTypes.IDENTIFIER_DB_NAME)),
+							columnDef(column(link, BasicTypes.REV_MAX_DB_NAME)),
+							columnDef(column(link, scopeRef.getColumn(ReferencePart.name).getDBName())),
+							columnDef(column(source, sourceOrderAttr.getDbMapping()[0].getDBName()))
 						),
 						leftJoin(
-							table(linkTable.getDBMapping().getDBName(), "l"), 
-							table(sourceTable.getDBMapping().getDBName(), "s"), 
+							table(linkTable.getDBMapping().getDBName(), link), 
+							table(sourceTable.getDBMapping().getDBName(), source), 
 							and(
 								hasBranches ?
-									eqSQL(util.branchColumnRef("l"), util.branchColumnRef("s"))
+									eqSQL(util.branchColumnRef(link), util.branchColumnRef(source))
 									: literalTrueLogical(),
-								eqSQL(column("l", sourceRef.getColumn(ReferencePart.name).getDBName()), column("s", BasicTypes.IDENTIFIER_DB_NAME)),
-								le(column("l", BasicTypes.REV_MAX_DB_NAME), column("s", BasicTypes.REV_MAX_DB_NAME)),
-								ge(column("l", BasicTypes.REV_MAX_DB_NAME), column("s", BasicTypes.REV_MIN_DB_NAME))
+								eqSQL(column(link, sourceRef.getColumn(ReferencePart.name).getDBName()), column(source, BasicTypes.IDENTIFIER_DB_NAME)),
+								le(column(link, BasicTypes.REV_MAX_DB_NAME), column(source, BasicTypes.REV_MAX_DB_NAME)),
+								ge(column(link, BasicTypes.REV_MAX_DB_NAME), column(source, BasicTypes.REV_MIN_DB_NAME))
 							)
 						),
 						eqSQL(
 							literal(DBType.STRING, sourceTable.getName()), 
-							column("l", sourceRef.getColumn(ReferencePart.type).getDBName())),
+							column(link, sourceRef.getColumn(ReferencePart.type).getDBName())),
 						Util.listWithoutNull(
-							util.branchOrderOrNull("l"),
-							order(false, column("l", BasicTypes.IDENTIFIER_DB_NAME)),
-							order(false, column("l", BasicTypes.REV_MAX_DB_NAME)))
+							util.branchOrderOrNull(link),
+							order(false, column(link, scopeRef.getColumn(ReferencePart.name).getDBName()))
+						)
 					)
 				).toSql(connection.getSQLDialect());
 								
@@ -171,6 +191,7 @@ public class SynthesizeLinkOrderProcessor extends AbstractConfiguredInstance<Syn
 							util.branchColumnDefOrNull(),
 							columnDef(column(BasicTypes.IDENTIFIER_DB_NAME)),
 							columnDef(column(BasicTypes.REV_MAX_DB_NAME)),
+							columnDef(column(scopeRef.getColumn(ReferencePart.name).getDBName())),
 							columnDef(column(orderAttr.getDbMapping()[0].getDBName()))
 						),
 						table(linkTable.getDBMapping().getDBName()),
@@ -179,8 +200,8 @@ public class SynthesizeLinkOrderProcessor extends AbstractConfiguredInstance<Syn
 							column(sourceRef.getColumn(ReferencePart.type).getDBName())),
 						Util.listWithoutNull(
 							util.branchOrderOrNull(),
-							order(false, column(BasicTypes.IDENTIFIER_DB_NAME)),
-							order(false, column(BasicTypes.REV_MAX_DB_NAME)))
+							order(false, column(scopeRef.getColumn(ReferencePart.name).getDBName()))
+						)
 					)
 				).toSql(connection.getSQLDialect());
 
@@ -190,38 +211,23 @@ public class SynthesizeLinkOrderProcessor extends AbstractConfiguredInstance<Syn
 				int factor = getConfig().getFactor();
 				int idIndex = util.getBranchIndexInc() + 1;
 				int revIndex = util.getBranchIndexInc() + 2;
-				int orderIndex = util.getBranchIndexInc() + 3;
+				int scopeIndex = util.getBranchIndexInc() + 3;
+				int orderIndex = util.getBranchIndexInc() + 4;
 				try (ResultSet cursor = update.executeQuery(connection)) {
-					try (ResultSet result = select.executeQuery(connection)) {
+					try (OrderGenerator generator = new OrderGenerator(util, select, factor, connection)) {
 						while (cursor.next()) {
-							if (!result.next()) {
-								log.info("Inconsistent line count.", Log.WARN);
+							long branch = hasBranches ? cursor.getLong(1) : TLContext.TRUNK_ID;
+							long id = cursor.getLong(idIndex);
+							long revMax = cursor.getLong(revIndex);
+							long scopeId = cursor.getLong(scopeIndex);
+
+							if (!generator.next(branch, scopeId)) {
+								log.info("Inconsistent line count, missing order for branch=" + branch + ", id=" + id
+									+ ", revMax=" + revMax, Log.WARN);
 								break;
 							}
 
-							long branch1 = hasBranches ? cursor.getLong(1) : TLContext.TRUNK_ID;
-							long id1 = cursor.getLong(idIndex);
-							long rev1 = cursor.getLong(revIndex);
-
-							long branch2 = hasBranches ? result.getLong(1) : TLContext.TRUNK_ID;
-							long id2 = result.getLong(idIndex);
-							long rev2 = result.getLong(revIndex);
-							int order = result.getInt(orderIndex);
-
-							if (branch1 != branch2 || id1 != id2 || rev1 != rev2) {
-								log.info("Inconsistent lines: " + branch1 + ", " + id1 + ", " + rev1 + " vs. " + branch2
-									+ ", " + id2 + ", " + rev2, Log.WARN);
-								break;
-							}
-
-							long newOrder = order * factor;
-							if (newOrder > Integer.MAX_VALUE) {
-								log.info("Overflow: " + branch1 + ", " + id1 + ", " + rev1 + ": " + newOrder, Log.WARN);
-
-								newOrder = Integer.MAX_VALUE;
-							}
-
-							cursor.updateInt(orderIndex, (int) newOrder);
+							cursor.updateInt(orderIndex, generator.getSortOrder(branch, id, revMax));
 							cursor.updateRow();
 
 							cnt++;
@@ -236,4 +242,164 @@ public class SynthesizeLinkOrderProcessor extends AbstractConfiguredInstance<Syn
 		}
 	}
 
+	static class OrderGenerator implements AutoCloseable {
+		private ResultSet _result;
+
+		private int _scopeIndex;
+
+		private int _orderIndex;
+
+		private boolean _hasBranches;
+
+		private boolean _continuation;
+
+		private List<Value> _values;
+
+		private Map<Value, Value> _valueById;
+
+		private int _factor;
+
+		private int _idIndex;
+
+		private int _revIndex;
+
+		/**
+		 * Creates a {@link OrderGenerator}.
+		 */
+		public OrderGenerator(Util util, CompiledStatement select, int factor, PooledConnection connection)
+				throws SQLException {
+			_factor = factor;
+
+			_hasBranches = util.hasBranches();
+			_idIndex = util.getBranchIndexInc() + 1;
+			_revIndex = util.getBranchIndexInc() + 2;
+			_scopeIndex = util.getBranchIndexInc() + 3;
+			_orderIndex = util.getBranchIndexInc() + 4;
+
+			_result = select.executeQuery(connection);
+			_valueById = new HashMap<>();
+			_values = new ArrayList<>();
+		}
+
+		public int getSortOrder(long branch, long id, long revMax) {
+			Value value = _valueById.get(new Value(branch, id, revMax));
+			if (value == null) {
+				throw new IllegalArgumentException(
+					"No information available for link with branch=" + branch + ", id=" + id + ", revMax=" + revMax);
+			}
+			return value.getIndex() * _factor;
+		}
+
+		public boolean next(long branch, long scopeId) throws SQLException {
+			if (!_values.isEmpty() && _values.get(0).sameScope(branch, scopeId)) {
+				// Same batch.
+				return true;
+			}
+			_values.clear();
+			_valueById.clear();
+			boolean ok = readValues();
+			if (ok) {
+				_values.sort(Value::compareOrder);
+				for (int n = 0, cnt = _values.size(); n < cnt; n++) {
+					_values.get(n).initIndex(n);
+				}
+			}
+			return ok;
+		}
+
+		private boolean readValues() throws SQLException {
+			Value lastValue = null;
+			while (_continuation || _result.next()) {
+				long branch = _hasBranches ? _result.getLong(1) : TLContext.TRUNK_ID;
+				long id = _result.getLong(_idIndex);
+				long revMax = _result.getLong(_revIndex);
+				long scope = _result.getLong(_scopeIndex);
+				Object order = _result.getObject(_orderIndex);
+
+				Value nextValue = new Value(branch, id, revMax, scope, order);
+
+				if (lastValue == null || lastValue.sameScope(nextValue)) {
+					_values.add(nextValue);
+					_valueById.put(nextValue, nextValue);
+					_continuation = false;
+					lastValue = nextValue;
+				} else {
+					_continuation = true;
+					return true;
+				}
+			}
+			_continuation = false;
+			return !_values.isEmpty();
+		}
+
+		@Override
+		public void close() throws SQLException {
+			_result.close();
+		}
+
+		private static class Value {
+			private long _branch;
+
+			private long _scope;
+
+			private Object _order;
+
+			private int _index;
+
+			private long _id;
+
+			private long _revMax;
+
+			public Value(long branch, long id, long revMax, long scope, Object order) {
+				this(branch, id, revMax);
+				_scope = scope;
+				_order = order;
+			}
+
+			public Value(long branch, long id, long revMax) {
+				_branch = branch;
+				_id = id;
+				_revMax = revMax;
+			}
+
+			public void initIndex(int n) {
+				_index = n;
+			}
+
+			public int getIndex() {
+				return _index;
+			}
+
+			public boolean sameScope(Value other) {
+				long branch = other._branch;
+				long scope = other._scope;
+				return sameScope(branch, scope);
+			}
+
+			public boolean sameScope(long branch, long scope) {
+				return _branch == branch && _scope == scope;
+			}
+
+			public int compareOrder(Value other) {
+				return ComparableComparator.INSTANCE.compare(_order, other._order);
+			}
+
+			@Override
+			public int hashCode() {
+				return Objects.hash(_branch, _id, _revMax);
+			}
+
+			@Override
+			public boolean equals(Object obj) {
+				if (this == obj)
+					return true;
+				if (obj == null)
+					return false;
+				if (getClass() != obj.getClass())
+					return false;
+				Value other = (Value) obj;
+				return _branch == other._branch && _id == other._id && _revMax == other._revMax;
+			}
+		}
+	}
 }

51e34df1196cea8e6dd9b2cb749e5a7dc7272b63
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-06-03 16:17:24 +0200

    Ticket #28036: Fixed copyright header.

--- com.top_logic.element/src/main/java/com/top_logic/element/model/migration/model/AddLinksProcessor.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/migration/model/AddLinksProcessor.java
@@ -1,7 +1,7 @@
 /*
  * SPDX-FileCopyrightText: 2024 (c) Business Operation Systems GmbH <info@top-logic.com>
- *
- * SPDX-License-Identifier: LicenseRef-BOS-TopLogic-1.0
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
  */
 package com.top_logic.element.model.migration.model;
 

9b824cae26e3536ab7d5ca49fee634ad73c623e0
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-06-11 16:50:32 +0200

    Ticket #28036: Added processor to move objects between tables.

---
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/migration/model/refactor/MoveObjectsProcessor.java
@@ -0,0 +1,169 @@
+/*
+ * SPDX-FileCopyrightText: 2024 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.element.model.migration.model.refactor;
+
+import static com.top_logic.basic.db.sql.SQLFactory.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.top_logic.basic.CalledByReflection;
+import com.top_logic.basic.Log;
+import com.top_logic.basic.config.AbstractConfiguredInstance;
+import com.top_logic.basic.config.InstantiationContext;
+import com.top_logic.basic.config.PolymorphicConfiguration;
+import com.top_logic.basic.config.annotation.Mandatory;
+import com.top_logic.basic.config.annotation.Name;
+import com.top_logic.basic.config.annotation.TagName;
+import com.top_logic.basic.db.sql.CompiledStatement;
+import com.top_logic.basic.db.sql.SQLColumnDefinition;
+import com.top_logic.basic.sql.DBType;
+import com.top_logic.basic.sql.PooledConnection;
+import com.top_logic.dob.MOAttribute;
+import com.top_logic.dob.meta.BasicTypes;
+import com.top_logic.dob.meta.MOReference;
+import com.top_logic.dob.meta.MOReference.ReferencePart;
+import com.top_logic.dob.meta.MORepository;
+import com.top_logic.dob.meta.MOStructure;
+import com.top_logic.dob.sql.DBAttribute;
+import com.top_logic.knowledge.service.db2.AbstractFlexDataManager;
+import com.top_logic.knowledge.service.db2.PersistentObject;
+import com.top_logic.knowledge.service.migration.MigrationContext;
+import com.top_logic.knowledge.service.migration.MigrationProcessor;
+import com.top_logic.model.migration.Util;
+import com.top_logic.model.migration.data.MigrationException;
+import com.top_logic.model.migration.data.QualifiedTypeName;
+import com.top_logic.model.migration.data.Type;
+
+/**
+ * {@link MigrationProcessor} moving objects from one table to another.
+ */
+public class MoveObjectsProcessor extends AbstractConfiguredInstance<MoveObjectsProcessor.Config<?>>
+		implements MigrationProcessor {
+
+	/**
+	 * Configuration options for {@link MoveObjectsProcessor}.
+	 */
+	@TagName("move-objects")
+	public interface Config<I extends MoveObjectsProcessor> extends PolymorphicConfiguration<I> {
+		/**
+		 * Name of the table to take the objects from.
+		 */
+		@Name("source-table")
+		String getSourceTable();
+
+		/**
+		 * Name of the table to move the objects to.
+		 */
+		@Name("dest-table")
+		String getDestTable();
+
+		/**
+		 * The type of objects to move.
+		 */
+		@Mandatory
+		@Name("type")
+		QualifiedTypeName getType();
+	}
+
+	/**
+	 * Creates a {@link MoveObjectsProcessor} from configuration.
+	 * 
+	 * @param context
+	 *        The context for instantiating sub configurations.
+	 * @param config
+	 *        The configuration.
+	 */
+	@CalledByReflection
+	public MoveObjectsProcessor(InstantiationContext context, Config<?> config) {
+		super(context, config);
+	}
+
+	@Override
+	public void doMigration(MigrationContext context, Log log, PooledConnection connection) {
+		String sourceTableName = getConfig().getSourceTable();
+		String destTableName = getConfig().getDestTable();
+		log.info(
+			"Moving objects of type '" + getConfig().getType().getName() + "' from table '" + sourceTableName + "' to '"
+			+ destTableName + "'. ");
+
+		MORepository repository = context.getPersistentRepository();
+		MOStructure sourceTable = (MOStructure) repository.getMetaObject(sourceTableName);
+		MOStructure destTable = (MOStructure) repository.getMetaObject(destTableName);
+		MOReference typeRef = (MOReference) sourceTable.getAttribute(PersistentObject.TYPE_REF);
+
+		Util util = context.getSQLUtils();
+		try {
+			Type type = util.getTLTypeOrFail(connection, getConfig().getType());
+
+			List<String> columns = new ArrayList<>();
+			List<SQLColumnDefinition> columnDefs = new ArrayList<>();
+			for (MOAttribute attr : sourceTable.getAttributes()) {
+				if (destTable.getAttributeOrNull(attr.getName()) == null) {
+					continue;
+				}
+
+				for (DBAttribute column : attr.getDbMapping()) {
+					columns.add(column.getDBName());
+					columnDefs.add(columnDef(column.getDBName()));
+				}
+			}
+
+			CompiledStatement copy = query(
+				insert(
+					table(destTable.getDBMapping().getDBName()),
+					columns,
+					select(
+						columnDefs,
+						table(sourceTable.getDBMapping().getDBName()),
+						eqSQL(column(typeRef.getColumn(ReferencePart.name).getDBName()),
+							literal(DBType.ID, type.getID())))))
+								.toSql(connection.getSQLDialect());
+
+			int cntCopy = copy.executeUpdate(connection);
+			log.info("Copied " + cntCopy + " rows from table '" + sourceTableName + "' to '" + destTableName + "'.");
+
+			CompiledStatement update = query(
+				update(
+					table(AbstractFlexDataManager.FLEX_DATA_DB_NAME),
+					and(
+						eqSQL(
+							column(AbstractFlexDataManager.TYPE_DBNAME),
+							literal(DBType.STRING, sourceTableName)),
+						inSetSelect(
+							column(AbstractFlexDataManager.IDENTIFIER_DBNAME),
+							select(
+								columns(columnDef(BasicTypes.IDENTIFIER_DB_NAME)),
+								table(sourceTable.getDBMapping().getDBName()),
+								eqSQL(
+									column(typeRef.getColumn(ReferencePart.name).getDBName()),
+									literal(DBType.ID, type.getID()))))),
+
+					columnNames(AbstractFlexDataManager.TYPE_DBNAME),
+					expressions(literal(DBType.STRING, destTableName)))).toSql(connection.getSQLDialect());
+
+			int cntUpdate = update.executeUpdate(connection);
+			log.info("Migrated " + cntUpdate + " flex attributes from table '" + sourceTableName + "' to '" + destTableName + "'.");
+			
+			CompiledStatement delete = query(
+				delete(
+					table(sourceTable.getDBMapping().getDBName()),
+					eqSQL(column(typeRef.getColumn(ReferencePart.name).getDBName()),
+						literal(DBType.ID, type.getID()))))
+				.toSql(connection.getSQLDialect());
+
+			int cntDelete = delete.executeUpdate(connection);
+			log.info("Deleted " + cntDelete + " rows from table '" + sourceTableName + ".");
+		} catch (SQLException | MigrationException ex) {
+			log.error(
+				"Failed to move objects of type '" + getConfig().getType().getName() + "' from '"
+					+ sourceTableName + "' to '" + destTableName + "': " + ex.getMessage(),
+				ex);
+		}
+	}
+
+}

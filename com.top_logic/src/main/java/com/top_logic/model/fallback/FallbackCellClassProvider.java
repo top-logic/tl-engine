f37dd2641ac4df6e825cc47891dc6c4175564950
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-07-19 17:20:46 +0200

    Ticket #27216: Set CSS classes to explicit and fallback values.

---
+++ com.top_logic/src/main/java/com/top_logic/model/fallback/FallbackCellClassProvider.java
@@ -0,0 +1,50 @@
+/*
+ * SPDX-FileCopyrightText: 2024 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.model.fallback;
+
+import com.top_logic.basic.util.Utils;
+import com.top_logic.layout.form.FormMember;
+import com.top_logic.layout.table.AbstractCellClassProvider;
+import com.top_logic.layout.table.CellClassProvider;
+import com.top_logic.layout.table.TableRenderer.Cell;
+import com.top_logic.model.TLObject;
+import com.top_logic.model.TLStructuredTypePart;
+
+/**
+ * {@link CellClassProvider} assigning CSS class values to a table cell depending on the explicit
+ * assignment status of the displayed value.
+ */
+public class FallbackCellClassProvider extends AbstractCellClassProvider {
+
+	private TLStructuredTypePart _attribute;
+
+	private StorageWithFallback _fallbackStorage;
+
+	/**
+	 * Creates a {@link FallbackCellClassProvider}.
+	 */
+	public FallbackCellClassProvider(TLStructuredTypePart attribute, StorageWithFallback fallbackStorage) {
+		_attribute = attribute;
+		_fallbackStorage = fallbackStorage;
+	}
+
+	@Override
+	public String getCellClass(Cell cell) {
+		if (cell.cellExists() && !(cell.getValue() instanceof FormMember)) {
+			Object row = cell.getRowObject();
+			if (row instanceof TLObject obj) {
+				Object explicitValue = _fallbackStorage.getExplicitValue(obj, _attribute);
+				if (Utils.isEmpty(explicitValue)) {
+					return UpdateFallbackCssClass.CSS_FALLBACK;
+				} else {
+					return UpdateFallbackCssClass.CSS_EXPLICIT;
+				}
+			}
+		}
+		return null;
+	}
+
+}

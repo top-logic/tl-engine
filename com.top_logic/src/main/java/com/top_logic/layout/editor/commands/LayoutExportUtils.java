beeba9ab7dad7d59eaddccd10ecf024de3e6d168
Sven Förster <sven.foerster@top-logic.com>
2024-07-29 19:01:56 +0200

    Ticket #28180: Use unique names for the export of database layouts into the filesystem.

--- com.top_logic/src/main/java/com/top_logic/layout/editor/commands/LayoutExportUtils.java
+++ com.top_logic/src/main/java/com/top_logic/layout/editor/commands/LayoutExportUtils.java
@@ -5,16 +5,14 @@
  */
 package com.top_logic.layout.editor.commands;
 
-import java.io.File;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.Set;
 
+import com.top_logic.basic.FileManager;
 import com.top_logic.basic.StringServices;
 import com.top_logic.basic.io.FileUtilities;
-import com.top_logic.basic.tooling.Workspace;
 import com.top_logic.basic.util.ResKey;
 import com.top_logic.basic.util.ResourcesModule;
 import com.top_logic.knowledge.wrap.person.Person;
@@ -22,6 +20,7 @@ import com.top_logic.layout.component.TabComponent;
 import com.top_logic.layout.processor.LayoutModelConstants;
 import com.top_logic.layout.tabbar.TabInfo.TabConfig;
 import com.top_logic.mig.html.layout.LayoutComponent;
+import com.top_logic.mig.html.layout.LayoutConstants;
 import com.top_logic.mig.html.layout.LayoutStorage;
 import com.top_logic.mig.html.layout.LayoutUtils;
 import com.top_logic.mig.html.layout.tiles.ContextTileComponent;
@@ -41,24 +40,6 @@ import com.top_logic.util.TLResKeyUtil;
 public class LayoutExportUtils {
 
 	/**
-	 * Layout scope of the given key. If no scope exists then use the project root directory as
-	 * fallback scope.
-	 */
-	public static String getRootLayoutScope(String layoutKey) {
-		int lastIndexOf = layoutKey.lastIndexOf(FileUtilities.PATH_SEPARATOR);
-
-		if (lastIndexOf != -1) {
-			return layoutKey.substring(0, lastIndexOf + 1);
-		} else {
-			return getProjectRootDirectory();
-		}
-	}
-
-	private static String getProjectRootDirectory() {
-		return Workspace.topLevelProjectDirectory().getName() + FileUtilities.PATH_SEPARATOR;
-	}
-
-	/**
 	 * Returns the direct scope (only respecting tabs and dialogs) for the given component.
 	 */
 	public static String getLayoutScope(LayoutComponent component) {
@@ -72,46 +53,14 @@ public class LayoutExportUtils {
 	}
 
 	/**
-	 * Similar to {@link #getLayoutScope(LayoutComponent)} but returns an unique scope.
+	 * Layout resource name for a {@link LayoutComponent component}.
 	 */
-	public static String getUniqueLayoutScope(Collection<String> existingScopes, String parentScope, String scope) {
-		if (StringServices.isEmpty(scope)) {
-			return StringServices.EMPTY_STRING;
-		} else {
-			return getUniqueScopeInternal(existingScopes, parentScope, scope);
-		}
+	public static String getLayoutName(LayoutComponent component) {
+		return getBasename(component) + LayoutModelConstants.LAYOUT_XML_FILE_SUFFIX;
 	}
 
-	/**
-	 * Returns an unique filename for the given scope.
-	 */
-	public static String getUniqueFilename(Collection<String> existingFilenames, String scope, String fullScope,
-			LayoutComponent component) {
-		if (!StringServices.isEmpty(scope)) {
-			return getFilename(component) + LayoutModelConstants.LAYOUT_XML_FILE_SUFFIX;
-		} else {
-			return getUniqueFilename(existingFilenames, getFilename(component), fullScope);
-		}
-	}
-
-	private static String getUniqueFilename(Collection<String> filenames, String filename, String fullScope) {
-		String uniqueFilename = filename + LayoutModelConstants.LAYOUT_XML_FILE_SUFFIX;
-
-		int counter = 2;
-
-		while (isUniqueFilename(filenames, fullScope, uniqueFilename)) {
-			uniqueFilename = filename + counter + LayoutModelConstants.LAYOUT_XML_FILE_SUFFIX;
-
-			counter++;
-		}
-
-		return uniqueFilename;
-	}
-
-	private static String getFilename(LayoutComponent component) {
-		String filename = getTitleName(component) + " " + getLocalName(component);
-
-		return getCompatibleFilenamePart(filename);
+	private static String getBasename(LayoutComponent component) {
+		return getCompatibleFilenamePart(getTitleName(component) + " " + getLocalName(component));
 	}
 
 	private static String getLocalName(LayoutComponent component) {
@@ -148,12 +97,6 @@ public class LayoutExportUtils {
 		return component instanceof TabComponent;
 	}
 
-	private static boolean isUniqueFilename(Collection<String> filenames, String layoutScope, String filename) {
-		String path = layoutScope + filename;
-
-		return filenames.contains(path) || isInvalidFile(path);
-	}
-
 	/**
 	 * Returns the complete layout scope.
 	 */
@@ -165,26 +108,17 @@ public class LayoutExportUtils {
 		return parentScope + childScope + FileUtilities.PATH_SEPARATOR;
 	}
 
-	private static boolean isInvalidFile(String layoutPath) {
-		File layoutsDirectory = LayoutUtils.getCurrentTopLayoutBaseDirectory();
-
-		File file = new File(layoutsDirectory, layoutPath);
-
-		return file.exists();
-	}
-
-	private static String getUniqueScopeInternal(Collection<String> existingScopes, String parentScope, String scope) {
-		String uniqueChildScope = scope;
-
-		int counter = 2;
-
-		while (existingScopes.contains(uniqueChildScope) || isInvalidFile(parentScope + uniqueChildScope)) {
-			uniqueChildScope = scope + counter;
-
-			counter++;
-		}
-
-		return uniqueChildScope;
+	/**
+	 * Whether a resource with the given name exists in the application layout directory.
+	 * 
+	 * @param name
+	 *        Resource name
+	 * 
+	 * @see FileManager#exists(String)
+	 * @see LayoutConstants#LAYOUT_BASE_DIRECTORY
+	 */
+	public static boolean existsLayoutInFilesystem(String name) {
+		return FileManager.getInstance().exists(LayoutConstants.LAYOUT_BASE_RESOURCE + "/" + name);
 	}
 
 	private static void appendTabLayoutScope(StringBuilder layoutScopeBuilder, LayoutComponent component) {

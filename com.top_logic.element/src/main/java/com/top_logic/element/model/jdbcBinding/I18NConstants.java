23fbeedec8abc2a1f67b7a368993b8008a203cab
Jan Stolzenburg <jan.stolzenburg@top-logic.com>
2024-09-04 16:00:22 +0200

    Ticket #27514: Add a generator for a `meta.xml` for a `TLModule`.

---
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/jdbcBinding/I18NConstants.java
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: 2024 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.element.model.jdbcBinding;
+
+import com.top_logic.basic.util.ResKey;
+import com.top_logic.layout.I18NConstantsBase;
+import com.top_logic.model.TLModule;
+
+/**
+ * I18N constants for this package.
+ * 
+ * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
+ */
+public class I18NConstants extends I18NConstantsBase {
+
+	/** @en No {@link TLModule} selected. */
+	public static ResKey NO_TL_MODULE_SELECTED;
+
+	static {
+		initConstants(I18NConstants.class);
+	}
+}

d25b8e70b55c1581da8b905e6d974876e15ad8fe
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-09-09 13:30:48 +0200

    Ticket #28221: OMG - Tomcat 11 fails to access a part multipe times.

--- com.top_logic/src/main/java/com/top_logic/knowledge/service/binary/FileItemBinaryData.java
+++ com.top_logic/src/main/java/com/top_logic/knowledge/service/binary/FileItemBinaryData.java
@@ -5,54 +5,65 @@
  */
 package com.top_logic.knowledge.service.binary;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import jakarta.servlet.http.Part;
 
+import com.top_logic.basic.Settings;
 import com.top_logic.basic.io.binary.AbstractBinaryData;
+import com.top_logic.basic.io.binary.BinaryData;
 
 /**
- * Wrap a {@link Part} as BinaryData to avoid unnecessary copying of Uploaded Data.
- * 
- * This class is not aware of changes of the underlying FileItem and therefore is not immutable.
- * 
- * @author <a href="mailto:kha@top-logic.com">kha</a>
+ * Wraps a {@link Part} as {@link BinaryData}.
  */
 public class FileItemBinaryData extends AbstractBinaryData {
 
     /** The Object wrapped by this class */
-	private final Part fileItem;
+	private final Part _part;
 
-	public FileItemBinaryData(Part aFileItem) {
-        this.fileItem = aFileItem;
+	private File _tempFile;
+
+	/**
+	 * Creates a {@link FileItemBinaryData}.
+	 *
+	 * @param part
+	 *        The {@link Part} of a multi-part request to wrap.
+	 */
+	public FileItemBinaryData(Part part) {
+		this._part = part;
     }
     
-    /**
-     * @see com.top_logic.basic.io.binary.BinaryData#getSize()
-     */
     @Override
 	public long getSize() {
-        return fileItem.getSize();
+		return _part.getSize();
     }
     
-    /**
-     * @see com.top_logic.basic.io.binary.BinaryData#getStream()
-     */
     @Override
 	public InputStream getStream() throws IOException {
-        return fileItem.getInputStream();
+		// Note: In Tomcat 11, accessing the input stream of an uploaded part seems to be possible
+		// only once. Since the framework may access the data of a binary data multiple times, the
+		// contents must be copied to a safe location before using it.
+		if (_tempFile == null) {
+			File tempDir = Settings.getInstance().getTempDir();
+			_tempFile = File.createTempFile("upload", ".data", tempDir);
+			_part.write(_tempFile.getAbsolutePath());
+		}
+
+		return new FileInputStream(_tempFile);
     }
     
 	@Override
 	public String getName() {
-		String submittedFileName = fileItem.getSubmittedFileName();
-		return submittedFileName != null ? submittedFileName : fileItem.getName();
+		String submittedFileName = _part.getSubmittedFileName();
+		return submittedFileName != null ? submittedFileName : _part.getName();
 	}
 
 	@Override
 	public String getContentType() {
-		return nonNullContentType(fileItem.getContentType());
+		return nonNullContentType(_part.getContentType());
 	}
     
 }

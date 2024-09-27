e2c43c9efe8018319c9a07d788f9809664e99660
Sven Förster <sven.foerster@top-logic.com>
2024-09-27 13:56:58 +0200

    Ticket #28285: Include dash as valid character for a tlscript string.

--- com.top_logic.model.search/src/main/webapp/script/ace/mode-tlscript.js
+++ com.top_logic.model.search/src/main/webapp/script/ace/mode-tlscript.js
@@ -141,13 +141,13 @@ ace.define('ace/mode/tlscript_highlight_rules', function(require, exports, modul
 					// Single quoted strings
 					// 'text'
 					token: "string.single",
-					regex: /'\w*'/
+					regex: /'[\w-]*'/
 				},
 				{
 					// Double quoted strings
 					// "text"
 					token: "string.single",
-					regex: /"\w*"/
+					regex: /"[\w-]*"/
 				},
 				{
 					// Arithmetic operators

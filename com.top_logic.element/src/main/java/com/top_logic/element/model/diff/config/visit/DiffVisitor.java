a99d3a9620b15d8a400fb925e76021ec8f018aa7
Daniel Busche <daniel.busche@top-logic.com>
2024-10-09 15:06:21 +0200

    Ticket #28304: Ensure that abstract property can be changed incremental.

--- com.top_logic.element/src/main/java/com/top_logic/element/model/diff/config/visit/DiffVisitor.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/diff/config/visit/DiffVisitor.java
@@ -26,6 +26,7 @@ import com.top_logic.element.model.diff.config.RemoveGeneralization;
 import com.top_logic.element.model.diff.config.RenamePart;
 import com.top_logic.element.model.diff.config.SetAnnotations;
 import com.top_logic.element.model.diff.config.UpdateBag;
+import com.top_logic.element.model.diff.config.UpdateAbstract;
 import com.top_logic.element.model.diff.config.UpdateMandatory;
 import com.top_logic.element.model.diff.config.UpdateMultiplicity;
 import com.top_logic.element.model.diff.config.UpdateOrdered;
@@ -103,6 +104,9 @@ public interface DiffVisitor<R, A, E extends Throwable> {
 
 	/** Visit case for {@link UpdateBag}. */
 	R visit(UpdateBag diff, A arg) throws E;
+
+	/** Visit case for {@link UpdateAbstract}. */
+	R visit(UpdateAbstract diff, A arg) throws E;
 
 	/** Visit case for {@link MoveClassifier}. */
 	R visit(MoveClassifier diff, A arg) throws E;

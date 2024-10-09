fb8242917b94fad63e38a3c92348f949ca3736b2
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-10-09 17:40:35 +0200

    Ticket #28310: Moved components and commands for process handling.
    Moved implementations from tl-bpe-app to tl-bpe since those commands
    might be useful even without the default process handling views.

--- com.top_logic.bpe/src/main/java/com/top_logic/bpe/layout/execution/DisplayDescriptionAware.java
+++ com.top_logic.bpe/src/main/java/com/top_logic/bpe/layout/execution/DisplayDescriptionAware.java
@@ -3,7 +3,7 @@
  * 
  * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
  */
-package com.top_logic.bpe.app.layout;
+package com.top_logic.bpe.layout.execution;
 
 import com.top_logic.basic.config.annotation.Hidden;
 import com.top_logic.model.TLObject;

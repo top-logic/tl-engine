bbac7fd9beffb85ceeab38f6a044983ce7dd7931
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-10-10 12:32:54 +0200

    Ticket #28310: Refactoring of process start component.
    Allow to configure post create actions. Extracted specialized action for
    generic process cockpit to separate implementation "ShowProcessAction".

--- com.top_logic.bpe.app/src/main/java/com/top_logic/bpe/app/layout/StepOutHelper.java
+++ com.top_logic.bpe.app/src/main/java/com/top_logic/bpe/app/layout/StepOutHelper.java
@@ -3,7 +3,7 @@
  * 
  * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
  */
-package com.top_logic.bpe.layout.execution.command;
+package com.top_logic.bpe.app.layout;
 
 import com.top_logic.layout.DisplayContext;
 import com.top_logic.mig.html.layout.LayoutComponent;

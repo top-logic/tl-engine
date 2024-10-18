08c2375d139af2d304302539009088acf9a7062d
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-10-18 11:29:21 +0200

    Ticket #28310: Moved form type and form template annotations to element.
    This allows for the FormDefinitionEditor to directly use the annotations
    if no form type can be determined from the FormContextDefinition
    configuration context. This allows for displaying a FormDefinition
    property somewhere deep within some configuration item stored in an
    attribute value, if the attribute is annotated with functions to
    retrieve the form type and form templates.
    
    Used this functionality to introduce an option for the form definition
    of a manual task to use either a custom form or the standard form with
    adjusted visibility.

---
+++ com.top_logic.bpe/src/main/java/com/top_logic/bpe/bpml/display/StandardForm.java
@@ -0,0 +1,165 @@
+/*
+ * SPDX-FileCopyrightText: 2024 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.bpe.bpml.display;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.top_logic.basic.CalledByReflection;
+import com.top_logic.basic.config.AbstractConfiguredInstance;
+import com.top_logic.basic.config.ConfigurationItem;
+import com.top_logic.basic.config.InstantiationContext;
+import com.top_logic.basic.config.PolymorphicConfiguration;
+import com.top_logic.basic.config.TypedConfiguration;
+import com.top_logic.basic.config.annotation.Key;
+import com.top_logic.basic.config.annotation.Name;
+import com.top_logic.basic.func.Function0;
+import com.top_logic.bpe.execution.model.ProcessExecution;
+import com.top_logic.element.layout.formeditor.definition.FieldDefinition;
+import com.top_logic.element.layout.formeditor.definition.TLFormDefinition;
+import com.top_logic.element.meta.form.EditContext;
+import com.top_logic.element.meta.form.fieldprovider.ConfigurationFieldProvider;
+import com.top_logic.element.meta.form.fieldprovider.form.TLFormType;
+import com.top_logic.layout.form.values.DeclarativeFormOptions;
+import com.top_logic.layout.form.values.edit.OptionMapping;
+import com.top_logic.layout.form.values.edit.annotation.Options;
+import com.top_logic.model.TLObject;
+import com.top_logic.model.TLStructuredType;
+import com.top_logic.model.TLStructuredTypePart;
+import com.top_logic.model.form.definition.ContainerDefinition;
+import com.top_logic.model.form.definition.FormDefinition;
+import com.top_logic.model.form.definition.FormElement;
+import com.top_logic.model.form.definition.FormVisibility;
+
+/**
+ * {@link FormProvider} using the standard form of the {@link ProcessExecution} object customized
+ * with visibility annotations.
+ */
+public class StandardForm extends AbstractConfiguredInstance<StandardForm.Config<?>> implements FormProvider {
+
+	/**
+	 * Configuration options for {@link StandardForm}.
+	 */
+	public interface Config<I extends StandardForm> extends PolymorphicConfiguration<I> {
+
+		/**
+		 * Customizations for attributes displayed in the default from of the process type.
+		 */
+		@Key(AnnotationOverlay.ATTRIBUTE)
+		List<AnnotationOverlay> getOverlays();
+
+		/**
+		 * Customization of an attribute in the default from defined for the process type.
+		 */
+		interface AnnotationOverlay extends ConfigurationItem {
+			/**
+			 * @see #getAttribute()
+			 */
+			String ATTRIBUTE = "attribute";
+
+			/**
+			 * Name of the attribute to customize.
+			 */
+			@Name(ATTRIBUTE)
+			@Options(fun = AttributesOfType.class, mapping = AttributeName.class)
+			String getAttribute();
+
+			/**
+			 * The visibility to apply to the field for the given {@link #getAttribute()} in the
+			 * default form.
+			 */
+			FormVisibility getVisibility();
+
+			class AttributesOfType extends Function0<List<? extends TLStructuredTypePart>> {
+				private final EditContext _editContext;
+
+				/**
+				 * Creates a {@link StandardForm.Config.AnnotationOverlay.AttributesOfType}.
+				 */
+				public AttributesOfType(DeclarativeFormOptions options) {
+					_editContext = ConfigurationFieldProvider.editContext(options);
+				}
+
+				@Override
+				public List<? extends TLStructuredTypePart> apply() {
+					TLObject object = _editContext.getObject();
+					if (object == null) {
+						return Collections.emptyList();
+					}
+
+					TLFormType typeAnnotation = _editContext.getAnnotation(TLFormType.class);
+					TLStructuredType formType =
+						TLFormType.resolve(typeAnnotation, object, _editContext.getDescriptionKey());
+					if (formType == null) {
+						return Collections.emptyList();
+					}
+
+					return formType.getAllParts();
+				}
+			}
+
+			class AttributeName implements OptionMapping {
+				@Override
+				public Object toSelection(Object option) {
+					if (option == null) {
+						return null;
+					}
+					return ((TLStructuredTypePart) option).getName();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Creates a {@link StandardForm} from configuration.
+	 * 
+	 * @param context
+	 *        The context for instantiating sub configurations.
+	 * @param config
+	 *        The configuration.
+	 */
+	@CalledByReflection
+	public StandardForm(InstantiationContext context, Config<?> config) {
+		super(context, config);
+	}
+
+	@Override
+	public FormDefinition getFormDefinition(TLStructuredType modelType) {
+		if (modelType == null) {
+			return null;
+		}
+		
+		TLFormDefinition annotation = modelType.getAnnotation(TLFormDefinition.class);
+		if (annotation == null) {
+			return null;
+		}
+		
+		FormDefinition defaultForm = annotation.getForm();
+		FormDefinition form = TypedConfiguration.copy(defaultForm);
+		
+		Map<String, FormVisibility> customization =
+			getConfig().getOverlays().stream().collect(Collectors.toMap(o -> o.getAttribute(), o -> o.getVisibility()));
+		
+		updateForm(form, customization);
+		return form;
+	}
+
+	private void updateForm(FormElement<?> form, Map<String, FormVisibility> customization) {
+		if (form instanceof ContainerDefinition<?> container) {
+			for (PolymorphicConfiguration<?> content : container.getContent()) {
+				updateForm((FormElement<?>) content, customization);
+			}
+		}
+		if (form instanceof FieldDefinition field) {
+			FormVisibility customVisibility = customization.get(field.getAttribute());
+			if (customVisibility != null) {
+				field.setVisibility(customVisibility);
+			}
+		}
+	}
+}
--- com.top_logic.element/src/main/java/com/top_logic/element/layout/formeditor/FormDefinitionEditor.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/layout/formeditor/FormDefinitionEditor.java
@@ -7,12 +7,19 @@ package com.top_logic.element.layout.formeditor;
 
 import static com.top_logic.layout.form.values.Fields.*;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 import com.top_logic.basic.config.ConfigurationChange;
+import com.top_logic.basic.config.ConfigurationItem;
 import com.top_logic.basic.config.ConfigurationListener;
 import com.top_logic.basic.config.PropertyDescriptor;
 import com.top_logic.basic.config.equal.ConfigEquality;
-import com.top_logic.basic.util.ResKey;
 import com.top_logic.element.layout.formeditor.builder.FormDefinitionUtil;
+import com.top_logic.element.meta.form.EditContext;
+import com.top_logic.element.meta.form.fieldprovider.ConfigurationFieldProvider;
+import com.top_logic.element.meta.form.fieldprovider.form.TLFormTemplates;
+import com.top_logic.element.meta.form.fieldprovider.form.TLFormType;
 import com.top_logic.layout.Control;
 import com.top_logic.layout.DisplayContext;
 import com.top_logic.layout.basic.AbstractControlBase;
@@ -26,7 +33,6 @@ import com.top_logic.layout.form.model.BooleanField;
 import com.top_logic.layout.form.model.CommandField;
 import com.top_logic.layout.form.template.DefaultFormFieldControlProvider;
 import com.top_logic.layout.form.values.edit.EditorFactory;
-import com.top_logic.layout.form.values.edit.Labels;
 import com.top_logic.layout.form.values.edit.ValueModel;
 import com.top_logic.layout.form.values.edit.editor.Editor;
 import com.top_logic.layout.form.values.edit.editor.EditorUtils;
@@ -45,7 +51,12 @@ import com.top_logic.tool.boundsec.HandlerResult;
 import com.top_logic.util.Resources;
 
 /**
- * {@link Editor} for a {@link FormDefinition}.
+ * {@link Editor} for a {@link FormDefinition} property of a typed configuration item.
+ * 
+ * <p>
+ * The form is displayed as button opening a form editor that displays the form in a dialog and
+ * allows to edit the form.
+ * </p>
  * 
  * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
  */
@@ -54,180 +65,245 @@ public class FormDefinitionEditor implements Editor {
 	@Override
 	public FormMember createUI(EditorFactory editorFactory, FormContainer container, ValueModel model) {
 		final PropertyDescriptor property = model.getProperty();
-		FormContextDefinition configItem = (FormContextDefinition) model.getModel();
-		PropertyDescriptor typeProperty = configItem.descriptor().getProperty(FormContextDefinition.FORM_CONTEXT_TYPE);
 		
-		CommandField commandField = new CommandField(normalizeFieldName(property.getPropertyName())) {
-
-			@Override
-			public HandlerResult executeCommand(DisplayContext context) {
-				TLStructuredType type = resolveFormType();
-				FormDefinition formDefinitionOrigin = (FormDefinition) model.getValue();
-
-				LayoutComponent contextComponent =
-					editorFactory.getSettings().get(ComponentConfigurationDialogBuilder.COMPONENT);
-				GUIEditorDialog guiEditorDialog = new GUIEditorDialog(contextComponent);
-				// setting of a FormDefinition is recorded as block
-				ScriptingRecorder.annotateAsDontRecord(guiEditorDialog.getDialogModel());
-				guiEditorDialog.setFormDefinitionCopy(formDefinitionOrigin);
-				boolean inEditMode = container.isActive();
-				guiEditorDialog.setOpenInEditMode(inEditMode);
-				guiEditorDialog.setType(type);
-				guiEditorDialog.setTemplateProvider(() -> FormDefinitionUtil.getInheritedFormDefinitionTemplates(type));
-				guiEditorDialog.setAcceptCommand(newAcceptCommand(formDefinitionOrigin, guiEditorDialog));
-				if (inEditMode) {
-					FormDefinition formDefinitionGui = guiEditorDialog.getFormDefinition();
-					guiEditorDialog
-						.addAdditionalCommand(newDiscardChangesCommand(formDefinitionOrigin, formDefinitionGui));
-					guiEditorDialog.addAdditionalCommand(newClearFormCommand(formDefinitionGui));
-				}
-				return guiEditorDialog.open(context);
-			}
-
-			private TLStructuredType resolveFormType() {
-				TLModelPartRef formType = configItem.getFormContextType();
-				return (TLStructuredType) formType.resolveType();
-			}
-
-			private Command newAcceptCommand(FormDefinition formDefinitionOrigin, GUIEditorDialog guiEditorDialog) {
-				FormMember member = this;
-				return new Command() {
-
-					@Override
-					public HandlerResult executeCommand(DisplayContext context) {
-						FormDefinition newFormDefinition;
-						FormDefinition currentFormDefinition = guiEditorDialog.getFormDefinition();
-						if (currentFormDefinition.getContent().isEmpty()) {
-							newFormDefinition = null;
-						} else {
-							newFormDefinition = currentFormDefinition;
-						}
-						if (!ConfigEquality.INSTANCE_ALL_BUT_DERIVED.equals(newFormDefinition, formDefinitionOrigin)) {
-							model.setValue(newFormDefinition);
-						}
-
-						if (ScriptingRecorder.isEnabled()) {
-							/* Recording is turned off by annotation the dialog model. Therefore the
-							 * accept command is not recorded. When the dialog is closed, recording
-							 * is re-enabled, so it is possible to record in a
-							 * DialogClosedListener. */
-							guiEditorDialog.getDialogModel().addListener(DialogModel.CLOSED_PROPERTY,
-								new DialogClosedListener() {
-
-									@Override
-									public void handleDialogClosed(Object sender, Boolean oldValue, Boolean newValue) {
-										if (ScriptingRecorder.isRecordingActive()) {
-											ValueModelUpdate.recordValueModelUpdate(member, newFormDefinition);
-										}
-									}
-								});
-						}
-						return HandlerResult.DEFAULT_RESULT;
-					}
-				};
-			}
-
-			private CommandModel newDiscardChangesCommand(FormDefinition formDefinitionOrigin,
-					FormDefinition formDefinitionGui) {
-				return new DiscardChangesCommandModel(formDefinitionOrigin, formDefinitionGui);
-			}
-
-			private CommandModel newClearFormCommand(FormDefinition formDefintionGui) {
-				return new ClearFormCommandModel(formDefintionGui);
-			}
-		};
-		// setting of a FormDefinition is recorded as block
-		ScriptingRecorder.annotateAsDontRecord(commandField);
-		commandField.setImage(Icons.OPEN_GUI_EDITOR);
-		commandField.setNotExecutableImage(Icons.OPEN_GUI_EDITOR_DISABLED);
-		// Reason key when whole form is not editable.
-		commandField.setNotExecutableReasonKey(com.top_logic.common.webfolder.ui.I18NConstants.FIELD_DISABLED);
-		commandField.setTooltip(Resources.getInstance().getString(I18NConstants.OPEN_FORM_EDITOR_DIALOG));
-		if (configItem.getFormContextType() == null) {
-			setNotExecutable(commandField);
-		}
-		
-		ConfigurationListener typeChangedListener = new ConfigurationListener() {
-
-			@Override
-			public void onChange(ConfigurationChange change) {
-				Object newValue = change.getNewValue();
-				if (newValue == null) {
-					setNotExecutable(commandField);
-					model.setValue(null);
-				} else {
-					commandField.setExecutable();
-					Object oldValue = change.getOldValue();
-					if (oldValue != null && !newValue.equals(oldValue)) {
-						TLType oldType;
-						if (oldValue instanceof TLModelPartRef) {
-							oldType = ((TLModelPartRef) oldValue).resolveType();
-						} else {
-							oldType = (TLType) oldValue;
-						}
-						TLType newType;
-						if (newValue instanceof TLModelPartRef) {
-							newType = ((TLModelPartRef) newValue).resolveType();
-						} else {
-							newType = (TLType) newValue;
-						}
-						if (!TLModelUtil.isCompatibleType(oldType, newType)) {
-							// type changed incompatible: definition is invalid
-							model.setValue(null);
-						}
-					}
-				}
-			}
-		};
-
+		String fieldName = normalizeFieldName(property.getPropertyName());
+	
 		BooleanField changeField = EditorUtils.addChangeField(container, model);
-		ConfigurationListener valueChangedListener = new ConfigurationListener() {
-			@Override
-			public void onChange(ConfigurationChange change) {
-				changeField.setAsBoolean(true);
+		CommandField commandField = new OpenEditor(fieldName, container, model, editorFactory, changeField);
+		container.addMember(commandField);
+	
+		return commandField;
+	}
+
+	private final class OpenEditor extends CommandField {
+		private final FormContainer _container;
+
+		private final ValueModel _model;
+
+		private final EditorFactory _editorFactory;
+
+		/**
+		 * Creates a {@link OpenEditor}.
+		 */
+		private OpenEditor(String name, FormContainer container,
+				ValueModel model, EditorFactory editorFactory, BooleanField changeField) {
+			super(name);
+
+			_container = container;
+			_model = model;
+			_editorFactory = editorFactory;
+
+			// setting of a FormDefinition is recorded as block
+			ScriptingRecorder.annotateAsDontRecord(this);
+			setImage(Icons.OPEN_GUI_EDITOR);
+			setNotExecutableImage(Icons.OPEN_GUI_EDITOR_DISABLED);
+			// Reason key when whole form is not editable.
+			setNotExecutableReasonKey(com.top_logic.common.webfolder.ui.I18NConstants.FIELD_DISABLED);
+			setTooltip(Resources.getInstance().getString(I18NConstants.OPEN_FORM_EDITOR_DIALOG));
+			/* Ensure that the command can be executed, also when the owner group is in view mode to
+			 * allow the user to view the actual form definition. */
+			setInheritDeactivation(false);
+
+			if (resolveFormType() == null) {
+				setNotExecutable(I18NConstants.FORM_EDITOR_DIALOG_DISABLED_NO_TYPE_SELECTED);
 			}
-		};
 
-		commandField.setControlProvider(new DefaultFormFieldControlProvider() {
-			
-			@Override
-			public Control visitCommandField(CommandField member, Void arg) {
-				AbstractControlBase result = (AbstractControlBase) super.visitCommandField(member, arg);
-				result.addListener(AbstractControlBase.ATTACHED_PROPERTY, new AttachedPropertyListener() {
-
-					@Override
-					public void handleAttachEvent(AbstractControlBase sender, Boolean oldValue, Boolean newValue) {
-						if (newValue) {
-							configItem.addConfigurationListener(typeProperty, typeChangedListener);
-							configItem.addConfigurationListener(property, valueChangedListener);
-						} else {
-							configItem.removeConfigurationListener(typeProperty, typeChangedListener);
-							configItem.removeConfigurationListener(property, valueChangedListener);
+			ConfigurationListener typeChangedListener = new ConfigurationListener() {
+				@Override
+				public void onChange(ConfigurationChange change) {
+					Object newValue = change.getNewValue();
+					if (newValue == null) {
+						setNotExecutable(I18NConstants.FORM_EDITOR_DIALOG_DISABLED_NO_TYPE_SELECTED);
+						model.setValue(null);
+					} else {
+						setExecutable();
+						Object oldValue = change.getOldValue();
+						if (oldValue != null && !newValue.equals(oldValue)) {
+							TLType oldType;
+							if (oldValue instanceof TLModelPartRef) {
+								oldType = ((TLModelPartRef) oldValue).resolveType();
+							} else {
+								oldType = (TLType) oldValue;
+							}
+							TLType newType;
+							if (newValue instanceof TLModelPartRef) {
+								newType = ((TLModelPartRef) newValue).resolveType();
+							} else {
+								newType = (TLType) newValue;
+							}
+							if (!TLModelUtil.isCompatibleType(oldType, newType)) {
+								// type changed incompatible: definition is invalid
+								model.setValue(null);
+							}
 						}
 					}
+				}
+			};
 
-				});
-				return result;
+			ConfigurationListener valueChangedListener = new ConfigurationListener() {
+				@Override
+				public void onChange(ConfigurationChange change) {
+					changeField.setAsBoolean(true);
+				}
+			};
+
+			final PropertyDescriptor property = model.getProperty();
+			ConfigurationItem owner = _model.getModel();
+			PropertyDescriptor typeProperty =
+				owner instanceof FormContextDefinition
+					? owner.descriptor().getProperty(FormContextDefinition.FORM_CONTEXT_TYPE)
+					: null;
+
+			setControlProvider(new DefaultFormFieldControlProvider() {
+				@Override
+				public Control visitCommandField(CommandField member, Void arg) {
+					AbstractControlBase result = (AbstractControlBase) super.visitCommandField(member, arg);
+					result.addListener(AbstractControlBase.ATTACHED_PROPERTY, new AttachedPropertyListener() {
+						@Override
+						public void handleAttachEvent(AbstractControlBase sender, Boolean oldValue, Boolean newValue) {
+							if (newValue) {
+								if (typeProperty != null) {
+									owner.addConfigurationListener(typeProperty, typeChangedListener);
+								}
+								owner.addConfigurationListener(property, valueChangedListener);
+							} else {
+								if (typeProperty != null) {
+									owner.removeConfigurationListener(typeProperty, typeChangedListener);
+								}
+								owner.removeConfigurationListener(property, valueChangedListener);
+							}
+						}
+
+					});
+					return result;
+				}
+			});
+		}
+
+		@Override
+		public HandlerResult executeCommand(DisplayContext context) {
+			FormDefinition formDefinitionOrigin = (FormDefinition) _model.getValue();
+
+			LayoutComponent contextComponent =
+				_editorFactory.getSettings().get(ComponentConfigurationDialogBuilder.COMPONENT);
+			GUIEditorDialog guiEditorDialog = new GUIEditorDialog(contextComponent);
+			// setting of a FormDefinition is recorded as block
+			ScriptingRecorder.annotateAsDontRecord(guiEditorDialog.getDialogModel());
+			guiEditorDialog.setFormDefinitionCopy(formDefinitionOrigin);
+			boolean inEditMode = _container.isActive();
+			guiEditorDialog.setOpenInEditMode(inEditMode);
+			initTemplates(guiEditorDialog);
+			guiEditorDialog.setAcceptCommand(newAcceptCommand(formDefinitionOrigin, guiEditorDialog));
+			if (inEditMode) {
+				FormDefinition formDefinitionGui = guiEditorDialog.getFormDefinition();
+				guiEditorDialog
+					.addAdditionalCommand(newDiscardChangesCommand(formDefinitionOrigin, formDefinitionGui));
+				guiEditorDialog.addAdditionalCommand(newClearFormCommand(formDefinitionGui));
 			}
-		});
-		container.addMember(commandField);
+			return guiEditorDialog.open(context);
+		}
 
-		/* Ensure that the command can be executed, also when the owner group is in view mode to
-		 * allow the user to view the actual form definition. */
-		commandField.setInheritDeactivation(false);
+		private TLStructuredType resolveFormType() {
+			ConfigurationItem owner = _model.getModel();
+			if (owner instanceof FormContextDefinition contextDef) {
+				TLStructuredType contextType = contextType(contextDef);
+				if (contextType != null) {
+					return contextType;
+				}
+			}
 
-		return commandField;
+			// Try to resolve from edit context.
+			EditContext editContext =
+				ConfigurationFieldProvider.editContext(_editorFactory.getInitializerProvider());
 
+			TLFormType typeAnnotation = editContext.getAnnotation(TLFormType.class);
+			return TLFormType.resolve(typeAnnotation, editContext.getObject(), editContext.getDescriptionKey());
+		}
+
+		private void initTemplates(GUIEditorDialog guiEditorDialog) {
+			ConfigurationItem owner = _model.getModel();
+
+			Supplier<? extends List<FormDefinitionTemplate>> templateProvider;
+
+			if (owner instanceof FormContextDefinition contextDef) {
+				TLStructuredType type = contextType(contextDef);
+				if (type != null) {
+					templateProvider = () -> FormDefinitionUtil.getInheritedFormDefinitionTemplates(type);
+
+					guiEditorDialog.setType(type);
+					guiEditorDialog.setTemplateProvider(templateProvider);
+					return;
+				}
+			}
+
+			EditContext editContext =
+				ConfigurationFieldProvider.editContext(_editorFactory.getInitializerProvider());
+
+			TLFormType typeAnnotation = editContext.getAnnotation(TLFormType.class);
+			TLFormTemplates templatesAnnotation = editContext.getAnnotation(TLFormTemplates.class);
+
+			TLStructuredType type =
+				TLFormType.resolve(typeAnnotation, editContext.getObject(), editContext.getDescriptionKey());
+			templateProvider = TLFormTemplates.resolve(templatesAnnotation, editContext.getObject());
+
+			guiEditorDialog.setType(type);
+			guiEditorDialog.setTemplateProvider(templateProvider);
+		}
+
+		private TLStructuredType contextType(FormContextDefinition contextDef) {
+			TLModelPartRef formType = contextDef.getFormContextType();
+			if (formType == null) {
+				return null;
+			}
+			return (TLStructuredType) formType.resolveType();
+		}
+
+		private Command newAcceptCommand(FormDefinition formDefinitionOrigin, GUIEditorDialog guiEditorDialog) {
+			FormMember member = this;
+			return new Command() {
+
+				@Override
+				public HandlerResult executeCommand(DisplayContext context) {
+					FormDefinition newFormDefinition;
+					FormDefinition currentFormDefinition = guiEditorDialog.getFormDefinition();
+					if (currentFormDefinition.getContent().isEmpty()) {
+						newFormDefinition = null;
+					} else {
+						newFormDefinition = currentFormDefinition;
+					}
+					if (!ConfigEquality.INSTANCE_ALL_BUT_DERIVED.equals(newFormDefinition, formDefinitionOrigin)) {
+						_model.setValue(newFormDefinition);
+					}
+
+					if (ScriptingRecorder.isEnabled()) {
+						/* Recording is turned off by annotation the dialog model. Therefore the
+						 * accept command is not recorded. When the dialog is closed, recording
+						 * is re-enabled, so it is possible to record in a
+						 * DialogClosedListener. */
+						guiEditorDialog.getDialogModel().addListener(DialogModel.CLOSED_PROPERTY,
+							new DialogClosedListener() {
+
+								@Override
+								public void handleDialogClosed(Object sender, Boolean oldValue, Boolean newValue) {
+									if (ScriptingRecorder.isRecordingActive()) {
+										ValueModelUpdate.recordValueModelUpdate(member, newFormDefinition);
+									}
+								}
+							});
+					}
+					return HandlerResult.DEFAULT_RESULT;
+				}
+			};
+		}
+
+		private CommandModel newDiscardChangesCommand(FormDefinition formDefinitionOrigin,
+				FormDefinition formDefinitionGui) {
+			return new DiscardChangesCommandModel(formDefinitionOrigin, formDefinitionGui);
+		}
+
+		private CommandModel newClearFormCommand(FormDefinition formDefintionGui) {
+			return new ClearFormCommandModel(formDefintionGui);
+		}
 	}
-
-	void setNotExecutable(CommandField commandField) {
-		commandField.setNotExecutable(I18NConstants.FORM_EDITOR_DIALOG_DISABLED_NO_TYPE_SELECTED);
-	}
-
-	ResKey label(PropertyDescriptor property) {
-		return Labels.propertyLabelKey(property);
-	}
-
-
 }
 

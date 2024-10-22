5d0785854a22b464ddd4021124da28a91fc39fd0
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-10-22 11:07:19 +0200

    Ticket #28310: Cleanup ResKeyAttributeValueBinding.

--- com.top_logic/src/main/java/com/top_logic/model/io/bindings/ResKeyAttributeValueBinding.java
+++ com.top_logic/src/main/java/com/top_logic/model/io/bindings/ResKeyAttributeValueBinding.java
@@ -25,6 +25,12 @@ import com.top_logic.model.io.AttributeValueBinding;
 public class ResKeyAttributeValueBinding implements AttributeValueBinding<ResKey> {
 
 	/**
+	 * Top level tag to write the resource key to. This is required to also allow storing
+	 * <code>null</code> values (then this tag is missing).
+	 */
+	private static final String RESOURCE_TAG = "resource";
+
+	/**
 	 * Singleton {@link ResKeyAttributeValueBinding} instance.
 	 */
 	public static final ResKeyAttributeValueBinding INSTANCE = new ResKeyAttributeValueBinding();
@@ -38,16 +44,17 @@ public class ResKeyAttributeValueBinding implements AttributeValueBinding<ResKey
 			throws XMLStreamException {
 		int tag = in.nextTag();
 		if (tag == XMLStreamConstants.START_ELEMENT) {
-			if (!"resource".equals(in.getLocalName())) {
-				throw new XMLStreamException("Expected resource element, but found: " + in.getLocalName(),
+			if (!RESOURCE_TAG.equals(in.getLocalName())) {
+				throw new XMLStreamException("Expected '" + RESOURCE_TAG + "' element, but found: " + in.getLocalName(),
 					in.getLocation());
 			}
 
 			ResKey result = ResKey.ValueBinding.INSTANCE.loadConfigItem(in, null);
 
 			XMLStreamUtil.ensureEndTag(in);
-			if (!"resource".equals(in.getLocalName())) {
-				throw new XMLStreamException("Expected end of resource element, but found: " + in.getLocalName(),
+			if (!RESOURCE_TAG.equals(in.getLocalName())) {
+				throw new XMLStreamException(
+					"Expected end of '" + RESOURCE_TAG + "' element, but found: " + in.getLocalName(),
 					in.getLocation());
 			}
 
@@ -62,7 +69,7 @@ public class ResKeyAttributeValueBinding implements AttributeValueBinding<ResKey
 	public void storeValue(Log log, XMLStreamWriter out, TLStructuredTypePart attribute, ResKey value)
 			throws XMLStreamException {
 		if (value != null) {
-			out.writeStartElement("resource");
+			out.writeStartElement(RESOURCE_TAG);
 			ResKey.ValueBinding.INSTANCE.saveConfigItem(out, value);
 			out.writeEndElement();
 		}

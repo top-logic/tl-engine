a7c47e0d04ac1b38870f19df7737875edfa85bd7
Daniel Busche <daniel.busche@top-logic.com>
2024-12-05 17:01:22 +0100

    Ticket #28307: Added comment, that mandatory is only set, if it is necessary.

--- com.top_logic.element/src/main/java/com/top_logic/element/model/export/ModelConfigExtractor.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/model/export/ModelConfigExtractor.java
@@ -330,24 +330,35 @@ public class ModelConfigExtractor implements TLModelVisitor<ModelPartConfig, Voi
 			copyIfDifferent(config::isOrdered, config::setOrdered, model.isOrdered());
 			copyIfDifferent(config::isBag, config::setBag, model.isBag());
 		} else if (model instanceof TLClassPart) {
-			Boolean allOverriddenMandatory = null;
-			for (TLClassPart overridden : TLModelUtil.getOverriddenParts((TLClassPart) model)) {
-				boolean mandatory = overridden.isMandatory();
-				if (allOverriddenMandatory == null) {
-					allOverriddenMandatory = mandatory;
-				} else {
-					if (allOverriddenMandatory.booleanValue() != mandatory) {
-						// Some overridden attributes are mandatory and some not. Set local value!
-						allOverriddenMandatory = null;
-						break;
-					}
-				}
-			}
-			if (allOverriddenMandatory == null || allOverriddenMandatory.booleanValue() != modelMandatory) {
-				config.setMandatory(modelMandatory);
-			}
+			setMandatoryForOverride(config, (TLClassPart) model, modelMandatory);
 		}
 		return override;
+	}
+
+	/**
+	 * Sets {@link PartConfig#getMandatory()} for the given overriding part.
+	 * 
+	 * @implNote It is checked whether all directly overridden parts have the same "mandatory"
+	 *           value. If this is the case and the value matches the specified "mandatory" value,
+	 *           nothing is set to avoid setting the configuration property unnecessarily.
+	 */
+	private void setMandatoryForOverride(PartConfig config, TLClassPart model, boolean modelMandatory) {
+		Boolean allOverriddenMandatory = null;
+		for (TLClassPart overridden : TLModelUtil.getOverriddenParts(model)) {
+			boolean mandatory = overridden.isMandatory();
+			if (allOverriddenMandatory == null) {
+				allOverriddenMandatory = mandatory;
+			} else {
+				if (allOverriddenMandatory.booleanValue() != mandatory) {
+					// Some overridden attributes are mandatory and some not. Set local value!
+					allOverriddenMandatory = null;
+					break;
+				}
+			}
+		}
+		if (allOverriddenMandatory == null || allOverriddenMandatory.booleanValue() != modelMandatory) {
+			config.setMandatory(modelMandatory);
+		}
 	}
 
 	private String typeRef(TLModule ownerModule, TLType type) {

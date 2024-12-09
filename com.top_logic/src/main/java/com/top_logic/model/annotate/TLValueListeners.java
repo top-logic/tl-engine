f90fafc0bf00fe5d08450de82b00b33c1e0ea9df
Daniel Busche <daniel.busche@top-logic.com>
2024-12-09 10:45:36 +0100

    Ticket #28411: Added annotation to add ValueListener to attributes.

---
+++ com.top_logic/src/main/java/com/top_logic/model/annotate/TLValueListeners.java
@@ -0,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: 2024 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.model.annotate;
+
+import java.util.List;
+
+import com.top_logic.basic.annotation.InApp;
+import com.top_logic.basic.config.PolymorphicConfiguration;
+import com.top_logic.basic.config.annotation.DefaultContainer;
+import com.top_logic.basic.config.annotation.TagName;
+import com.top_logic.layout.form.FormField;
+import com.top_logic.layout.form.ValueListener;
+import com.top_logic.layout.form.values.edit.InAppImplementations;
+import com.top_logic.layout.form.values.edit.annotation.Options;
+import com.top_logic.model.annotate.AnnotationInheritance.Policy;
+
+/**
+ * {@link TLAttributeAnnotation} to annotate {@link ValueListener value listeners} to the field
+ * created for an attribute.
+ */
+@InApp
+@TagName("value-listeners")
+// Listeners are collected explicitly by code over the whole inheritance hierarchy. 
+@AnnotationInheritance(Policy.REDEFINE)
+public interface TLValueListeners extends TLAttributeAnnotation {
+
+	/**
+	 * Listeners to trigger when the {@link FormField} for the annotated attribute changes its
+	 * value.
+	 */
+	@DefaultContainer
+	@Options(fun = InAppImplementations.class)
+	List<PolymorphicConfiguration<? extends ValueListener>> getListeners();
+
+}
+

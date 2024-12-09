0fb9a47cdbfdb78785da2f91463a58492f1ea6d5
Daniel Busche <daniel.busche@top-logic.com>
2024-12-09 13:17:08 +0100

    Ticket #28411: Added missing @Deprecated annotation.

--- com.top_logic/src/main/java/com/top_logic/model/TransientObject.java
+++ com.top_logic/src/main/java/com/top_logic/model/TransientObject.java
@@ -70,6 +70,7 @@ public abstract class TransientObject extends AbstractTLObject {
 	 * @deprecated Use {@link #tId()} instead
 	 */
 	@Override
+	@Deprecated
 	public ObjectKey getObjectKey() {
 		return tId();
 	}

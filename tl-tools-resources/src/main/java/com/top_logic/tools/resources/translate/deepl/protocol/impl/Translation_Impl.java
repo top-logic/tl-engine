9c336bb8590302e0299dfa49a395670c7349d980
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-01-16 13:47:20 +0100

    Ticket #28450: Updated msgbuf dependency to 1.1.5.

--- tl-tools-resources/src/main/java/com/top_logic/tools/resources/translate/deepl/protocol/impl/Translation_Impl.java
+++ tl-tools-resources/src/main/java/com/top_logic/tools/resources/translate/deepl/protocol/impl/Translation_Impl.java
@@ -1,7 +1,7 @@
 package com.top_logic.tools.resources.translate.deepl.protocol.impl;
 
 /**
- * A single translation result of a <code>TranslationResult</code> message.
+ * Implementation of {@link com.top_logic.tools.resources.translate.deepl.protocol.Translation}.
  */
 public class Translation_Impl extends de.haumacher.msgbuf.data.AbstractDataObject implements com.top_logic.tools.resources.translate.deepl.protocol.Translation {
 

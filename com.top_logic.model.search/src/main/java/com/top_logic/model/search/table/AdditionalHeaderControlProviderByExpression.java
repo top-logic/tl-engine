04b3d644bd727237cb463d4116d4e6f5ff520386
Jonathan Huesing <jonathan.huesing@top-logic.com>
2025-01-24 14:40:08 +0100

    Ticket #28459: improve header control configuration and documentation

--- com.top_logic.model.search/src/main/java/com/top_logic/model/search/table/AdditionalHeaderControlProviderByExpression.java
+++ com.top_logic.model.search/src/main/java/com/top_logic/model/search/table/AdditionalHeaderControlProviderByExpression.java
@@ -14,7 +14,9 @@ import com.top_logic.basic.config.AbstractConfiguredInstance;
 import com.top_logic.basic.config.ConfigurationItem;
 import com.top_logic.basic.config.InstantiationContext;
 import com.top_logic.basic.config.PolymorphicConfiguration;
+import com.top_logic.basic.config.SimpleInstantiationContext;
 import com.top_logic.basic.config.TypedConfiguration;
+import com.top_logic.basic.config.annotation.Label;
 import com.top_logic.basic.config.annotation.Name;
 import com.top_logic.basic.config.annotation.TagName;
 import com.top_logic.basic.config.order.DisplayOrder;
@@ -25,6 +27,7 @@ import com.top_logic.layout.provider.MetaLabelProvider;
 import com.top_logic.layout.table.model.AdditionalHeaderControlModel;
 import com.top_logic.layout.table.model.ColumnBaseConfig;
 import com.top_logic.layout.table.model.SimpleAdditionalHeaderControl;
+import com.top_logic.mig.html.layout.LayoutComponent;
 import com.top_logic.model.search.expr.config.dom.Expr;
 import com.top_logic.model.search.expr.query.QueryExecutor;
 
@@ -40,8 +43,8 @@ public class AdditionalHeaderControlProviderByExpression
 	/** {@link ConfigurationItem} for the {@link AdditionalHeaderControlProviderByExpression}. */
 	@TagName(Config.TAG_NAME)
 	@DisplayOrder({
-		Config.LABEL,
 		Config.RES_KEY,
+		Config.LABEL,
 		Config.TOOLTIP,
 		Config.USE_ROW_OBJECTS,
 		ColumnBaseConfig.CSS_CLASS
@@ -65,56 +68,60 @@ public class AdditionalHeaderControlProviderByExpression
 
 		/**
 		 * The TL-Script expression for the label.
-		 * 
+		 *
 		 * <p>
-		 * This script is called with a single argument: The {@link List} of
-		 * {@link AdditionalHeaderControlModel#getValues() column values}.
+		 * This script is expected take two arguments, the {@link List} of
+		 * {@link AdditionalHeaderControlModel#getValues() column values} as first argument and the
+		 * current component model as second argument.
 		 * </p>
 		 * <p>
 		 * The result of the script is converted to a {@link String} with the
 		 * {@link MetaLabelProvider}.
 		 * </p>
 		 * <p>
-		 * If not set, only the label {@link #getResKey()} is written.
+		 * If {@link #getResKey()} is also given, the dynamic label is passed as single argument to
+		 * the configured {@link ResKey} for embedding. The static label text should contain a
+		 * placeholder <code>{0}</code> where the dynamic label is to be inserted.
 		 * </p>
 		 */
 		@Name(LABEL)
+		@Label("Dynamic Label")
 		Expr getLabel();
 
 		/**
 		 * The TL-Script expression for the tooltip.
 		 * <p>
-		 * This script is called with a single parameter: The {@link List} of
-		 * {@link AdditionalHeaderControlModel#getValues() column values}.
+		 * This script is expected take two arguments, the {@link List} of
+		 * {@link AdditionalHeaderControlModel#getValues() column values} as first argument and the
+		 * current component model as second argument.
 		 * </p>
 		 * <p>
 		 * The result of the script is converted to a {@link String} with the
 		 * {@link MetaLabelProvider}.
 		 * </p>
 		 * 
-		 * <p>
-		 * If the value is not set, only the static {@link #getResKey()} tooltip is written.
-		 * </p>
 		 */
 		@Name(TOOLTIP)
 		Expr getTooltip();
 
 		/**
-		 * The {@link ResKey} for the label.
+		 * The {@link ResKey} for the static label.
+		 *
 		 * <p>
-		 * The tooltip {@link ResKey} is derived from this setting with the <code>.tooltip</code>
-		 * suffix. If a value is given, both translations (with and without the
-		 * <code>.tooltip</code> suffix) must be given.
+		 * If provided along with {@link #getLabel()}, the static label text should contain a
+		 * placeholder <code>{0}</code> where the dynamic content will be inserted. If only the
+		 * static label is given, it will be displayed as is.
 		 * </p>
-		 * 
+		 *
 		 * <p>
-		 * If this is not set, only the {@link #getTooltip()} script is written
+		 * Note: The tooltip {@link ResKey} is derived by appending <code>.tooltip</code> to this
+		 * key. Both translations (with and without suffix) must exist when using this key.
 		 * </p>
-		 * 
+		 *
 		 * @see SimpleAdditionalHeaderControl#composeStaticWithDynamicPart(ResKey, Object)
-		 *      Interaction of the script and key.
 		 */
 		@Name(RES_KEY)
+		@Label("Static Label")
 		ResKey getResKey();
 
 		/**
@@ -136,6 +143,8 @@ public class AdditionalHeaderControlProviderByExpression
 
 	}
 
+	private LayoutComponent _component;
+
 	private final QueryExecutor _labelScript;
 
 	private final QueryExecutor _tooltipScript;
@@ -149,6 +158,10 @@ public class AdditionalHeaderControlProviderByExpression
 	/** {@link TypedConfiguration} constructor for {@link AdditionalHeaderControlProviderByExpression}. */
 	public AdditionalHeaderControlProviderByExpression(InstantiationContext context, Config config) {
 		super(context, config);
+
+		if (!(context instanceof SimpleInstantiationContext)) {
+			context.resolveReference(InstantiationContext.OUTER, LayoutComponent.class, c -> _component = c);
+		}
 		_labelScript = QueryExecutor.compileOptional(config.getLabel());
 		_tooltipScript = QueryExecutor.compileOptional(config.getTooltip());
 		_resKey = config.getResKey();
@@ -159,7 +172,7 @@ public class AdditionalHeaderControlProviderByExpression
 	@Override
 	public Control createControl(Object model, String style) {
 		return new AdditionalHeaderControlByExpression((AdditionalHeaderControlModel) model,
-			_labelScript, _tooltipScript, _resKey, _cssClass, _useRowObjects, emptyMap());
+			_labelScript, _tooltipScript, _resKey, _cssClass, _useRowObjects, _component, emptyMap());
 	}
 
 }

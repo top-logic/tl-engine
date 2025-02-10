4436b99cebbf56a002871fd7223cee13bd7e5eb9
Jonathan Huesing <jonathan.huesing@top-logic.com>
2025-02-10 16:47:26 +0100

    Ticket #28310: rename and create documentation for the added Functions

--- com.top_logic.bpe/src/main/java/com/top_logic/bpe/execution/script/ExecuteTransition.java
+++ com.top_logic.bpe/src/main/java/com/top_logic/bpe/execution/script/ExecuteTransition.java
@@ -5,11 +5,15 @@
  */
 package com.top_logic.bpe.execution.script;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.top_logic.basic.config.ConfigurationException;
 import com.top_logic.basic.config.InstantiationContext;
 import com.top_logic.bpe.BPEUtil;
+import com.top_logic.bpe.bpml.model.Edge;
+import com.top_logic.bpe.bpml.model.Node;
+import com.top_logic.bpe.execution.engine.ExecutionEngine;
 import com.top_logic.bpe.execution.model.Token;
 import com.top_logic.element.meta.TypeSpec;
 import com.top_logic.model.TLType;
@@ -22,47 +26,69 @@ import com.top_logic.model.search.expr.config.operations.ArgumentDescriptor;
 import com.top_logic.model.util.TLModelUtil;
 
 /**
- * TODO jhu add comment
- *
+ * A {@link GenericMethod} implementation that executes a transition from a given {@link Token} to a
+ * specified {@link Node}.
+ * 
  * @author <a href="mailto:Jonathan.Hüsing@top-logic.com">Jonathan Hüsing</a>
  */
-public class GetReachableTargets extends GenericMethod {
+public class ExecuteTransition extends GenericMethod {
 
-	protected GetReachableTargets(String name, SearchExpression[] arguments) {
+	/**
+	 * Creates a new {@link ExecuteTransition}.
+	 *
+	 */
+	protected ExecuteTransition(String name, SearchExpression[] arguments) {
 		super(name, arguments);
 	}
 
 	@Override
 	public GenericMethod copy(SearchExpression[] arguments) {
-		return new GetReachableTargets(getName(), arguments);
+		return new ExecuteTransition(getName(), arguments);
 	}
 
 	@Override
 	public TLType getType(List<TLType> argumentTypes) {
-		return TLModelUtil.findType(TypeSpec.OBJECT_TYPE);
-	}
-
-	@Override
-	protected Object eval(Object[] arguments, EvalContext definitions) {
-		if (arguments.length < 1 || arguments[0] == null) {
-			return false;
-		}
-
-		Token currentToken = (Token) arguments[0];
-
-		// Find path to target
-		return BPEUtil.getPossibleTransitions(currentToken).keySet();
-
+		return TLModelUtil.findType(TypeSpec.BOOLEAN_TYPE);
 	}
 
 	/**
-	 * {@link AbstractSimpleMethodBuilder} creating an {@link GetReachableTargets} function.
+	 * Attempts transition from a given {@link Token} to a specified {@link Node}.
+	 * 
+	 * @param arguments
+	 *        args[0]: source token, args[1]: target node
+	 * @param definitions
+	 *        evaluation context
+	 * @return true if transition executed successfully
 	 */
-	public static final class Builder extends AbstractSimpleMethodBuilder<GetReachableTargets> {
+	@Override
+	protected Object eval(Object[] arguments, EvalContext definitions) {
+		if (arguments.length < 2 || arguments[0] == null || arguments[1] == null) {
+			return false;
+		}
 
-		/** Description of parameters for a {@link GetReachableTargets}. */
+		Token token = (Token) arguments[0];
+		Node target = (Node) arguments[1];
+
+		// validate transition path
+		List<Edge> path = BPEUtil.getPossibleTransitions(token).getOrDefault(target, new ArrayList<>());
+		if (path.isEmpty()) {
+			return false;
+		}
+
+		// execute path
+		ExecutionEngine.getInstance().execute(token, path, null);
+		return true;
+	}
+
+	/**
+	 * {@link AbstractSimpleMethodBuilder} creating an {@link ExecuteTransition} function.
+	 */
+	public static final class Builder extends AbstractSimpleMethodBuilder<ExecuteTransition> {
+
+		/** Description of parameters for a {@link ExecuteTransition}. */
 		public static final ArgumentDescriptor DESCRIPTOR = ArgumentDescriptor.builder()
 			.mandatory("currentToken")
+			.mandatory("target")
 			.build();
 
 		/**
@@ -78,9 +104,9 @@ public class GetReachableTargets extends GenericMethod {
 		}
 
 		@Override
-		public GetReachableTargets build(Expr expr, SearchExpression[] args)
+		public ExecuteTransition build(Expr expr, SearchExpression[] args)
 				throws ConfigurationException {
-			return new GetReachableTargets(getConfig().getName(), args);
+			return new ExecuteTransition(getConfig().getName(), args);
 		}
 
 	}

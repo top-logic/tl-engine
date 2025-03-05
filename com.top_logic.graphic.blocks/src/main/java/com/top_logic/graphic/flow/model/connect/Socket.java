935e7ff2385fd5df0234614980a11a484e15fb5b
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-03-05 19:18:18 +0100

    Ticket #25918: Starting point for flow chart layouting.

---
+++ com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/flow/model/connect/Socket.java
@@ -0,0 +1,49 @@
+/*
+ * SPDX-FileCopyrightText: 2025 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.graphic.flow.model.connect;
+
+import com.top_logic.graphic.blocks.math.Vec;
+
+/**
+ * 
+ */
+public class Socket {
+
+	private double _x;
+
+	private double _y;
+
+	public double getX() {
+		return _x;
+	}
+
+	public void setX(double x) {
+		_x = x;
+	}
+
+	public double getY() {
+		return _y;
+	}
+
+	public void setY(double y) {
+		_y = y;
+	}
+
+	public Vec getPos() {
+		return Vec.vec(_x, _y);
+	}
+
+	public void setPos(Vec pos) {
+		_x = pos.getX();
+		_y = pos.getY();
+	}
+
+	public void setPos(double x, double y) {
+		_x = x;
+		_y = y;
+	}
+
+}

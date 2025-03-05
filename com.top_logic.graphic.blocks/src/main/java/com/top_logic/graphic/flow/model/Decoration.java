b040d281dd12db185fea8ef9d237755d1e654409
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-03-05 19:18:18 +0100

    Ticket #25918: Starting point for flow chart layouting.

---
+++ com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/flow/model/Decoration.java
@@ -0,0 +1,43 @@
+/*
+ * SPDX-FileCopyrightText: 2025 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.graphic.flow.model;
+
+import com.top_logic.graphic.blocks.svg.RenderContext;
+import com.top_logic.graphic.blocks.svg.SvgWriter;
+
+/**
+ * {@link DrawElement} that wraps a single other {@link DrawElement}.
+ */
+public abstract class Decoration extends AbstractDrawElement {
+
+	private DrawElement _content = new EmptyBlock();
+
+	/**
+	 * The decorated element.
+	 */
+	public DrawElement getContent() {
+		return _content;
+	}
+
+	/**
+	 * @see #getContent()
+	 */
+	public Decoration setContent(DrawElement content) {
+		_content = content;
+		return this;
+	}
+
+	@Override
+	public void computeIntrinsicSize(RenderContext context, double offsetX, double offsetY) {
+		_content.computeIntrinsicSize(context, offsetX, offsetY);
+	}
+
+	@Override
+	public void draw(SvgWriter out) {
+		_content.draw(out);
+	}
+
+}

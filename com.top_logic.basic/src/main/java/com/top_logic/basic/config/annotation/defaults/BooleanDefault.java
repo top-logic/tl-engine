612a602b6e838c0003faf45f5830be099e8bb430
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-03-07 11:23:19 +0100

    Ticket #25918: Allow to use default annotations on method parameters.
    Those annotations can also be useful when defining TL-Script methods
    from Java method signatures.

--- com.top_logic.basic/src/main/java/com/top_logic/basic/config/annotation/defaults/BooleanDefault.java
+++ com.top_logic.basic/src/main/java/com/top_logic/basic/config/annotation/defaults/BooleanDefault.java
@@ -19,7 +19,7 @@ import com.top_logic.basic.config.annotation.TagName;
  * 
  * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
  */
-@Target(ElementType.METHOD)
+@Target({ ElementType.METHOD, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 @TagName("boolean-default")
 public @interface BooleanDefault {

46210faf5ddc85c1652b269646154ff0a7166e70
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-03-19 09:26:05 +0100

    Ticket #28558: By default, collapse more contents in server config.

--- com.top_logic.service.openapi.server/src/main/java/com/top_logic/service/openapi/server/conf/ParametersConfig.java
+++ com.top_logic.service.openapi.server/src/main/java/com/top_logic/service/openapi/server/conf/ParametersConfig.java
@@ -7,6 +7,7 @@ package com.top_logic.service.openapi.server.conf;
 
 import com.top_logic.basic.config.ConfigurationItem;
 import com.top_logic.basic.config.annotation.Key;
+import com.top_logic.layout.form.values.edit.annotation.CollapseEntries;
 import com.top_logic.service.openapi.server.parameter.RequestParameter;
 
 /**
@@ -30,6 +31,7 @@ public interface ParametersConfig extends ConfigurationItem {
 	 */
 	@com.top_logic.basic.config.annotation.Name(PARAMETERS)
 	@Key(RequestParameter.Config.NAME_ATTRIBUTE)
+	@CollapseEntries
 	java.util.List<RequestParameter.Config<? extends RequestParameter<?>>> getParameters();
 
 }

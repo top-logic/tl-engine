25af5444b0df27869bcf08397da3278450ed9423
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-03-28 10:31:15 +0100

    Ticket #28310: Added new migration baseline for TL-7.9.3.

---
+++ test-migrate-apps/test-app-7-9-3/src/test/java/com/top_logic/test/app793/TestApp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 My Company. All Rights Reserved
+ */
+package com.top_logic.test.app793;
+
+import junit.framework.TestCase;
+
+/**
+ * Test case for the app <code>Test App 7.9.3</code>. 
+ */
+public class TestApp extends TestCase {
+	
+	public void testIt() {
+		// Add some test.
+	}
+	
+}

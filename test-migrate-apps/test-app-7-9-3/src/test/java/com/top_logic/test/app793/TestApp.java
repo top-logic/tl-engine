11e20f990f69d0538f7f399ee49a79fe2265d986
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-03-28 15:53:32 +0100

    Ticket #28310: Added test case for 7.9.3 migration.

--- test-migrate-apps/test-app-7-9-3/src/test/java/com/top_logic/test/app793/TestApp.java
+++ test-migrate-apps/test-app-7-9-3/src/test/java/com/top_logic/test/app793/TestApp.java
@@ -3,15 +3,25 @@
  */
 package com.top_logic.test.app793;
 
+import junit.framework.Test;
 import junit.framework.TestCase;
+
+import test.com.top_logic.AppMigrationTestSetup;
+import test.com.top_logic.layout.scripting.XmlScriptedTestUtil;
 
 /**
  * Test case for the app <code>Test App 7.9.3</code>. 
  */
 public class TestApp extends TestCase {
 	
-	public void testIt() {
-		// Add some test.
+	/**
+	 * Test suite creation.
+	 */
+	public static Test suite() {
+		String[] testCases = {
+			"TestUI",
+		};
+		return AppMigrationTestSetup.suite(XmlScriptedTestUtil.suite(TestApp.class, testCases));
 	}
-	
+
 }

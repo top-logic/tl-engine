fa6cc58a6cdf92a815c8960ce6056037dd570762
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-03-31 12:45:44 +0200

    Ticket #28310: Allow apply XSLT column transform processor to flex data.

--- com.top_logic/src/main/java/com/top_logic/knowledge/service/db2/migration/processor/StringColumnTransformProcessor.java
+++ com.top_logic/src/main/java/com/top_logic/knowledge/service/db2/migration/processor/StringColumnTransformProcessor.java
@@ -24,12 +24,20 @@ import com.top_logic.basic.db.model.DBTable;
 import com.top_logic.basic.db.model.util.DBSchemaUtils;
 import com.top_logic.basic.db.sql.CompiledStatement;
 import com.top_logic.basic.db.sql.SQLColumnDefinition;
+import com.top_logic.basic.db.sql.SQLExpression;
+import com.top_logic.basic.db.sql.SQLFactory;
 import com.top_logic.basic.db.sql.SQLQuery;
 import com.top_logic.basic.db.sql.SQLSelect;
 import com.top_logic.basic.sql.DBHelper;
+import com.top_logic.basic.sql.DBType;
 import com.top_logic.basic.sql.PooledConnection;
 import com.top_logic.basic.sql.SQLH;
+import com.top_logic.dob.MOAttribute;
+import com.top_logic.dob.meta.BasicTypes;
+import com.top_logic.dob.meta.MORepository;
+import com.top_logic.dob.meta.MOStructure;
 import com.top_logic.dob.sql.DBTableMetaObject;
+import com.top_logic.knowledge.service.db2.AbstractFlexDataManager;
 import com.top_logic.knowledge.service.migration.MigrationContext;
 import com.top_logic.knowledge.service.migration.MigrationProcessor;
 
@@ -94,42 +102,137 @@ public abstract class StringColumnTransformProcessor<C extends StringColumnTrans
 	@Override
 	public void doMigration(MigrationContext context, Log log, PooledConnection connection) {
 		C config = getConfig();
-		String table = mangleDBName(config.getTable());
-		String column = mangleDBName(config.getColumn());
+
+		List<SQLColumnDefinition> columns = new ArrayList<>();
+		List<String> keyColumns = new ArrayList<>();
+		
+		String table;
 		try {
+			if (config.getRawTableNames()) {
+				table = mangleDBName(config.getTable());
+				String valueColumn = mangleDBName(config.getColumn());
 
-			DBTable tableDef =
-				DBSchemaUtils.extractTable(connection.getPool(), DBSchemaFactory.createDBSchema(), table);
-			if (tableDef == null) {
-				log.info("No such table '" + table + "', skipping migration.");
-				return;
-			}
+				DBTable tableDef =
+					DBSchemaUtils.extractTable(connection.getPool(), DBSchemaFactory.createDBSchema(), table);
+				if (tableDef == null) {
+					log.info("No such table '" + table + "', skipping migration.");
+					return;
+				}
 
-			DBPrimary primaryKey = tableDef.getPrimaryKey();
+				DBPrimary primaryKey = tableDef.getPrimaryKey();
 
-			List<SQLColumnDefinition> columns = new ArrayList<>();
-			columns.add(columnDef(column));
-			List<String> keyColumns = new ArrayList<>();
-			for (DBColumnRef keyColumn : primaryKey.getColumnRefs()) {
-				String keyColumnName = keyColumn.getName();
-				columns.add(columnDef(keyColumnName));
-				keyColumns.add(keyColumnName);
-			}
+				columns.add(columnDef(valueColumn));
+				for (DBColumnRef keyColumn : primaryKey.getColumnRefs()) {
+					String keyColumnName = keyColumn.getName();
+					columns.add(columnDef(keyColumnName));
+					keyColumns.add(keyColumnName);
+				}
 
-			SQLQuery<SQLSelect> select = query(select(columns, table(table)));
+				process(log, connection, table, valueColumn, columns, keyColumns, SQLFactory.literalTrueLogical());
+			} else {
+				MORepository repository = context.getPersistentRepository();
+				MOStructure tableDef = (MOStructure) repository.getMetaObject(config.getTable());
+				MOAttribute attributeDef = tableDef.getAttributeOrNull(config.getColumn());
+				if (attributeDef == null) {
+					// Use flex data.
+					MOStructure flexTable = (MOStructure) repository.getMetaObject(AbstractFlexDataManager.FLEX_DATA);
+					table = flexTable.getDBMapping().getDBName();
 
-			DBHelper sqlDialect = connection.getSQLDialect();
-			CompiledStatement query = select.toSql(sqlDialect);
-			query.setResultSetConfiguration(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE);
+					if (context.hasBranchSupport()) {
+						columns.add(columnDef(AbstractFlexDataManager.BRANCH_DBNAME));
+						keyColumns.add(AbstractFlexDataManager.BRANCH_DBNAME);
+					}
 
-			try (ResultSet resultSet = query.executeQuery(connection)) {
-				processRows(log, resultSet, table, column, keyColumns);
+					columns.add(columnDef(AbstractFlexDataManager.IDENTIFIER_DBNAME));
+					keyColumns.add(AbstractFlexDataManager.IDENTIFIER_DBNAME);
+
+					columns.add(columnDef(BasicTypes.REV_MAX_DB_NAME));
+					keyColumns.add(BasicTypes.REV_MAX_DB_NAME);
+
+					columns.add(columnDef(AbstractFlexDataManager.TYPE_DBNAME));
+					keyColumns.add(AbstractFlexDataManager.TYPE_DBNAME);
+					
+					columns.add(columnDef(AbstractFlexDataManager.ATTRIBUTE_DBNAME));
+					keyColumns.add(AbstractFlexDataManager.ATTRIBUTE_DBNAME);
+
+					{
+						log.info("Processing VARCHAR flex data for table '" + config.getTable() + "' and attribute '"
+							+ config.getColumn() + "'.");
+
+						String valueColumn = AbstractFlexDataManager.VARCHAR_DATA_DBNAME;
+
+						List<SQLColumnDefinition> allColumns = new ArrayList<>();
+						allColumns.add(columnDef(valueColumn));
+						allColumns.addAll(columns);
+
+						process(log, connection, table, valueColumn, allColumns, keyColumns,
+							and(
+								eqSQL(column(AbstractFlexDataManager.TYPE_DBNAME),
+									literal(DBType.STRING, config.getTable())),
+								eqSQL(column(AbstractFlexDataManager.ATTRIBUTE_DBNAME),
+									literal(DBType.STRING, config.getColumn())),
+								eqSQL(column(AbstractFlexDataManager.DATA_TYPE_DBNAME),
+									literal(DBType.BYTE, AbstractFlexDataManager.STRING_TYPE))));
+					}
+
+					{
+						log.info("Processing CLOB flex data for table '" + config.getTable() + "' and attribute '"
+							+ config.getColumn() + "'.");
+
+						String valueColumn = AbstractFlexDataManager.CLOB_DATA_DBNAME;
+
+						List<SQLColumnDefinition> allColumns = new ArrayList<>();
+						allColumns.add(columnDef(valueColumn));
+						allColumns.addAll(columns);
+
+						process(log, connection, table, valueColumn, allColumns, keyColumns,
+							and(
+								eqSQL(column(AbstractFlexDataManager.TYPE_DBNAME),
+									literal(DBType.STRING, config.getTable())),
+								eqSQL(column(AbstractFlexDataManager.ATTRIBUTE_DBNAME),
+									literal(DBType.STRING, config.getColumn())),
+								eqSQL(column(AbstractFlexDataManager.DATA_TYPE_DBNAME),
+									literal(DBType.BYTE, AbstractFlexDataManager.CLOB_TYPE))));
+					}
+				} else {
+					// Use row data.
+					table = tableDef.getDBMapping().getDBName();
+
+					String valueColumn = attributeDef.getDbMapping()[0].getDBName();
+					columns.add(columnDef(valueColumn));
+
+					if (tableDef.getDBMapping().multipleBranches()) {
+						columns.add(columnDef(BasicTypes.BRANCH_DB_NAME));
+						keyColumns.add(BasicTypes.BRANCH_DB_NAME);
+					}
+
+					columns.add(columnDef(BasicTypes.IDENTIFIER_DB_NAME));
+					keyColumns.add(BasicTypes.IDENTIFIER_DB_NAME);
+
+					columns.add(columnDef(BasicTypes.REV_MAX_DB_NAME));
+					keyColumns.add(BasicTypes.REV_MAX_DB_NAME);
+
+					process(log, connection, table, valueColumn, columns, keyColumns, SQLFactory.literalTrueLogical());
+				}
 			}
 		} catch (SQLException ex) {
-			log.error("Failed to transform values in column '" + column + "' of table '"
-				+ table + "'.", ex);
+			log.error("Failed to transform values in column '" + config.getColumn() + "' of table '"
+				+ config.getTable() + "'.", ex);
 		}
 
+	}
+
+	private void process(Log log, PooledConnection connection, String table, String column,
+			List<SQLColumnDefinition> columns, List<String> keyColumns, SQLExpression where) throws SQLException {
+		SQLQuery<SQLSelect> select = query(select(columns, table(table), where));
+
+		DBHelper sqlDialect = connection.getSQLDialect();
+		CompiledStatement query = select.toSql(sqlDialect);
+		query.setResultSetConfiguration(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE);
+
+		try (ResultSet resultSet = query.executeQuery(connection)) {
+			processRows(log, resultSet, table, column, keyColumns);
+		}
 	}
 
 	private String mangleDBName(String name) {

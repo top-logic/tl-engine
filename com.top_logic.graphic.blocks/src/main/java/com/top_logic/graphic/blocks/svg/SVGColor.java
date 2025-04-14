7f7f5f65718277ae3a67556bf10f227f6840a9e3
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-04-14 12:46:59 +0200

    Ticket #25918: Fixed GWT generation.

---
+++ com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/blocks/svg/SVGColor.java
@@ -0,0 +1,53 @@
+/*
+ * SPDX-FileCopyrightText: 2025 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.graphic.blocks.svg;
+
+/**
+ * A color value.
+ * 
+ * @implNote The color implementation for Java AWT cannot be used, since it is not compatible with
+ *           GWT.
+ */
+public class SVGColor {
+
+	private final int _value;
+
+	/**
+	 * Creates a {@link SVGColor}.
+	 */
+	public SVGColor(int value) {
+		_value = value;
+	}
+
+	/**
+	 * The red color component.
+	 */
+	public int getRed() {
+		return _value & 0xFF0000 >> 16;
+	}
+
+	/**
+	 * The green color component.
+	 */
+	public int getGreen() {
+		return _value & 0x00FF00 >> 8;
+	}
+
+	/**
+	 * The blue color component.
+	 */
+	public int getBlue() {
+		return _value & 0x0000FF >> 0;
+	}
+
+	/**
+	 * The alpha channel.
+	 */
+	public int getAlpha() {
+		return _value & 0xFF000000 >> 24;
+	}
+
+}

9382f14b4c2e18eb729d146bf1574a1adb34109e
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-04-16 11:25:26 +0200

    Ticket #25918: Renamed model package to operations.

--- com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/flow/data/Empty.java
+++ com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/flow/data/Empty.java
@@ -1,6 +1,6 @@
 package com.top_logic.graphic.flow.data;
 
-public interface Empty extends Box, com.top_logic.graphic.flow.model.EmptyOperations {
+public interface Empty extends Box, com.top_logic.graphic.flow.operations.EmptyOperations {
 
 	/**
 	 * Creates a {@link com.top_logic.graphic.flow.data.Empty} instance.

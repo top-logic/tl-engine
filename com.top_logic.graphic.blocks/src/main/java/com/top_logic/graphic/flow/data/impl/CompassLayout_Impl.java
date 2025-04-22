451b7be496d21983b60cdd7805970027c0aff869
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-04-22 17:10:48 +0200

    Ticket #24649: Send updates from client to server.
    Added selection channel.

--- com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/flow/data/impl/CompassLayout_Impl.java
+++ com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/flow/data/impl/CompassLayout_Impl.java
@@ -324,9 +324,18 @@ public class CompassLayout_Impl extends com.top_logic.graphic.flow.data.impl.Box
 			H_PADDING__PROP, 
 			V_PADDING__PROP));
 
+	private static java.util.Set<String> TRANSIENT_PROPERTIES = java.util.Collections.unmodifiableSet(new java.util.HashSet<>(
+			java.util.Arrays.asList(
+				)));
+
 	@Override
 	public java.util.List<String> properties() {
 		return PROPERTIES;
+	}
+
+	@Override
+	public java.util.Set<String> transientProperties() {
+		return TRANSIENT_PROPERTIES;
 	}
 
 	@Override

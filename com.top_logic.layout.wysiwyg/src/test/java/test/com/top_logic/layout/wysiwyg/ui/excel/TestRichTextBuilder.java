365c0bd0e3285f84940e7e4cb51bbcde4b793f86
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-05-02 10:47:57 +0200

    Ticket #28581: Added test case that uses ExcelWriter.

--- com.top_logic.layout.wysiwyg/src/test/java/test/com/top_logic/layout/wysiwyg/ui/excel/TestRichTextBuilder.java
+++ com.top_logic.layout.wysiwyg/src/test/java/test/com/top_logic/layout/wysiwyg/ui/excel/TestRichTextBuilder.java
@@ -14,16 +14,21 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.RichTextString;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRichTextString;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Node;
+import org.junit.Assert;
 
 import com.top_logic.layout.wysiwyg.ui.excel.RichTextBuilder;
 
@@ -33,8 +38,8 @@ import com.top_logic.layout.wysiwyg.ui.excel.RichTextBuilder;
 @SuppressWarnings("javadoc")
 public class TestRichTextBuilder extends TestCase {
 
-	public void testConvert() throws IOException {
-		Workbook workbook = new HSSFWorkbook();
+	public void testConvert() throws IOException, InvalidFormatException {
+		Workbook workbook = createWorkbook();
 		RichTextBuilder builder = new RichTextBuilder(workbook);
 		Node document = Jsoup.parse("<p>Some <b>bold <i>and</i> funny</b> words.</p> <p>And another paragraph.</p>");
 		builder.append(document);
@@ -44,20 +49,29 @@ public class TestRichTextBuilder extends TestCase {
 		//            000000000011111111113333333
 		//            012345678901234567890123456 7 8901234567890012345678
 		assertEquals("Some bold and funny words.\n\nAnd another paragraph.\n", text.toString());
-		assertEquals(Arrays.asList(5, 10, 13, 19), runs(text));
+		assertEquals(Arrays.asList(0, 5, 10, 13, 19), runs(text));
 
-		save(workbook, text, "target/TestRichTextBuilder-simple.xls", builder.getWidth());
+		save(workbook, text, "target/TestRichTextBuilder-simple.xlsx", builder.getWidth());
+
+		try (XSSFWorkbook checkWorkbook = new XSSFWorkbook(new File("target/TestRichTextBuilder-simple.xlsx"))) {
+			XSSFCell cell = checkWorkbook.getSheetAt(0).getRow(0).getCell(0);
+			Assert.assertEquals(CellType.STRING, cell.getCellType());
+			XSSFRichTextString value = cell.getRichStringCellValue();
+			Assert.assertEquals("Some bold and funny words.\n\nAnd another paragraph.\n", value.getString());
+			Assert.assertEquals(5, value.numFormattingRuns());
+			Assert.assertTrue(value.getFontOfFormattingRun(1).getBold());
+		}
 	}
 
 	public void testHeading() throws IOException {
-		Workbook workbook = new HSSFWorkbook();
+		Workbook workbook = createWorkbook();
 		RichTextBuilder builder = new RichTextBuilder(workbook);
 		Node document = Jsoup.parse(
 			"<h1>Heading 1</h1> <p>Some <b>bold</b> content.</p> <h2>Sub-Heading</h2> <p>Some <i>italics</i> content.</p>");
 		builder.append(document);
 		RichTextString text = builder.getText();
 
-		save(workbook, text, "target/TestRichTextBuilder-heading.xls", builder.getWidth());
+		save(workbook, text, "target/TestRichTextBuilder-heading.xlsx", builder.getWidth());
 	}
 
 	private static List<Integer> runs(RichTextString text) {
@@ -69,22 +83,36 @@ public class TestRichTextBuilder extends TestCase {
 		return runs;
 	}
 
-	public void testDummy() throws IOException {
-		Workbook workbook = new HSSFWorkbook();
+	public void testDummy() throws IOException, InvalidFormatException {
+		Workbook workbook = createWorkbook();
 
 		Font font1 = workbook.createFont();
-		Font font2 = workbook.createFont();
-		Font font3 = workbook.createFont();
 		font1.setColor(Font.COLOR_RED);
+		Font font2 = workbook.createFont();
 		font2.setBold(true);
+		Font font3 = workbook.createFont();
 		font3.setItalic(true);
 
 		RichTextString value = workbook.getCreationHelper().createRichTextString("RichTextFormat");
 		value.applyFont(0, 4, font1);
-		value.applyFont(5, 8, font2);
-		value.applyFont(8, 13, font3);
+		value.applyFont(4, 8, font2);
+		value.applyFont(8, 14, font3);
 
-		save(workbook, value, "target/TestRichTextBuilder-dummy.xls", 20);
+		save(workbook, value, "target/TestRichTextBuilder-dummy.xlsx", 20);
+
+		try (XSSFWorkbook checkWorkbook = new XSSFWorkbook(new File("target/TestRichTextBuilder-dummy.xlsx"))) {
+			XSSFCell cell = checkWorkbook.getSheetAt(0).getRow(0).getCell(0);
+			Assert.assertEquals(CellType.STRING, cell.getCellType());
+			XSSFRichTextString checkValue = cell.getRichStringCellValue();
+			Assert.assertEquals("RichTextFormat", checkValue.getString());
+			Assert.assertEquals(3, checkValue.numFormattingRuns());
+			Assert.assertTrue(checkValue.getFontOfFormattingRun(1).getBold());
+		}
+	}
+
+	private Workbook createWorkbook() {
+		// Note: SXSSFWorkbook does not support rich text formatting.
+		return new XSSFWorkbook();
 	}
 
 	private void save(Workbook workbook, RichTextString richTextValue, String fileName, int width) throws IOException {

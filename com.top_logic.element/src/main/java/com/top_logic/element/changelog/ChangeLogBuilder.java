44e0e0ade962ea99568fe032dc1f6ff69ed2ac5f
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-05-05 09:28:49 +0200

    Ticket #28604: Pull out technical change log builder from model builder.

--- com.top_logic.element/src/main/java/com/top_logic/element/changelog/ChangeLogBuilder.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/changelog/ChangeLogBuilder.java
@@ -29,13 +29,10 @@ import com.top_logic.knowledge.event.ObjectCreation;
 import com.top_logic.knowledge.objects.KnowledgeItem;
 import com.top_logic.knowledge.service.HistoryManager;
 import com.top_logic.knowledge.service.KnowledgeBase;
-import com.top_logic.knowledge.service.PersistencyLayer;
 import com.top_logic.knowledge.service.ReaderConfig;
 import com.top_logic.knowledge.service.ReaderConfigBuilder;
 import com.top_logic.knowledge.service.Revision;
 import com.top_logic.knowledge.wrap.person.Person;
-import com.top_logic.mig.html.ListModelBuilder;
-import com.top_logic.mig.html.layout.LayoutComponent;
 import com.top_logic.model.ModelKind;
 import com.top_logic.model.TLClass;
 import com.top_logic.model.TLModel;
@@ -43,126 +40,91 @@ import com.top_logic.model.TLModule;
 import com.top_logic.model.TLObject;
 import com.top_logic.model.TLType;
 import com.top_logic.model.util.TLModelUtil;
-import com.top_logic.util.model.ModelService;
 
 /**
  * 
  */
-public class ChangeLogBuilder implements ListModelBuilder {
+public class ChangeLogBuilder {
 
-	@Override
-	public boolean supportsModel(Object aModel, LayoutComponent aComponent) {
-		return true;
+	private KnowledgeBase _kb;
+
+	private HistoryManager _hm;
+
+	private TLModel _model;
+
+	private Revision _startRev;
+
+	private Revision _stopRev;
+
+	private Map<MOStructure, List<TLClass>> _classesByTable;
+
+	/**
+	 * Creates a {@link ChangeLogBuilder}.
+	 */
+	public ChangeLogBuilder(KnowledgeBase kb, TLModel model) {
+		_kb = kb;
+		_model = model;
+		_hm = kb.getHistoryManager();
+
+		_startRev = _hm.getRevision(1);
+		_stopRev = _hm.getRevision(_hm.getLastRevision());
 	}
 
-	@Override
-	public Collection<?> getModel(Object businessModel, LayoutComponent aComponent) {
+	/**
+	 * The first {@link Revision} to analyze.
+	 */
+	public Revision getStartRev() {
+		return _startRev;
+	}
+
+	/**
+	 * The last {@link Revision} to analyze.
+	 */
+	public Revision getStopRev() {
+		return _stopRev;
+	}
+
+	/**
+	 * @see #getStartRev()
+	 */
+	public ChangeLogBuilder setStartRev(Revision startRev) {
+		_startRev = startRev;
+		return this;
+	}
+
+	/**
+	 * @see #getStopRev()
+	 */
+	public ChangeLogBuilder setStopRev(Revision stopRev) {
+		_stopRev = stopRev;
+		return this;
+	}
+
+	/**
+	 */
+	public Collection<com.top_logic.element.changelog.model.ChangeSet> build() {
 		List<com.top_logic.element.changelog.model.ChangeSet> log = new ArrayList<>();
 
-		Map<MOStructure, List<TLClass>> classesByTable = new HashMap<>();
+		analyzeModel();
 
-		TLModel model = ModelService.getApplicationModel();
-		for (TLModule module : model.getModules()) {
-			for (TLType type : module.getTypes()) {
-				if (type.getModelKind() == ModelKind.CLASS) {
-					TLClass classType = (TLClass) type;
-
-					MOStructure table = TLModelUtil.getTable(classType);
-					classesByTable.computeIfAbsent(table, x -> new ArrayList<>()).add(classType);
-				}
-			}
-		}
-
-		KnowledgeBase kb = PersistencyLayer.getKnowledgeBase();
-		HistoryManager hm = kb.getHistoryManager();
-		Revision startRev = hm.getRevisionAt(System.currentTimeMillis() - 1000 * 60 * 60 * 24);
-		if (startRev.getCommitNumber() < 1) {
-			startRev = hm.getRevision(1);
-		}
-		Revision stopRev = hm.getRevision(hm.getLastRevision());
-		ReaderConfig readerConfig = ReaderConfigBuilder.createConfig(startRev, stopRev);
-		ChangeSetReader reader = kb.getChangeSetReader(readerConfig);
+		ReaderConfig readerConfig = ReaderConfigBuilder.createConfig(_startRev, _stopRev);
+		ChangeSetReader reader = _kb.getChangeSetReader(readerConfig);
 		while (true) {
 			ChangeSet changeSet = reader.read();
 			if (changeSet == null) {
 				break;
 			}
 
-			Revision revision = hm.getRevision(changeSet.getRevision());
-			String authorSpec = revision.getAuthor();
-			Person author;
-			if (authorSpec.startsWith("person:")) {
-				author = kb.resolveObjectKey(
-					new DefaultObjectKey(
-						hm.getTrunk().getBranchId(), revision.getCommitNumber(),
-						kb.getMORepository().getMetaObject(Person.OBJECT_NAME),
-						LongID.fromExternalForm(authorSpec.substring("person:".length()))))
-					.getWrapper();
-			} else {
-				author = null;
-			}
+			Revision revision = _hm.getRevision(changeSet.getRevision());
 
 			TransientChangeSet entry = new TransientChangeSet();
 			entry.setDate(new Date(revision.getDate()));
 			entry.setRevision(revision);
 			entry.setMessage(revision.getLog());
-			entry.setAuthor(author);
+			entry.setAuthor(resolveAuthor(revision));
 
-			List<ObjectCreation> creations = changeSet.getCreations();
-
-			Set<ObjectKey> createdKeys = creations.stream().map(c -> c.getOriginalObject()).collect(Collectors.toSet());
-
-			for (ObjectCreation creation : creations) {
-				MetaObject table = creation.getObjectType();
-
-				List<TLClass> classes = classesByTable.get(table);
-				if (classes == null) {
-					continue;
-				}
-
-				KnowledgeItem item = kb.resolveObjectKey(creation.getOriginalObject());
-				TLObject object = item.getWrapper();
-
-				TLObject container = object.tContainer();
-				if (container != null && createdKeys.contains(container.tId())) {
-					// Only a part of some other object, ignore.
-					continue;
-				}
-
-				TransientCreation change = new TransientCreation();
-				change.setObject(object);
-
-				entry.addChange(change);
-			}
-
-			List<ItemDeletion> deletions = changeSet.getDeletions();
-
-			Set<ObjectKey> deletedKeys = deletions.stream()
-				.map(c -> c.getObjectId().toObjectKey(changeSet.getRevision() - 1)).collect(Collectors.toSet());
-
-			for (ItemDeletion deletion : deletions) {
-				MetaObject table = deletion.getObjectType();
-
-				List<TLClass> classes = classesByTable.get(table);
-				if (classes == null) {
-					continue;
-				}
-
-				KnowledgeItem item =
-					kb.resolveObjectKey(deletion.getObjectId().toObjectKey(changeSet.getRevision() - 1));
-				TLObject object = item.getWrapper();
-
-				TLObject container = object.tContainer();
-				if (container != null && deletedKeys.contains(container.tId())) {
-					// Only a part of some other object, ignore.
-					continue;
-				}
-
-				TransientDeletion change = new TransientDeletion();
-				change.setObject(object);
-
-				entry.addChange(change);
-			}
+			analyzeCreations(changeSet, entry);
+			analyzeDeletions(changeSet, entry);
 
 			log.add(entry);
 		}
@@ -170,13 +132,110 @@ public class ChangeLogBuilder implements ListModelBuilder {
 		return log;
 	}
 
-	@Override
-	public boolean supportsListElement(LayoutComponent component, Object candidate) {
-		return candidate instanceof com.top_logic.element.changelog.model.ChangeSet;
+	/**
+	 * Analyzes creations in the given {@link ChangeSet} and transfer them to the given model change
+	 * set.
+	 */
+	private void analyzeCreations(ChangeSet changeSet, TransientChangeSet entry) {
+		List<ObjectCreation> creations = changeSet.getCreations();
+
+		// All object IDs of objects created in the current change set.
+		Set<ObjectKey> createdKeys = creations.stream().map(c -> c.getOriginalObject()).collect(Collectors.toSet());
+
+		for (ObjectCreation creation : creations) {
+			MetaObject table = creation.getObjectType();
+
+			List<TLClass> classes = _classesByTable.get(table);
+			if (classes == null) {
+				// Only a technical object, ignore.
+				continue;
+			}
+
+			KnowledgeItem item = _kb.resolveObjectKey(creation.getOriginalObject());
+			TLObject object = item.getWrapper();
+
+			TLObject container = object.tContainer();
+			if (container != null && createdKeys.contains(container.tId())) {
+				// Only a part of some other object, ignore.
+				continue;
+			}
+
+			// Record a creation.
+			TransientCreation change = new TransientCreation();
+			change.setObject(object);
+
+			entry.addChange(change);
+		}
 	}
 
-	@Override
-	public Object retrieveModelFromListElement(LayoutComponent component, Object candidate) {
-		return null;
+	private void analyzeDeletions(ChangeSet changeSet, TransientChangeSet entry) {
+		List<ItemDeletion> deletions = changeSet.getDeletions();
+
+		Set<ObjectKey> deletedKeys = deletions.stream()
+			.map(c -> c.getObjectId().toObjectKey(changeSet.getRevision() - 1)).collect(Collectors.toSet());
+
+		for (ItemDeletion deletion : deletions) {
+			MetaObject table = deletion.getObjectType();
+
+			List<TLClass> classes = _classesByTable.get(table);
+			if (classes == null) {
+				// Only a technical object, ignore.
+				continue;
+			}
+
+			KnowledgeItem item =
+				_kb.resolveObjectKey(deletion.getObjectId().toObjectKey(changeSet.getRevision() - 1));
+			TLObject object = item.getWrapper();
+
+			TLObject container = object.tContainer();
+			if (container != null && deletedKeys.contains(container.tId())) {
+				// Only a part of some other object, ignore.
+				continue;
+			}
+
+			// Record a deletion.
+			TransientDeletion change = new TransientDeletion();
+			change.setObject(object);
+
+			entry.addChange(change);
+		}
+	}
+
+	/**
+	 * Lookup the account that was the author of the given {@link Revision}, or <code>null</code>
+	 * for a technical transaction.
+	 */
+	private Person resolveAuthor(Revision revision) {
+		String authorSpec = revision.getAuthor();
+		Person author;
+		if (authorSpec.startsWith("person:")) {
+			author = _kb.resolveObjectKey(
+				new DefaultObjectKey(
+					_hm.getTrunk().getBranchId(), revision.getCommitNumber(),
+					_kb.getMORepository().getMetaObject(Person.OBJECT_NAME),
+					LongID.fromExternalForm(authorSpec.substring("person:".length()))))
+				.getWrapper();
+		} else {
+			author = null;
+		}
+		return author;
+	}
+
+	/**
+	 * Analyze the application model to map technical changes to model changes.
+	 */
+	private void analyzeModel() {
+		_classesByTable = new HashMap<>();
+
+		for (TLModule module : _model.getModules()) {
+			for (TLType type : module.getTypes()) {
+				if (type.getModelKind() == ModelKind.CLASS) {
+					TLClass classType = (TLClass) type;
+
+					MOStructure table = TLModelUtil.getTable(classType);
+					_classesByTable.computeIfAbsent(table, x -> new ArrayList<>()).add(classType);
+				}
+			}
+		}
 	}
 }

7e9a715839b1f26397b991bec093a9fdbcdc818a
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-05-06 10:39:18 +0200

    Ticket #28604: Enhanced storage documentation.

--- com.top_logic.element/src/main/java/com/top_logic/element/meta/kbbased/storage/ColumnStorage.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/meta/kbbased/storage/ColumnStorage.java
@@ -6,12 +6,19 @@
 package com.top_logic.element.meta.kbbased.storage;
 
 import com.top_logic.dob.MOAttribute;
+import com.top_logic.element.meta.SeparateTableStorage;
 import com.top_logic.element.meta.StorageImplementation;
 import com.top_logic.model.access.WithStorageAttribute;
 
 /**
  * {@link StorageImplementation} that stores values in a column of the object's table.
  * 
+ * <p>
+ * If a storage does not implement this interface and does not represent a derived attribute, it
+ * must implement {@link SeparateTableStorage}.
+ * </p>
+ * 
+ * @see SeparateTableStorage
  * @see WithStorageAttribute
  */
 public interface ColumnStorage extends StorageImplementation {

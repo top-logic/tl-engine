47ff8ebd4070a5eb3d19b34d7dc3b7ef6384e167
Isabell Wittich <isabell.wittich@top-logic.com>
2025-05-15 12:05:31 +0200

    Ticket #28568: Extract observer to an own class.

--- com.top_logic.element/src/main/java/com/top_logic/element/meta/form/DefaultAttributeFormFactory.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/meta/form/DefaultAttributeFormFactory.java
@@ -25,7 +25,6 @@ import com.top_logic.basic.IdentifierUtil;
 import com.top_logic.basic.Logger;
 import com.top_logic.basic.StringServices;
 import com.top_logic.basic.col.Mapping;
-import com.top_logic.basic.col.Sink;
 import com.top_logic.basic.col.TypedAnnotatable;
 import com.top_logic.basic.col.TypedAnnotatable.Property;
 import com.top_logic.basic.config.PolymorphicConfiguration;
@@ -38,7 +37,6 @@ import com.top_logic.basic.xml.TagWriter;
 import com.top_logic.element.meta.AttributeOperations;
 import com.top_logic.element.meta.AttributeUpdate;
 import com.top_logic.element.meta.AttributeUpdateContainer;
-import com.top_logic.element.meta.AttributeUpdateContainer.Handle;
 import com.top_logic.element.meta.LegacyTypeCodes;
 import com.top_logic.element.meta.kbbased.filtergen.Generator;
 import com.top_logic.knowledge.service.KBUtils;
@@ -75,8 +73,6 @@ import com.top_logic.model.annotate.ui.CssClassProvider;
 import com.top_logic.model.annotate.ui.PDFRendererAnnotation;
 import com.top_logic.model.annotate.ui.TLCssClass;
 import com.top_logic.model.annotate.util.ConstraintCheck;
-import com.top_logic.model.form.definition.FormVisibility;
-import com.top_logic.model.util.Pointer;
 import com.top_logic.tool.boundsec.BoundCommandGroup;
 import com.top_logic.tool.boundsec.BoundObject;
 import com.top_logic.tool.boundsec.BoundRole;
@@ -228,7 +224,6 @@ public class DefaultAttributeFormFactory extends AttributeFormFactoryBase {
 			if (result instanceof FormField) {
 				FormField field = (FormField) result;
 
-
 				TLConstraints annotation = attribute.getAnnotation(TLConstraints.class);
 				if (annotation != null) {
 					List<ConstraintCheck> checks = TypedConfiguration.getInstanceList(
@@ -260,48 +255,8 @@ public class DefaultAttributeFormFactory extends AttributeFormFactoryBase {
 
 		TLObject object = update.getOverlay();
 
-		class Observer implements ValueListener, Sink<Pointer> {
-			private final List<AttributeUpdateContainer.Handle> _handles = new ArrayList<>();
-
-			@Override
-			public void valueChanged(FormField changedField, Object oldValue, Object newValue) {
-				FormVisibility mode = modeSelector.getMode(object, attribute);
-				mode.applyTo(member);
-				if (member instanceof FormField) {
-					switch (mode) {
-						case READ_ONLY:
-							/* Reset to original value to prevent modifying values by temporarily
-							 * activating fields. */
-							((FormField) member).reset();
-							break;
-						case HIDDEN:
-							/* Clear value to prevent leaking irrelevant values into the model. */
-							((FormField) member).setValue(null);
-							break;
-						default:
-							break;
-					}
-				}
-
-				removeListeners();
-
-				modeSelector.traceDependencies(object, attribute, this, updateContainer.getFormContext());
-			}
-
-			private void removeListeners() {
-				for (Handle handle : _handles) {
-					handle.release();
-				}
-				_handles.clear();
-			}
-
-			@Override
-			public void add(Pointer p) {
-				_handles.add(updateContainer.addValueListener(p.object(), p.attribute(), this));
-			}
-		}
-
-		new Observer().valueChanged(null, null, null);
+		new FieldModeObserver(member, updateContainer, modeSelector, object, attribute).valueChanged(null,
+				null, null);
 	}
 
 	private Constraint toFormConstraint(AttributeUpdate update, final AttributeUpdateContainer updateContainer,
---
+++ com.top_logic.element/src/main/java/com/top_logic/element/meta/form/ModeObserver.java
@@ -0,0 +1,93 @@
+/*
+ * SPDX-FileCopyrightText: 2025 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+package com.top_logic.element.meta.form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.top_logic.basic.col.Sink;
+import com.top_logic.element.meta.AttributeUpdateContainer;
+import com.top_logic.element.meta.AttributeUpdateContainer.Handle;
+import com.top_logic.layout.form.FormField;
+import com.top_logic.layout.form.ValueListener;
+import com.top_logic.model.TLObject;
+import com.top_logic.model.TLStructuredTypePart;
+import com.top_logic.model.annotate.ModeSelector;
+import com.top_logic.model.form.definition.FormVisibility;
+import com.top_logic.model.util.Pointer;
+
+/**
+ * This class serves as a listener that listens for changes to a list of model references. Based on
+ * the changes in the models, the class computes the {@link FormVisibility} dynamically.
+ * 
+ * The listener uses the {@link ModeSelector} to determine how a given element should be displayed
+ * or hidden, depending on the state or changes of the models being referenced. Each time a model is
+ * updated, the listener recalculates the visibility of the form elements based on the selected
+ * mode.
+ * 
+ * The class maintains a list of model references, which are monitored for changes. Upon detecting
+ * any updates, the listener triggers a re-evaluation of the visibility conditions.
+ * 
+ * @author <a href="mailto:iwi@top-logic.com">Isabell Wittich</a>
+ */
+public abstract class ModeObserver implements ValueListener, Sink<Pointer> {
+
+	private final List<AttributeUpdateContainer.Handle> _handles = new ArrayList<>();
+
+	private AttributeUpdateContainer _updateContainer;
+
+	private ModeSelector _modeSelector;
+
+	private TLObject _object;
+
+	private TLStructuredTypePart _attribute;
+
+	/**
+	 * Creates a {@link ModeObserver}.
+	 * 
+	 * @param updateContainer
+	 *        Container to get the values of.
+	 * @param modeSelector
+	 *        Calculates the actual {@link FormVisibility}.
+	 * @param object
+	 *        Model to observe.
+	 * @param attribute
+	 *        The attribute to observe. Maybe <code>null</code>.
+	 */
+	public ModeObserver(AttributeUpdateContainer updateContainer, ModeSelector modeSelector, TLObject object,
+			TLStructuredTypePart attribute) {
+		_updateContainer = updateContainer;
+		_modeSelector = modeSelector;
+		_object = object;
+		_attribute = attribute;
+	}
+
+	@Override
+	public void valueChanged(FormField changedField, Object oldValue, Object newValue) {
+		FormVisibility mode = _modeSelector.getMode(_object, _attribute);
+		valueChanged(mode);
+
+		removeListeners();
+		_modeSelector.traceDependencies(_object, _attribute, this, _updateContainer.getFormContext());
+	}
+
+	/**
+	 * Handles the changes of the visibility depending on the given {@link FormVisibility}.
+	 */
+	public abstract void valueChanged(FormVisibility mode);
+
+	private void removeListeners() {
+		for (Handle handle : _handles) {
+			handle.release();
+		}
+		_handles.clear();
+	}
+
+	@Override
+	public void add(Pointer p) {
+		_handles.add(_updateContainer.addValueListener(p.object(), p.attribute(), this));
+	}
+}

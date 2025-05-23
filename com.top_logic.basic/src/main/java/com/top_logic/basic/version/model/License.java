d5f295f42f5176ca4608216d94b945abe50d3b3f
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-05-23 15:42:53 +0200

    Ticket #24649: Updated more msgbuf generated files.

--- com.top_logic.basic/src/main/java/com/top_logic/basic/version/model/License.java
+++ com.top_logic.basic/src/main/java/com/top_logic/basic/version/model/License.java
@@ -124,9 +124,18 @@ public class License extends de.haumacher.msgbuf.data.AbstractDataObject impleme
 			URL__PROP, 
 			COMMENTS__PROP));
 
+	private static java.util.Set<String> TRANSIENT_PROPERTIES = java.util.Collections.unmodifiableSet(new java.util.HashSet<>(
+			java.util.Arrays.asList(
+				)));
+
 	@Override
 	public java.util.List<String> properties() {
 		return PROPERTIES;
+	}
+
+	@Override
+	public java.util.Set<String> transientProperties() {
+		return TRANSIENT_PROPERTIES;
 	}
 
 	@Override

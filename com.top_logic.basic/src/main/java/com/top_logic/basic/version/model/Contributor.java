919f5d7470e8c383e749dff98a97e7483a766451
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-05-23 15:42:53 +0200

    Ticket #25918: Updated more msgbuf generated files.

--- com.top_logic.basic/src/main/java/com/top_logic/basic/version/model/Contributor.java
+++ com.top_logic.basic/src/main/java/com/top_logic/basic/version/model/Contributor.java
@@ -190,9 +190,18 @@ public class Contributor extends de.haumacher.msgbuf.data.AbstractDataObject imp
 			ORGANIZATION__PROP, 
 			ORGANIZATION_URL__PROP));
 
+	private static java.util.Set<String> TRANSIENT_PROPERTIES = java.util.Collections.unmodifiableSet(new java.util.HashSet<>(
+			java.util.Arrays.asList(
+				)));
+
 	@Override
 	public java.util.List<String> properties() {
 		return PROPERTIES;
+	}
+
+	@Override
+	public java.util.Set<String> transientProperties() {
+		return TRANSIENT_PROPERTIES;
 	}
 
 	@Override

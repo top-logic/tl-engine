66928174d1f3a7657331037c31d5fe304da61f6d
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-06-04 17:13:24 +0200

    Ticket #25918: Allow to transform images.

--- com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/flow/operations/ImageOperations.java
+++ com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/flow/operations/ImageOperations.java
@@ -5,8 +5,10 @@
  */
 package com.top_logic.graphic.flow.operations;
 
+import com.top_logic.graphic.blocks.math.Mat;
 import com.top_logic.graphic.blocks.svg.RenderContext;
 import com.top_logic.graphic.blocks.svg.SvgWriter;
+import com.top_logic.graphic.flow.data.Image;
 
 /**
  * Embeds another image.
@@ -32,8 +34,68 @@ public interface ImageOperations extends BoxOperations {
 
 	@Override
 	default void draw(SvgWriter out) {
-		out.image(self().getX(), self().getY(), self().getWidth(), self().getHeight(), self().getHref(),
-			self().getAlign(), self().getScale());
+		Image self = self();
+
+		switch (self.getOrientation()) {
+			case NORMAL:
+				writeImage(out, self);
+				break;
+			case FLIP_H: {
+				out.beginGroup();
+				out.transform(Mat.translate(self.getWidth(), 0).apply(Mat.FLIP_X));
+				writeImage(out, self);
+				out.endGroup();
+				break;
+			}
+			case FLIP_V: {
+				out.beginGroup();
+				out.transform(Mat.translate(0, self.getHeight()).apply(Mat.FLIP_Y));
+				writeImage(out, self);
+				out.endGroup();
+				break;
+			}
+			case ROTATE_90: {
+				out.beginGroup();
+				out.transform(Mat.translate(self.getWidth(), 0).apply(Mat.ROT_90));
+				writeImage(out, self);
+				out.endGroup();
+				break;
+			}
+			case ROTATE_180: {
+				out.beginGroup();
+				out.transform(Mat.translate(self.getWidth(), self.getHeight()).apply(Mat.ROT_180));
+				writeImage(out, self);
+				out.endGroup();
+				break;
+			}
+			case FLIP_H_ROTATE_90: {
+				out.beginGroup();
+				out.transform(Mat.translate(self.getWidth(), self.getHeight()).apply(Mat.FLIP_X_ROT_90));
+				writeImage(out, self);
+				out.endGroup();
+				break;
+			}
+			case FLIP_H_ROTATE_270: {
+				out.beginGroup();
+				out.transform(Mat.translate(0, 0).apply(Mat.FLIP_X_ROT_270));
+				writeImage(out, self);
+				out.endGroup();
+				break;
+			}
+			case ROTATE_270: {
+				out.beginGroup();
+				out.transform(Mat.translate(0, self.getHeight()).apply(Mat.ROT_270));
+				writeImage(out, self);
+				out.endGroup();
+				break;
+			}
+		}
+	}
+
+	/** private */
+	default void writeImage(SvgWriter out, Image self) {
+		out.image(self.getX(), self.getY(), self.getWidth(), self.getHeight(), self.getHref(),
+			self.getAlign(), self.getScale());
 	}
 
 }

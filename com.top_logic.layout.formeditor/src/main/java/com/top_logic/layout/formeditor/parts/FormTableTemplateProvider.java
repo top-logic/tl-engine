34367f40bdbe86045d49598f53fe6d5c26725c55
Jonathan Huesing <jonathan.huesing@top-logic.com>
2025-07-16 09:06:06 +0200

    Ticket #28588: add option to make form table selectable
    - add config option
    - overwrite label description for FormTable

--- com.top_logic.layout.formeditor/src/main/java/com/top_logic/layout/formeditor/parts/FormTableTemplateProvider.java
+++ com.top_logic.layout.formeditor/src/main/java/com/top_logic/layout/formeditor/parts/FormTableTemplateProvider.java
@@ -176,6 +176,8 @@ public class FormTableTemplateProvider extends AbstractFormElementProvider<FormT
 	 */
 	private final QueryExecutor _dynamicLabelExecutor;
 
+	private final boolean _selectable;
+
 	/**
 	 * Create a new {@link FormTableTemplateProvider} for a {@link FormTableDefinition} in a given
 	 * {@link InstantiationContext}.
@@ -184,6 +186,7 @@ public class FormTableTemplateProvider extends AbstractFormElementProvider<FormT
 		super(context, config);
 		_rowsExecutor = QueryExecutor.compileOptional(config.getRows());
 		_dynamicLabelExecutor = QueryExecutor.compileOptional(config.getDynamicLabel());
+		_selectable = config.getSelectable();
 	}
 
 	@Override
@@ -274,7 +277,7 @@ public class FormTableTemplateProvider extends AbstractFormElementProvider<FormT
 		} else {
 			tableField = FormFactory.newTableField(fieldName);
 		}
-		tableField.setSelectable(true);
+		tableField.setSelectable(_selectable);
 		tableField.setSelectionModel(new DefaultMultiSelectionModel(tableField));
 		return tableField;
 	}

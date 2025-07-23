90c1f298d40ee548e0a1a37fbd32ad9613726d9a
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-07-23 07:53:46 +0200

    Ticket #28346: Fixed file header.

--- com.top_logic/src/main/java/com/top_logic/tool/boundsec/CommandHandlerFormat.java
+++ com.top_logic/src/main/java/com/top_logic/tool/boundsec/CommandHandlerFormat.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2013 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
 package com.top_logic.tool.boundsec;
 
 import com.top_logic.basic.config.AbstractConfigurationValueProvider;

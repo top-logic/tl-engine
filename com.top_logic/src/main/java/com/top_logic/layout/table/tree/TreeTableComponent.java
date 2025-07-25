318140ce82590344850beb700535fba6cca3e2c5
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-07-25 09:41:33 +0200

    Ticket #28727: Removed left-over command.

--- com.top_logic/src/main/java/com/top_logic/layout/table/tree/TreeTableComponent.java
+++ com.top_logic/src/main/java/com/top_logic/layout/table/tree/TreeTableComponent.java
@@ -247,7 +247,6 @@ public class TreeTableComponent extends BoundComponent
 			BoundComponent.Config.super.modifyIntrinsicCommands(registry);
 			registry.registerButton(TreeTableExpandCollapseAll.EXPAND_ID);
 			registry.registerButton(TreeTableExpandCollapseAll.COLLAPSE_ID);
-			registry.registerButton(SelectSubtree.SELECT_SUBTREE_ID);
 		}
 	}
 

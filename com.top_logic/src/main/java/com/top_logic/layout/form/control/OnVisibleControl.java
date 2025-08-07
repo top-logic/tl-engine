c1d486d9735b730e9801a91d235b752eb1844c57
Daniel Busche <daniel.busche@top-logic.com>
2025-08-07 15:03:36 +0200

    Ticket #28740: OnVisibleControl needs only a VisiblityModel.

--- com.top_logic/src/main/java/com/top_logic/layout/form/control/OnVisibleControl.java
+++ com.top_logic/src/main/java/com/top_logic/layout/form/control/OnVisibleControl.java
@@ -9,64 +9,64 @@ import java.util.Collections;
 
 import com.top_logic.basic.listener.EventType.Bubble;
 import com.top_logic.layout.basic.ControlCommand;
-import com.top_logic.layout.form.FormMember;
+import com.top_logic.layout.form.model.VisibilityModel;
 import com.top_logic.mig.html.layout.VisibilityListener;
 
 /**
  * A {@link OnVisibleControl} is a control whose visibility is exactly that of a given
- * {@link FormMember}
+ * {@link VisibilityModel}.
  * 
  * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
  */
 public class OnVisibleControl extends AbstractCompositeControl<OnVisibleControl> {
 
 	/**
-	 * Listener attached to the given {@link #_member}
+	 * Listener attached to the given {@link #_model}
 	 * 
 	 * @see VisibilityListenerImpl
 	 */
 	private final VisibilityListener visibilityListener = new VisibilityListenerImpl();
 
 	/**
-	 * the {@link FormMember} which has the same visibility as this control
+	 * The {@link VisibilityModel} which has the same visibility as this control.
 	 */
-	final FormMember _member;
+	private final VisibilityModel _model;
 
 	/**
-	 * Creates a new {@link OnVisibleControl} which reacts on the visibility of the given member,
-	 * i.e. {@link FormMember#isVisible()} and {@link OnVisibleControl#isVisible()} have the same
-	 * value if this control is attached.
+	 * Creates a new {@link OnVisibleControl} which reacts on the visibility of the given model,
+	 * i.e. {@link VisibilityModel#isVisible()} and {@link OnVisibleControl#isVisible()} have the
+	 * same value if this control is attached.
 	 * 
-	 * @param member
-	 *        the {@link FormMember} with the same visibility. must not be <code>null</code>
+	 * @param model
+	 *        The {@link VisibilityModel} with the same visibility. Must not be <code>null</code>
 	 */
-	public OnVisibleControl(FormMember member) {
+	public OnVisibleControl(VisibilityModel model) {
 		super(Collections.<String, ControlCommand> emptyMap());
-		_member = member;
-		if (_member == null) {
-			throw new IllegalArgumentException("Given " + FormMember.class.getName() + " must not be null");
+		_model = model;
+		if (_model == null) {
+			throw new IllegalArgumentException("Given " + VisibilityModel.class.getName() + " must not be null");
 		}
 	}
 
 	@Override
-	public FormMember getModel() {
-		return _member;
+	public VisibilityModel getModel() {
+		return _model;
 	}
 
 	@Override
 	public boolean isVisible() {
-		return _member.isVisible();
+		return _model.isVisible();
 	}
 
 	@Override
 	protected void internalAttach() {
 		super.internalAttach();
-		_member.addListener(FormMember.VISIBLE_PROPERTY, visibilityListener);
+		_model.addListener(VisibilityModel.VISIBLE_PROPERTY, visibilityListener);
 	}
 
 	@Override
 	protected void internalDetach() {
-		_member.removeListener(FormMember.VISIBLE_PROPERTY, visibilityListener);
+		_model.removeListener(VisibilityModel.VISIBLE_PROPERTY, visibilityListener);
 		super.internalDetach();
 	}
 
@@ -78,8 +78,6 @@ public class OnVisibleControl extends AbstractCompositeControl<OnVisibleControl>
 	/**
 	 * Listener which forces the enclosing control to redraw if the visibility of the model, this
 	 * listener is attached to, changes.
-	 * 
-	 * @author <a href=mailto:daniel.busche@top-logic.com>Daniel Busche</a>
 	 */
 	private final class VisibilityListenerImpl implements VisibilityListener {
 
@@ -89,9 +87,9 @@ public class OnVisibleControl extends AbstractCompositeControl<OnVisibleControl>
 
 		@Override
 		public Bubble handleVisibilityChange(Object sender, Boolean oldVisibility, Boolean newVisibility) {
-			if (sender != OnVisibleControl.this._member) {
-				/* Don't care about change of visibility of member different from the observed one.
-				 * In case the observed member is a FormGroup, also visibility changes of inner
+			if (sender != OnVisibleControl.this.getModel()) {
+				/* Don't care about change of visibility of model different from the observed one.
+				 * In case the observed model is a FormGroup, also visibility changes of inner
 				 * FormFields are propagated. */
 				return Bubble.BUBBLE;
 			}

fc692c096da5f351c2476e089e204e91da3405e0
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-09-02 16:57:59 +0200

    Ticket #25918: Refactoring, moved complete layouting code to render info.

--- com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/flow/operations/tree/TreeRenderInfo.java
+++ com.top_logic.graphic.blocks/src/main/java/com/top_logic/graphic/flow/operations/tree/TreeRenderInfo.java
@@ -86,10 +86,31 @@ public class TreeRenderInfo {
 
 	private Map<Box, List<Box>> _childrenForParentNode;
 
+	private double _gapY;
+
+	private boolean _compact;
+
+	private double _parentAlign;
+
+	private double _parentOffset;
+
+	private double _gapX;
+
+	private double _width;
+
+	private double _height;
+
 	/**
 	 * Creates a {@link TreeRenderInfo}.
 	 */
-	public TreeRenderInfo(List<Box> nodes, List<TreeConnection> connections) {
+	public TreeRenderInfo(boolean compact, double gapX, double gapY, double parentAlign, double parentOffset,
+			List<Box> nodes,
+			List<TreeConnection> connections) {
+		_compact = compact;
+		_gapX = gapX;
+		_gapY = gapY;
+		_parentAlign = parentAlign;
+		_parentOffset = parentOffset;
 		Set<Box> nodeSet = new HashSet<>(nodes);
 
 		_nodeForAnchor = new HashMap<>();
@@ -211,6 +232,145 @@ public class TreeRenderInfo {
 		for (Box child : getChildren(node)) {
 			shiftY(child, shiftY);
 		}
+	}
+
+	/**
+	 * Arranges nodes in columns so that a parent node is placed in a column left to the column of
+	 * its children.
+	 */
+	public void computeLayout() {
+		double bottomY = -_gapY;
+
+		// Enter tree nodes in columns and assign reasonable Y coordinates to nodes.
+		for (Box root : getRoots()) {
+			bottomY = layoutTree(0, root, bottomY);
+		}
+
+		// Compute column widths.
+		double minX = 0;
+		for (Column column : getColumns()) {
+			double maxWidth = 0;
+			for (Box node : column.getBoxes()) {
+				maxWidth = Math.max(maxWidth, node.getWidth());
+			}
+
+			column._width = maxWidth;
+
+			minX += maxWidth;
+			minX += _gapX;
+		}
+
+		_width = minX - (getColumns().isEmpty() ? 0.0 : _gapX);
+		_height = bottomY;
+	}
+
+	/**
+	 * Helper method to layout a single tree within a forest.
+	 * 
+	 * @param parentBottomY
+	 *        The maximum Y coordinate used among all nodes placed so far (the parent and all of the
+	 *        current node's preceding siblings and their sub-trees.
+	 */
+	private double layoutTree(int level, Box node, double parentBottomY) {
+		Column column = mkColumn(level);
+
+		// The minimum Y coordinate, where the current node can be placed in its column.
+		double minY;
+		if (column.getBoxes().size() > 0) {
+			Box last = column.getBoxes().get(column.getBoxes().size() - 1);
+			minY = last.getBottomY() + _gapY;
+		} else {
+			minY = 0.0;
+		}
+
+		if (_compact) {
+			minY = Math.max(parentBottomY, minY);
+		}
+
+		column.addBox(node);
+
+		double bottomY;
+
+		List<Box> nextLevel = getChildren(node);
+		if (nextLevel.isEmpty()) {
+			// This is a leaf node.
+			if (!_compact) {
+				for (int l = level + 1; l < getColumns().size(); l++) {
+					Column down = getColumns().get(l);
+					if (down.size() > 0) {
+						Box bottom = down.getBox(down.size() - 1);
+						minY = Math.max(minY, bottom.getBottomY() + _gapY);
+					}
+				}
+			}
+
+			// Place node at the minimum Y coordinate possible.
+			node.setY(minY);
+			bottomY = minY + node.getHeight();
+		} else {
+			// Recursively place all children.
+			double childBottomY = minY;
+			for (Box child : nextLevel) {
+				childBottomY = layoutTree(level + 1, child, childBottomY);
+			}
+
+			// Place parent node relative to its direct children.
+			Box firstChild = nextLevel.get(0);
+			Box lastChild = nextLevel.get(nextLevel.size() - 1);
+
+			Box firstAnchor = getAnchor(firstChild);
+			Box lastAnchor = getAnchor(lastChild);
+
+			// Note: Anchor boxes are up to this point relative to the node's coordinate.
+			double firstY = firstChild.getY() + firstAnchor.getY() + 0.5 * firstAnchor.getHeight();
+			double lastY = lastChild.getY() + lastAnchor.getY() + 0.5 * lastAnchor.getHeight();
+
+			// The Y coordinate of the center of the node's anchor box.
+			double anchorY = firstY + (lastY - firstY) * _parentAlign + _parentOffset;
+
+			Box nodeAnchor = getAnchor(node);
+
+			double nodeY = anchorY - nodeAnchor.getY() - 0.5 * nodeAnchor.getHeight();
+
+			if (nodeY >= minY) {
+				// Current node must be moved to the bottom to align with its children.
+				minY = nodeY;
+			} else {
+				// Children must be moved to the bottom to align with the current node.
+
+				// nodeY < minY: Amount to shift the subtree to the bottom.
+				double shiftY = minY - nodeY;
+				for (Box child : nextLevel) {
+					shiftY(child, shiftY);
+				}
+				childBottomY += shiftY;
+
+				nodeY += shiftY;
+			}
+			node.setY(nodeY);
+
+			bottomY = Math.max(node.getBottomY(), childBottomY);
+		}
+
+		return bottomY;
+	}
+
+	/**
+	 * TODO
+	 *
+	 * @return
+	 */
+	public double getWidth() {
+		return _width;
+	}
+
+	/**
+	 * TODO
+	 *
+	 * @return
+	 */
+	public double getHeight() {
+		return _height;
 	}
 
 }

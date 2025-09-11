3a363b234df09bb1ae4d99b8a39167ebb3b99977
Daniel Busche <daniel.busche@top-logic.com>
2025-09-11 15:42:04 +0200

    Ticket #28604: Regenerated classes for *.proto files.

--- com.top_logic.basic/src/main/java/com/top_logic/basic/version/model/License.java
+++ com.top_logic.basic/src/main/java/com/top_logic/basic/version/model/License.java
@@ -118,15 +118,22 @@ public class License extends de.haumacher.msgbuf.data.AbstractDataObject impleme
 		return LICENSE__TYPE;
 	}
 
-	private static java.util.List<String> PROPERTIES = java.util.Collections.unmodifiableList(
-		java.util.Arrays.asList(
+	static final java.util.List<String> PROPERTIES;
+	static {
+		java.util.List<String> local = java.util.Arrays.asList(
 			NAME__PROP, 
 			URL__PROP, 
-			COMMENTS__PROP));
+			COMMENTS__PROP);
+		PROPERTIES = java.util.Collections.unmodifiableList(local);
+	}
 
-	private static java.util.Set<String> TRANSIENT_PROPERTIES = java.util.Collections.unmodifiableSet(new java.util.HashSet<>(
-			java.util.Arrays.asList(
-				)));
+	static final java.util.Set<String> TRANSIENT_PROPERTIES;
+	static {
+		java.util.HashSet<String> tmp = new java.util.HashSet<>();
+		tmp.addAll(java.util.Arrays.asList(
+				));
+		TRANSIENT_PROPERTIES = java.util.Collections.unmodifiableSet(tmp);
+	}
 
 	@Override
 	public java.util.List<String> properties() {

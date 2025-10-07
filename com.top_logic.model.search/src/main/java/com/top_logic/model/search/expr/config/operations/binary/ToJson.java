711814e6fbc37a00fe40f21cc784fb2711dadcda
Jonathan Huesing <jonathan.huesing@top-logic.com>
2025-10-07 14:10:50 +0200

    Ticket #28924: Correct copyright comment.

--- com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/config/operations/binary/ToJson.java
+++ com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/config/operations/binary/ToJson.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2024 by Top Logic, Inc. All rights reserved.
+ * SPDX-FileCopyrightText: 2025 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
  */
 package com.top_logic.model.search.expr.config.operations.binary;
 

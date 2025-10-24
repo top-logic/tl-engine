4bfa029d5f2ac16ed3e5a43beffd4ebcce97cc25
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-10-24 10:40:41 +0200

    Ticket #28949: Added row class provider parameterized by TL-Script.

--- com.top_logic.model.search/src/main/java/com/top_logic/model/search/providers/RowClassProviderByExpression.java
+++ com.top_logic.model.search/src/main/java/com/top_logic/model/search/providers/RowClassProviderByExpression.java
@@ -1,16 +1,11 @@
 /*
- * SPDX-FileCopyrightText: 2024 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * SPDX-FileCopyrightText: 2025 (c) Business Operation Systems GmbH <info@top-logic.com>
  * 
  * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
  */
 package com.top_logic.model.search.providers;
 
-import java.util.Collection;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
 import com.top_logic.basic.CalledByReflection;
-import com.top_logic.basic.StringServices;
 import com.top_logic.basic.annotation.InApp;
 import com.top_logic.basic.config.AbstractConfiguredInstance;
 import com.top_logic.basic.config.InstantiationContext;
@@ -18,30 +13,30 @@ import com.top_logic.basic.config.PolymorphicConfiguration;
 import com.top_logic.basic.config.SimpleInstantiationContext;
 import com.top_logic.basic.config.annotation.Label;
 import com.top_logic.basic.config.annotation.Mandatory;
-import com.top_logic.basic.config.annotation.TagName;
-import com.top_logic.layout.table.CellClassProvider;
-import com.top_logic.layout.table.TableRenderer.Cell;
+import com.top_logic.layout.table.RowClassProvider;
+import com.top_logic.layout.table.control.TableControl;
+import com.top_logic.layout.table.model.TableConfig;
+import com.top_logic.layout.table.model.TableConfiguration;
+import com.top_logic.layout.table.model.TableConfigurationProvider;
 import com.top_logic.layout.tree.model.TLTreeNode;
 import com.top_logic.mig.html.layout.LayoutComponent;
-import com.top_logic.model.TLObject;
-import com.top_logic.model.TLStructuredTypePart;
-import com.top_logic.model.annotate.ui.CssClassProvider;
 import com.top_logic.model.search.expr.config.dom.Expr;
 import com.top_logic.model.search.expr.query.QueryExecutor;
 
 /**
- * Computation of dynamic CSS classes for table cells in a TL-Script function.
+ * {@link RowClassProvider} that can be parameterized by TL-Script.
+ * 
+ * @implNote This implementation can directly be attached to a {@link TableConfig} by also
+ *           implementing {@link TableConfigurationProvider}.
  */
 @InApp
-@Label("CSS class provider in TL-Script")
-public class CellClassProviderByExpression extends AbstractConfiguredInstance<CellClassProviderByExpression.Config<?>>
-		implements CellClassProvider, CssClassProvider {
+public class RowClassProviderByExpression extends AbstractConfiguredInstance<RowClassProviderByExpression.Config<?>>
+		implements RowClassProvider, TableConfigurationProvider {
 
 	/**
-	 * Configuration options for {@link CellClassProviderByExpression}.
+	 * Configuration options for {@link RowClassProviderByExpression}.
 	 */
-	@TagName("cell-class-by-expression")
-	public interface Config<I extends CellClassProviderByExpression> extends PolymorphicConfiguration<I> {
+	public interface Config<I extends RowClassProviderByExpression> extends PolymorphicConfiguration<I> {
 		/**
 		 * Function computing the CSS class for a certain cell.
 		 * 
@@ -50,11 +45,6 @@ public class CellClassProviderByExpression extends AbstractConfiguredInstance<Ce
 		 * </p>
 		 * 
 		 * <dl>
-		 * <dt><code>value</code></dt>
-		 * <dd>The value of the cell that should receive a CSS class. If the column represents an
-		 * attribute of an object, then the value is the value of this attribute for the row
-		 * object.</dd>
-		 * 
 		 * <dt><code>row</code></dt>
 		 * <dd>The row object of the current table row. If the column represents an attribute of an
 		 * object, then the row is the object from which attributes are displayed in the current
@@ -66,17 +56,17 @@ public class CellClassProviderByExpression extends AbstractConfiguredInstance<Ce
 		 * 
 		 * <p>
 		 * The function is expected to return a string representing the CSS class to be set to the
-		 * current cell. Multiple CSS classes may be separated with white space. If the function
-		 * returns <code>null</code>, no CSS classes are added to the current cell.
+		 * current row. Multiple CSS classes may be separated with white space. If the function
+		 * returns <code>null</code>, no CSS classes are added to the current row.
 		 * </p>
 		 * 
 		 * <p>
-		 * To add the CSS class "<code>tl-warning</code>" to cells with values greater than
-		 * <code>99</code>, you may use the following expression:
+		 * To add the CSS class "<code>tl-warning</code>" to rows that display objects with some
+		 * propety greater than <code>99</code>, you may use the following expression:
 		 * </p>
 		 * 
 		 * <pre>
-		 * <code>value -> $value &gt; 99 ? "tl-warning" : null</code>
+		 * <code>row -> model -> $row.get(`my.moduel:MyClass#myProp`) &gt; 99 ? "tl-warning" : null</code>
 		 * </pre>
 		 * 
 		 * <p>
@@ -109,7 +99,7 @@ public class CellClassProviderByExpression extends AbstractConfiguredInstance<Ce
 	private QueryExecutor _cssClasses;
 
 	/**
-	 * Creates a {@link CellClassProviderByExpression} from configuration.
+	 * Creates a {@link RowClassProviderByExpression} from configuration.
 	 * 
 	 * @param context
 	 *        The context for instantiating sub configurations.
@@ -117,8 +107,9 @@ public class CellClassProviderByExpression extends AbstractConfiguredInstance<Ce
 	 *        The configuration.
 	 */
 	@CalledByReflection
-	public CellClassProviderByExpression(InstantiationContext context, Config<?> config) {
+	public RowClassProviderByExpression(InstantiationContext context, Config<?> config) {
 		super(context, config);
+
 		if (!(context instanceof SimpleInstantiationContext)) {
 			context.resolveReference(InstantiationContext.OUTER, LayoutComponent.class, c -> _component = c);
 		}
@@ -126,38 +117,22 @@ public class CellClassProviderByExpression extends AbstractConfiguredInstance<Ce
 	}
 
 	@Override
-	public String getCellClass(Cell cell) {
-		// See also RowClassProviderByExpression
-		Object value = cell.getValue();
-		Object row = cell.getRowObject();
-		if (row instanceof TLTreeNode<?> node) {
+	public String getTRClass(TableControl view, int rowOptions, int displayedRow, int row) {
+		// See also CellClassProviderByExpression
+		Object rowObj = view.getViewModel().getRowObject(row);
+		if (rowObj instanceof TLTreeNode<?> node) {
 			// This establishes consistency with tree grids, where the row object is just the
 			// business object of the node.
-			row = node.getBusinessObject();
+			rowObj = node.getBusinessObject();
 		}
 		Object componentModel = _component == null ? null : _component.getModel();
-		Object result = _cssClasses.execute(value, row, componentModel);
-		return toCssClass(result);
+		Object result = _cssClasses.execute(rowObj, componentModel);
+		return CellClassProviderByExpression.toCssClass(result);
 	}
 
 	@Override
-	public String getCssClass(TLObject model, TLStructuredTypePart attribute, Object value) {
-		Object componentModel = _component == null ? null : _component.getModel();
-		Object result = _cssClasses.execute(value, model, componentModel);
-		return toCssClass(result);
-	}
-
-	static String toCssClass(Object result) {
-		if (StringServices.isEmpty(result)) {
-			return null;
-		}
-		if (result instanceof Collection<?> collection) {
-			return collection.stream()
-				.map(CellClassProviderByExpression::toCssClass)
-				.filter(Objects::nonNull)
-				.collect(Collectors.joining(" "));
-		}
-		return result.toString();
+	public void adaptConfigurationTo(TableConfiguration table) {
+		table.setRowClassProvider(this);
 	}
 
 }

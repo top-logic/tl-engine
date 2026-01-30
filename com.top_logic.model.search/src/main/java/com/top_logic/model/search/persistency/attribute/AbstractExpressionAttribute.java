39fd1762d94514c550376c1147c3d277653cd958
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2026-01-30 09:28:50 +0100

    Ticket #29069: Cleanup: Better error message, if initializing a derived attribute fails.

--- com.top_logic.model.search/src/main/java/com/top_logic/model/search/persistency/attribute/AbstractExpressionAttribute.java
+++ com.top_logic.model.search/src/main/java/com/top_logic/model/search/persistency/attribute/AbstractExpressionAttribute.java
@@ -13,11 +13,14 @@ import com.top_logic.basic.config.annotation.Abstract;
 import com.top_logic.basic.config.annotation.DefaultContainer;
 import com.top_logic.basic.config.annotation.Label;
 import com.top_logic.basic.config.annotation.Name;
+import com.top_logic.basic.exception.I18NFailure;
 import com.top_logic.element.meta.kbbased.storage.AbstractDerivedStorage;
 import com.top_logic.model.TLStructuredTypePart;
 import com.top_logic.model.search.expr.SearchExpression;
 import com.top_logic.model.search.expr.config.dom.Expr;
 import com.top_logic.model.search.providers.AttributeByExpression;
+import com.top_logic.model.util.TLModelUtil;
+import com.top_logic.util.error.TopLogicException;
 
 /**
  * {@link AbstractDerivedStorage} for an attribute that is based on a configured search expression.
@@ -66,10 +69,16 @@ public abstract class AbstractExpressionAttribute<C extends AbstractExpressionAt
 	public void init(TLStructuredTypePart attribute) {
 		super.init(attribute);
 
-		// Note: The expression must be compiled in the context of the attribute it computes.
-		// Otherwise, access to model literals that is resolved by the compiler use the wrong
-		// versions (e.g. classifier instances).
-		_expr = compileExpr(attribute.getModel(), getConfig().getExpr());
+		try {
+			// Note: The expression must be compiled in the context of the attribute it computes.
+			// Otherwise, access to model literals that is resolved by the compiler use the wrong
+			// versions (e.g. classifier instances).
+			_expr = compileExpr(attribute.getModel(), getConfig().getExpr());
+		} catch (Exception ex) {
+			throw new TopLogicException(
+				I18NConstants.ERROR_INITIALIZING_ATTR__ATTR_MSG.fill(TLModelUtil.qualifiedName(attribute),
+					ex instanceof I18NFailure i18n ? i18n.getErrorKey() : ex.getMessage()));
+		}
 	}
 
 	/**

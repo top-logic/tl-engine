42f6fa5601baa45cd3bcef002e34751b9e2a89f0
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2026-02-06 12:43:40 +0100

    Ticket #29084: Annotated OpenAPI document interfaces with @Final.
    All concrete OpenAPI document types are now marked as final, which prevents
    polymorphic serialization ($type annotations) in JSON output.

--- com.top_logic.service.openapi.common/src/main/java/com/top_logic/service/openapi/common/document/TagObject.java
+++ com.top_logic.service.openapi.common/src/main/java/com/top_logic/service/openapi/common/document/TagObject.java
@@ -6,15 +6,17 @@
 package com.top_logic.service.openapi.common.document;
 
 import com.top_logic.basic.config.NamedConfigMandatory;
+import com.top_logic.basic.config.annotation.Final;
 import com.top_logic.basic.config.annotation.Label;
 import com.top_logic.basic.config.order.DisplayOrder;
 
 /**
  * Adds metadata to a single tag that is used by the Operation Object. It is not mandatory to have a
  * {@link TagObject} per tag defined in the {@link OperationObject} instances.
- * 
+ *
  * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
  */
+@Final
 @DisplayOrder({
 	TagObject.NAME_ATTRIBUTE,
 	TagObject.DESCRIPTION,

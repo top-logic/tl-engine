64aaf7396c4edd6a0cd0ee560f746ba0fb69a4d8
Daniel Busche <daniel.busche@top-logic.com>
2026-02-23 10:25:15 +0100

    Ticket #29092: Added common HttpServlet super class for TopLogic servlets.

---
+++ com.top_logic/src/main/java/com/top_logic/util/AbstractTopLogicServlet.java
@@ -0,0 +1,22 @@
+/*
+ * SPDX-FileCopyrightText: 2026 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+
+package com.top_logic.util;
+
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ * Abstract {@link HttpServlet} as marker for all servlet implementation used by
+ * <code>TopLogic</code>.
+ * 
+ * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
+ */
+public class AbstractTopLogicServlet extends HttpServlet {
+
+	// marker class.
+
+}
+

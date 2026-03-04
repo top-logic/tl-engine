244e3ce4e884c6e7d7edfc916f30e72812f00881
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2026-03-04 10:55:51 +0100

    Ticket #29102: Add tree table demo to DemoReactTableComponent.
    Create a DefaultTreeTableModel with folders, files, and versions hierarchy
    to demonstrate tree indentation and expand/collapse in the React table.
    
    User prompt: go ahead with phase B

--- com.top_logic.demo/src/main/java/com/top_logic/demo/react/DemoReactTableComponent.java
+++ com.top_logic.demo/src/main/java/com/top_logic/demo/react/DemoReactTableComponent.java
@@ -32,6 +32,9 @@ import com.top_logic.layout.react.control.table.ReactTextCellControl;
 import com.top_logic.layout.table.model.ColumnConfiguration;
 import com.top_logic.layout.table.model.ObjectTableModel;
 import com.top_logic.layout.table.model.TableConfiguration;
+import com.top_logic.layout.tree.model.DefaultTreeTableModel;
+import com.top_logic.layout.tree.model.DefaultTreeTableModel.DefaultTreeTableBuilder;
+import com.top_logic.layout.tree.model.DefaultTreeTableModel.DefaultTreeTableNode;
 import com.top_logic.mig.html.HTMLConstants;
 import com.top_logic.mig.html.layout.LayoutComponent;
 
@@ -53,6 +56,8 @@ public class DemoReactTableComponent extends LayoutComponent {
 	}
 
 	private ReactTableControl _tableControl;
+
+	private ReactTableControl _treeTableControl;
 
 	/**
 	 * Creates a new {@link DemoReactTableComponent}.
@@ -83,6 +88,24 @@ public class DemoReactTableComponent extends LayoutComponent {
 		out.writeAttribute(HTMLConstants.STYLE_ATTR, "height: 500px; border: 1px solid #ccc;");
 		out.endBeginTag();
 		_tableControl.write(displayContext, out);
+		out.endTag(HTMLConstants.DIV);
+
+		if (_treeTableControl == null) {
+			_treeTableControl = createDemoTreeTable();
+		}
+
+		out.beginTag(HTMLConstants.H2);
+		out.writeText("React Tree Table Demo");
+		out.endTag(HTMLConstants.H2);
+
+		out.beginTag(HTMLConstants.PARAGRAPH);
+		out.writeText("A tree table with expandable nodes. Click chevrons to expand/collapse.");
+		out.endTag(HTMLConstants.PARAGRAPH);
+
+		out.beginBeginTag(HTMLConstants.DIV);
+		out.writeAttribute(HTMLConstants.STYLE_ATTR, "height: 500px; border: 1px solid #ccc;");
+		out.endBeginTag();
+		_treeTableControl.write(displayContext, out);
 		out.endTag(HTMLConstants.DIV);
 	}
 
@@ -123,6 +146,64 @@ public class DemoReactTableComponent extends LayoutComponent {
 		ReactTableControl table = new ReactTableControl(model, cellProvider);
 		table.setSelectionMode("multi");
 		table.setFrozenColumnCount(2);
+		return table;
+	}
+
+	private ReactTableControl createDemoTreeTable() {
+		List<String> columnNames = Arrays.asList("name", "type", "size");
+
+		TableConfiguration tableConfig = TableConfiguration.table();
+		tableConfig.getDefaultColumn().setAccessor(MapAccessor.INSTANCE);
+		tableConfig.getDefaultColumn().setComparator(ComparableComparator.INSTANCE);
+
+		declareColumn(tableConfig, "name", "Name", "300px");
+		declareColumn(tableConfig, "type", "Type", "150px");
+		declareColumn(tableConfig, "size", "Size", "100px");
+
+		Map<String, Object> rootData = new LinkedHashMap<>();
+		rootData.put("name", "Root");
+		rootData.put("type", "Root");
+		rootData.put("size", "");
+
+		DefaultTreeTableModel model = new DefaultTreeTableModel(
+			new DefaultTreeTableBuilder(), rootData, columnNames, tableConfig);
+
+		DefaultTreeTableNode root = model.getRoot();
+		String[] folders = { "Documents", "Pictures", "Source Code", "Music" };
+
+		for (int f = 0; f < folders.length; f++) {
+			Map<String, Object> folderData = new LinkedHashMap<>();
+			folderData.put("name", folders[f]);
+			folderData.put("type", "Folder");
+			folderData.put("size", "");
+			DefaultTreeTableNode folder = root.createChild(folderData);
+
+			for (int i = 0; i < 5; i++) {
+				Map<String, Object> fileData = new LinkedHashMap<>();
+				fileData.put("name", "File " + (i + 1) + " in " + folders[f]);
+				fileData.put("type", "File");
+				fileData.put("size", String.valueOf((i + 1) * 128) + " KB");
+				DefaultTreeTableNode file = folder.createChild(fileData);
+
+				if (i == 0) {
+					for (int j = 0; j < 3; j++) {
+						Map<String, Object> subData = new LinkedHashMap<>();
+						subData.put("name", "Version " + (j + 1));
+						subData.put("type", "Version");
+						subData.put("size", String.valueOf((j + 1) * 64) + " KB");
+						file.createChild(subData);
+					}
+				}
+			}
+		}
+
+		LabelProvider labels = MetaLabelProvider.INSTANCE;
+		ReactCellControlProvider cellProvider = (rowObject, columnName, cellValue) -> {
+			return new ReactTextCellControl(labels.getLabel(cellValue));
+		};
+
+		ReactTableControl table = new ReactTableControl(model, cellProvider);
+		table.setSelectionMode("multi");
 		return table;
 	}
 

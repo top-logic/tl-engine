12feb0a43dd541b94bc0efa7e68ea5e03392f151
Daniel Busche <daniel.busche@top-logic.com>
2026-03-09 18:01:31 +0100

    Ticket #29092: Enable activation of multi-factor authentication.

---
+++ com.top_logic/src/main/java/com/top_logic/layout/component/configuration/CheckOTPDialog.java
@@ -0,0 +1,120 @@
+/*
+ * SPDX-FileCopyrightText: 2026 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+
+package com.top_logic.layout.component.configuration;
+
+import static com.top_logic.layout.form.template.model.Templates.*;
+
+import java.util.List;
+
+import com.top_logic.base.security.util.Password;
+import com.top_logic.basic.util.ResKey;
+import com.top_logic.event.infoservice.InfoService;
+import com.top_logic.html.template.TagTemplate;
+import com.top_logic.layout.DisplayContext;
+import com.top_logic.layout.DisplayDimension;
+import com.top_logic.layout.DisplayUnit;
+import com.top_logic.layout.basic.Command;
+import com.top_logic.layout.basic.CommandModel;
+import com.top_logic.layout.form.model.FormContext;
+import com.top_logic.layout.form.model.FormFactory;
+import com.top_logic.layout.form.model.StringField;
+import com.top_logic.layout.messagebox.AbstractTemplateDialog;
+import com.top_logic.layout.messagebox.MessageBox;
+import com.top_logic.layout.messagebox.MessageBox.ButtonType;
+import com.top_logic.layout.structure.DialogModel;
+import com.top_logic.tool.boundsec.HandlerResult;
+
+import dev.samstevens.totp.code.CodeGenerator;
+import dev.samstevens.totp.code.CodeVerifier;
+import dev.samstevens.totp.code.DefaultCodeGenerator;
+import dev.samstevens.totp.code.DefaultCodeVerifier;
+import dev.samstevens.totp.time.SystemTimeProvider;
+import dev.samstevens.totp.time.TimeProvider;
+
+/**
+ * A dialog to check a one time password.
+ * 
+ * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
+ */
+public class CheckOTPDialog extends AbstractTemplateDialog {
+
+	private static final String OTP_FIELD = "otp";
+
+	private Command _continuation;
+
+	private Password _secret;
+
+	/**
+	 * Creates a new {@link CheckOTPDialog}.
+	 */
+	public CheckOTPDialog(Command continuation, Password secret) {
+		this(continuation, secret, I18NConstants.CHECK_OTP_DIALOG_TITLE,
+			DisplayDimension.dim(500, DisplayUnit.PIXEL),
+			DisplayDimension.dim(300, DisplayUnit.PIXEL));
+	}
+
+	/**
+	 * Creates a new {@link CheckOTPDialog}.
+	 */
+	public CheckOTPDialog(Command continuation, Password secret, ResKey dialogTitle, DisplayDimension width,
+			DisplayDimension height) {
+		super(dialogTitle, width, height);
+		_continuation = continuation;
+		_secret = secret;
+	}
+
+	/**
+	 * Creates a new {@link CheckOTPDialog}.
+	 */
+	public CheckOTPDialog(Command continuation, Password secret, DialogModel dialogModel) {
+		super(dialogModel);
+		_continuation = continuation;
+		_secret = secret;
+	}
+
+	@Override
+	protected TagTemplate getTemplate() {
+		return div(resource(I18NConstants.CHECK_OTP_FIELD_MESSAGE), fieldBox(OTP_FIELD));
+	}
+
+	@Override
+	protected void fillFormContext(FormContext context) {
+		StringField otpField =
+			FormFactory.newStringField(OTP_FIELD, null, FormFactory.MANDATORY, !FormFactory.IMMUTABLE,
+			FormFactory.NO_CONSTRAINT);
+		otpField.setLabel(I18NConstants.CHECK_OTP_FIELD_LABEL);
+		context.addMember(otpField);
+	}
+
+	@Override
+	public Command getApplyClosure() {
+		return checkContextCommand()
+			.andThen(this::checkOTP);
+	}
+
+	private HandlerResult checkOTP(DisplayContext context) {
+		StringField field = (StringField) getFormContext().getField(OTP_FIELD);
+		String code = field.getAsString();
+		TimeProvider timeProvider = new SystemTimeProvider();
+		CodeGenerator codeGenerator = new DefaultCodeGenerator();
+		CodeVerifier verifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
+
+		boolean successful = verifier.isValidCode(_secret.decrypt(), code);
+		if (successful) {
+			return getDiscardClosure().andThen(_continuation).executeCommand(context);
+		}
+		InfoService.showError(I18NConstants.INVALID_OTP);
+		return HandlerResult.DEFAULT_RESULT;
+	}
+
+	@Override
+	protected void fillButtons(List<CommandModel> buttons) {
+		buttons.add(MessageBox.button(ButtonType.OK, getApplyClosure()));
+	}
+
+}
+
---
+++ com.top_logic/src/main/java/com/top_logic/layout/component/configuration/EnableMultiFactorAuthenticationDialog.java
@@ -0,0 +1,160 @@
+/*
+ * SPDX-FileCopyrightText: 2026 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+
+package com.top_logic.layout.component.configuration;
+
+import static com.top_logic.layout.form.template.model.Templates.*;
+
+import java.util.List;
+
+import com.top_logic.base.security.util.Password;
+import com.top_logic.basic.io.binary.BinaryData;
+import com.top_logic.basic.io.binary.BinaryDataFactory;
+import com.top_logic.basic.io.binary.BinaryDataSource;
+import com.top_logic.basic.util.ResKey;
+import com.top_logic.basic.version.Version;
+import com.top_logic.html.template.TagTemplate;
+import com.top_logic.knowledge.service.Transaction;
+import com.top_logic.knowledge.wrap.person.Person;
+import com.top_logic.layout.Control;
+import com.top_logic.layout.DisplayContext;
+import com.top_logic.layout.DisplayDimension;
+import com.top_logic.layout.DisplayUnit;
+import com.top_logic.layout.basic.Command;
+import com.top_logic.layout.basic.CommandModel;
+import com.top_logic.layout.form.FormField;
+import com.top_logic.layout.form.control.DisplayImageControl;
+import com.top_logic.layout.form.model.DataField;
+import com.top_logic.layout.form.model.FormContext;
+import com.top_logic.layout.form.model.FormFactory;
+import com.top_logic.layout.form.template.ControlProvider;
+import com.top_logic.layout.messagebox.AbstractTemplateDialog;
+import com.top_logic.layout.messagebox.MessageBox;
+import com.top_logic.layout.messagebox.MessageBox.ButtonType;
+import com.top_logic.layout.structure.DialogModel;
+import com.top_logic.tool.boundsec.HandlerResult;
+import com.top_logic.util.error.TopLogicException;
+
+import dev.samstevens.totp.code.HashingAlgorithm;
+import dev.samstevens.totp.exceptions.QrGenerationException;
+import dev.samstevens.totp.qr.QrData;
+import dev.samstevens.totp.qr.QrGenerator;
+import dev.samstevens.totp.qr.ZxingPngQrGenerator;
+import dev.samstevens.totp.secret.DefaultSecretGenerator;
+import dev.samstevens.totp.secret.SecretGenerator;
+
+/**
+ * Dialog to enable multi-factor authentication.
+ * 
+ * <p>
+ * This dialogs displays a QR code to initialize an authenticator app.
+ * </p>
+ * 
+ * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
+ */
+public class EnableMultiFactorAuthenticationDialog extends AbstractTemplateDialog {
+
+	private static final String QR_CODE_FIELD = "qrCode";
+
+	private final Person _account;
+
+	private final Password _mfaSecret;
+
+	/**
+	 * Creates a new {@link EnableMultiFactorAuthenticationDialog}.
+	 */
+	public EnableMultiFactorAuthenticationDialog(Person account) {
+		this(account, I18NConstants.ENABLE_MFA_AUTHENTICATION, DisplayDimension.dim(500, DisplayUnit.PIXEL),
+			DisplayDimension.dim(500, DisplayUnit.PIXEL));
+	}
+
+	/**
+	 * Creates a new {@link EnableMultiFactorAuthenticationDialog}.
+	 */
+	public EnableMultiFactorAuthenticationDialog(Person account, ResKey dialogTitle, DisplayDimension width,
+			DisplayDimension height) {
+		super(dialogTitle, width, height);
+		_account = account;
+		SecretGenerator secretGenerator = new DefaultSecretGenerator();
+		_mfaSecret = Password.fromPlainText(secretGenerator.generate());
+	}
+
+	/**
+	 * Creates a new {@link EnableMultiFactorAuthenticationDialog}.
+	 */
+	public EnableMultiFactorAuthenticationDialog(Person account, DialogModel dialogModel) {
+		super(dialogModel);
+		_account = account;
+		SecretGenerator secretGenerator = new DefaultSecretGenerator();
+		_mfaSecret = Password.fromPlainText(secretGenerator.generate());
+	}
+
+	@Override
+	protected TagTemplate getTemplate() {
+		return div(
+			div(resource(I18NConstants.INIT_AUTHENTICATOR_MESSAGE)),
+			div(style("text-align:center"), member(QR_CODE_FIELD)));
+	}
+
+	@Override
+	protected void fillFormContext(FormContext context) {
+		DataField qrCodeField = FormFactory.newDataField(QR_CODE_FIELD);
+		qrCodeField.initializeField(createQRCode());
+		qrCodeField.setImmutable(true);
+		qrCodeField.setControlProvider(new ControlProvider() {
+
+			@Override
+			public Control createControl(Object model, String style) {
+				return new DisplayImageControl((BinaryDataSource) ((FormField) model).getValue());
+			}
+		});
+		context.addMember(qrCodeField);
+	}
+
+	private BinaryData createQRCode() {
+		QrGenerator generator = new ZxingPngQrGenerator();
+		QrData qrData = new QrData.Builder()
+			.label(_account.getName())
+			.secret(_mfaSecret.decrypt())
+			.issuer(Version.getApplicationName())
+			.algorithm(HashingAlgorithm.SHA1)
+			.digits(6)
+			.period(30)
+			.build();
+		byte[] imageData;
+		try {
+			imageData = generator.generate(qrData);
+		} catch (QrGenerationException ex) {
+			throw new TopLogicException(I18NConstants.UNABLE_TO_CREATE_QR_CODE, ex);
+		}
+		return BinaryDataFactory.createBinaryData(imageData, generator.getImageMimeType());
+	}
+
+	@Override
+	public Command getApplyClosure() {
+		return checkContextCommand()
+			.andThen(this::updateMFA);
+	}
+
+	private HandlerResult updateMFA(DisplayContext context) {
+		Command updatePassword = ctx -> {
+			try (Transaction tx = _account.tKnowledgeBase().beginTransaction(I18NConstants.MFA_PASSWORD_UPDATED)) {
+				_account.tUpdateByName(Person.MFA_SECRET_ATTR, _mfaSecret);
+				tx.commit();
+			}
+			return HandlerResult.DEFAULT_RESULT;
+
+		};
+		return new CheckOTPDialog(updatePassword.andThen(getDiscardClosure()), _mfaSecret).open(context);
+	}
+
+	@Override
+	protected void fillButtons(List<CommandModel> buttons) {
+		buttons.add(MessageBox.button(ButtonType.OK, getApplyClosure()));
+	}
+
+}
+

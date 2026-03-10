40a7a77287e355389dcc2915b611b5b593ac7352
Daniel Busche <daniel.busche@top-logic.com>
2026-03-10 17:02:04 +0100

    Ticket #29092: Ensure that bookmarks are handled.

--- com.top_logic/src/main/java/com/top_logic/layout/component/configuration/OpenLoginDialogHook.java
+++ com.top_logic/src/main/java/com/top_logic/layout/component/configuration/OpenLoginDialogHook.java
@@ -6,11 +6,26 @@
 
 package com.top_logic.layout.component.configuration;
 
+import java.util.Collections;
+import java.util.Map;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import com.top_logic.base.context.TLSessionContext;
+import com.top_logic.basic.col.TypedAnnotatable;
+import com.top_logic.basic.col.TypedAnnotatable.Property;
 import com.top_logic.layout.DisplayContext;
+import com.top_logic.layout.basic.Command;
+import com.top_logic.layout.basic.DefaultDisplayContext;
+import com.top_logic.layout.internal.SubsessionHandler;
 import com.top_logic.layout.structure.DialogClosedListener;
 import com.top_logic.layout.structure.DialogModel;
 import com.top_logic.mig.html.layout.LoginHook;
 import com.top_logic.mig.html.layout.MainLayout;
+import com.top_logic.tool.boundsec.CommandHandler;
+import com.top_logic.tool.boundsec.CommandHandlerFactory;
+import com.top_logic.tool.boundsec.HandlerResult;
+import com.top_logic.tool.boundsec.commandhandlers.DefaultBookmarkHandler;
 import com.top_logic.util.TLContext;
 
 /**
@@ -20,15 +35,73 @@ import com.top_logic.util.TLContext;
  */
 public class OpenLoginDialogHook implements LoginHook {
 
+	private static final Property<Command> BOOKMARK = TypedAnnotatable.property(Command.class, "bookmark");
+
 	@Override
 	public void handleLogin(DisplayContext context, MainLayout mainLayout, Runnable callback) {
+		TLSessionContext sessionContext = context.getSessionContext();
+
+		// run attached bookmark handler
+		Command bookmarkHandler = sessionContext.reset(BOOKMARK);
+		if (bookmarkHandler != null) {
+			bookmarkHandler.executeCommand(context);
+		}
+
 		if (!TLContext.isAnonymous()) {
 			callback.run();
 			return;
 		}
+		SubsessionHandler subsessionHandler = findSubSessionHandler(context);
+		HttpServletRequest request = context.asRequest();
+		Map<String, ?> arguments = request.getParameterMap();
+
+		CommandHandler handler;
+		Map<String, Object> gotoArguments;
+		if (!arguments.isEmpty()) {
+			handler = CommandHandlerFactory.getInstance().getHandler(DefaultBookmarkHandler.COMMAND_RESOLVE_BOOKMARK);
+			if (handler != null) {
+				gotoArguments = subsessionHandler.resolveBookmarkArguments(request, arguments);
+			} else {
+				gotoArguments = Collections.emptyMap();
+			}
+		} else {
+			handler = null;
+			gotoArguments = Collections.emptyMap();
+		}
+		
+		Runnable closeCallback;
+		if (!gotoArguments.isEmpty()) {
+			context.set(SubsessionHandler.BOOKMARK_HANDLED, true);
+			Command displayBookmark = ctx -> {
+				findSubSessionHandler(ctx).processBookmark(ctx, handler, gotoArguments);
+				return HandlerResult.DEFAULT_RESULT;
+			};
+
+			closeCallback = () -> {
+				DisplayContext dc = DefaultDisplayContext.getDisplayContext();
+				TLSessionContext activeSessionContext = dc.getSessionContext();
+				if (activeSessionContext != sessionContext) {
+					/* A new Session was created! This happens when the user logged in. A new
+					 * MainLayout will be installed. Store a callback at the session to execute when
+					 * running this login hook with the new MainLayout. */
+					activeSessionContext.set(BOOKMARK, displayBookmark);
+				} else {
+					displayBookmark.executeCommand(dc);
+				}
+				callback.run();
+			};
+		} else {
+			closeCallback = callback;
+		}
+		
 		LoginViewDialog dialog = new LoginViewDialog();
-		dialog.getDialogModel().addListener(DialogModel.CLOSED_PROPERTY, runOnDialogClose(callback));
+		dialog.getDialogModel().addListener(DialogModel.CLOSED_PROPERTY, runOnDialogClose(closeCallback));
 		dialog.open(context);
+		
+	}
+
+	private SubsessionHandler findSubSessionHandler(DisplayContext ctx) {
+		return (SubsessionHandler) ctx.getLayoutContext();
 	}
 
 	private static DialogClosedListener runOnDialogClose(Runnable callback) {

16cad488578c45909479878f0b7b6a051e574506
Daniel Busche <daniel.busche@top-logic.com>
2026-03-10 17:02:04 +0100

    Ticket #29092: Ensure that bookmarks are handled.

--- com.top_logic/src/main/java/com/top_logic/mig/html/layout/BookmarkNotFoundNotification.java
+++ com.top_logic/src/main/java/com/top_logic/mig/html/layout/BookmarkNotFoundNotification.java
@@ -5,17 +5,12 @@
  */
 package com.top_logic.mig.html.layout;
 
-import java.util.Map;
-
-import com.top_logic.basic.config.InstantiationContext;
 import com.top_logic.layout.DisplayContext;
 import com.top_logic.layout.basic.Command;
 import com.top_logic.layout.basic.CommandModel;
 import com.top_logic.layout.messagebox.MessageBox;
 import com.top_logic.layout.messagebox.MessageBox.ButtonType;
 import com.top_logic.layout.messagebox.MessageBox.MessageType;
-import com.top_logic.tool.boundsec.AbstractCommandHandler;
-import com.top_logic.tool.boundsec.CommandHandler;
 import com.top_logic.tool.boundsec.HandlerResult;
 import com.top_logic.util.Resources;
 
@@ -27,7 +22,7 @@ import com.top_logic.util.Resources;
  * 
  * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
  */
-public final class BookmarkNotFoundNotification extends AbstractCommandHandler {
+public final class BookmarkNotFoundNotification implements Command {
 
 	/** arguments used to resolve the bookmark argument */
 	private Object _bookmarkArguments;
@@ -36,48 +31,49 @@ public final class BookmarkNotFoundNotification extends AbstractCommandHandler {
 	private Command _continutation;
 
 	/**
-	 * Creates a new {@link BookmarkNotFoundNotification}.
+	 * Sets the arguments that were used to find bookmark object.
+	 * 
+	 * @param bookmarkArguments
+	 *        Object to include into the error message. May be <code>null</code>.
 	 */
-	public BookmarkNotFoundNotification(InstantiationContext context, Config config) {
-		super(context, config);
-	}
-
 	public void setBookmarkArguments(Object bookmarkArguments) {
 		_bookmarkArguments = bookmarkArguments;
 	}
 
+	/**
+	 * Sets the command to execute when message was confirmed. May be <code>null</code>.
+	 */
 	public void setContinutation(Command continutation) {
 		_continutation = continutation;
 	}
 
 	@Override
-	public HandlerResult handleCommand(DisplayContext aContext, LayoutComponent aComponent,
-			Object model, Map<String, Object> someArguments) {
-		CommandModel button = MessageBox.button(ButtonType.OK, _continutation);
-		Resources resources = aContext.getResources();
+	public HandlerResult executeCommand(DisplayContext context) {
+		CommandModel button = MessageBox.button(ButtonType.OK, nonNull(_continutation));
+		Resources resources = context.getResources();
 		String confirmText;
 		if (_bookmarkArguments == null) {
 			confirmText = resources.getString(I18NConstants.BOOKMARK_NOT_FOUND);
 		} else {
 			confirmText = resources.getString(I18NConstants.BOOKMARK_NOT_FOUND__BOOKMARKARGS.fill(_bookmarkArguments));
 		}
-		return MessageBox.confirm(aContext, MessageType.INFO, confirmText, button);
+		return MessageBox.confirm(context, MessageType.INFO, confirmText, button);
+	}
+
+	private static Command nonNull(Command continutation) {
+		return continutation != null ? continutation : Command.DO_NOTHING;
 	}
 
 	/**
 	 * Creates a {@link BookmarkNotFoundNotification}
 	 * 
-	 * @param commandId
-	 *        The {@link CommandHandler#getID() id} of the command
 	 * @param bookmarkArguments
 	 *        The arguments used to resolve the object of the bookmark.
 	 * @param continuation
-	 *        A command to be executed when the user confirms the message. Must not be
-	 *        <code>null</code>.
+	 *        A command to be executed when the user confirms the message.
 	 */
-	public static BookmarkNotFoundNotification newInstance(String commandId, Object bookmarkArguments,
-			Command continuation) {
-		BookmarkNotFoundNotification result = newInstance(BookmarkNotFoundNotification.class, commandId);
+	public static BookmarkNotFoundNotification newInstance(Object bookmarkArguments, Command continuation) {
+		BookmarkNotFoundNotification result = new BookmarkNotFoundNotification();
 		result.setBookmarkArguments(bookmarkArguments);
 		result.setContinutation(continuation);
 		return result;

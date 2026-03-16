75097774c261066298d900380ee1a2f9cdfe2ce4
Daniel Busche <daniel.busche@top-logic.com>
2026-03-16 12:36:17 +0100

    Ticket #29092: Added callback to execute business logic after creation of new account.

--- tl-security-selfservice/src/main/java/com/top_logic/security/selfservice/model/TlSecuritySelfserviceFactory.java
+++ tl-security-selfservice/src/main/java/com/top_logic/security/selfservice/model/TlSecuritySelfserviceFactory.java
@@ -29,6 +29,20 @@ public class TlSecuritySelfserviceFactory extends com.top_logic.element.meta.kbb
 	}
 
 	/**
+	 * Lookup {@link Invitation#CONTEXT_OBJECTS_ATTR} of {@link Invitation}.
+	 */
+	public static com.top_logic.model.TLReference getContextObjectsInvitationAttr() {
+		return (com.top_logic.model.TLReference) getInvitationType().getPart(Invitation.CONTEXT_OBJECTS_ATTR);
+	}
+
+	/**
+	 * Lookup {@link Invitation#CREATE_CALLBACK_ATTR} of {@link Invitation}.
+	 */
+	public static com.top_logic.model.TLProperty getCreateCallbackInvitationAttr() {
+		return (com.top_logic.model.TLProperty) getInvitationType().getPart(Invitation.CREATE_CALLBACK_ATTR);
+	}
+
+	/**
 	 * Lookup {@link Invitation#EMAIL_ATTR} of {@link Invitation}.
 	 */
 	public static com.top_logic.model.TLProperty getEmailInvitationAttr() {

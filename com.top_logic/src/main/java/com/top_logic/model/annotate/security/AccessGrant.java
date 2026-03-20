b17769bbbfff5a389b97b639d0e5a952b1a38673
Daniel Busche <daniel.busche@top-logic.com>
2026-03-20 10:54:17 +0100

    Ticket #29088: Added access right configuration for model elements.

---
+++ com.top_logic/src/main/java/com/top_logic/model/annotate/security/AccessGrant.java
@@ -0,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: 2026 (c) Business Operation Systems GmbH <info@top-logic.com>
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
+ */
+
+package com.top_logic.model.annotate.security;
+
+import java.util.List;
+
+import com.top_logic.basic.config.ConfigurationItem;
+import com.top_logic.basic.config.annotation.Format;
+import com.top_logic.basic.config.annotation.Mandatory;
+import com.top_logic.basic.config.annotation.Name;
+import com.top_logic.model.annotate.TLAccessRights;
+import com.top_logic.tool.boundsec.CommandGroupReference;
+
+/**
+ * Configuration granting access to a specific operation to a set of roles.
+ *
+ * <p>
+ * An {@link AccessGrant} associates one operation (identified by a {@link CommandGroupReference})
+ * with the roles that are allowed to perform it on the annotated model element.
+ * </p>
+ *
+ * @see TLAccessRights
+ *
+ * @author    <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
+ */
+public interface AccessGrant extends ConfigurationItem {
+
+	/** Config property name for {@link #getOperation()}. */
+	String OPERATION = "operation";
+
+	/** Config property name for {@link #getRoles()}. */
+	String ROLES = "roles";
+
+	/**
+	 * The operation (command group) this grant applies to.
+	 */
+	@Mandatory
+	@Name(OPERATION)
+	CommandGroupReference getOperation();
+
+	/**
+	 * The roles that are granted access for the {@link #getOperation() operation}.
+	 */
+	@Format(CommaSeparatedRoles.class)
+	@Name(ROLES)
+	List<RoleConfig> getRoles();
+}
+

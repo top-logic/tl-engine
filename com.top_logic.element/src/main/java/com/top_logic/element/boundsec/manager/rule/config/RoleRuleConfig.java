5303144e09b19c018781a1cdf44cd14f27ee62ff
Daniel Busche <daniel.busche@top-logic.com>
2026-03-23 11:26:53 +0100

    Ticket #29088: Made security parents configurable.

--- com.top_logic.element/src/main/java/com/top_logic/element/boundsec/manager/rule/config/RoleRuleConfig.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/boundsec/manager/rule/config/RoleRuleConfig.java
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2017 (c) Business Operation Systems GmbH <info@top-logic.com>
- * 
+ *
  * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-BOS-TopLogic-1.0
  */
 package com.top_logic.element.boundsec.manager.rule.config;
@@ -8,15 +8,11 @@ package com.top_logic.element.boundsec.manager.rule.config;
 import java.util.List;
 
 import com.top_logic.basic.config.CommaSeparatedStrings;
-import com.top_logic.basic.config.ConfigurationItem;
-import com.top_logic.basic.config.PolymorphicConfiguration;
-import com.top_logic.basic.config.annotation.DefaultContainer;
 import com.top_logic.basic.config.annotation.Format;
 import com.top_logic.basic.config.annotation.Name;
 import com.top_logic.basic.config.annotation.Nullable;
 import com.top_logic.basic.config.order.DisplayOrder;
 import com.top_logic.basic.util.ResKey;
-import com.top_logic.element.boundsec.manager.rule.PathElement;
 import com.top_logic.element.boundsec.manager.rule.RoleProvider.Type;
 import com.top_logic.layout.form.template.SelectionControlProvider;
 import com.top_logic.layout.form.values.edit.annotation.ControlProvider;
@@ -31,9 +27,9 @@ import com.top_logic.tool.boundsec.wrap.RoleNameMapping;
 
 /**
  * Configuration of a role rule.
- * 
+ *
  * @see RoleRulesConfig#getRules()
- * 
+ *
  * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
  */
 @DisplayOrder({
@@ -46,10 +42,7 @@ import com.top_logic.tool.boundsec.wrap.RoleNameMapping;
 	RoleRuleConfig.XML_ATTRIBUTE_RESOURCE_KEY,
 	RoleRuleConfig.XML_TAG_PATH_ELEMENT,
 })
-public interface RoleRuleConfig extends ConfigurationItem {
-
-	/** Name of the value of {@link #getMetaElement()} in the configuration. */
-	String XML_ATTRIBUTE_META_ELEMENT = "meta-element";
+public interface RoleRuleConfig extends NavigationRuleConfig {
 
 	/** Name of the value of {@link #getSourceMetaElement()} in the configuration. */
 	String XML_ATTRIBUTE_SOURCE_META_ELEMENT = "source-meta-element";
@@ -60,38 +53,11 @@ public interface RoleRuleConfig extends ConfigurationItem {
 	/** Name of the value of {@link #getSourceRole()} in the configuration. */
 	String XML_ATTRIBUTE_SOURCE_ROLE = "source-role";
 
-	/**
-	 * Name of the value of {@link #getPathElements()} in the configuration.
-	 */
-	String XML_TAG_PATH_ELEMENT = "path";
-
 	/** Name of the value of {@link #getResKey()} in the configuration. */
 	String XML_ATTRIBUTE_RESOURCE_KEY = "resource-key";
 
 	/** Name of the value of {@link #getType()} in the configuration. */
 	String XML_ATTRIBUTE_TYPE = "type";
-
-	/** Name of the value of {@link #isInherit()} in the configuration. */
-	String XML_ATTRIBUTE_INHERIT = "inherit";
-
-	/**
-	 * The configuration of the steps to get from the source object to the target object to apply
-	 * role to.
-	 */
-	@Name(RoleRuleConfig.XML_TAG_PATH_ELEMENT)
-	@DefaultContainer
-	List<PolymorphicConfiguration<? extends PathElement>> getPathElements();
-
-	/**
-	 * Full qualified name of the {@link TLClass} to that an object must have to get the given
-	 * {@link #getRole()}.
-	 */
-	@Name(RoleRuleConfig.XML_ATTRIBUTE_META_ELEMENT)
-	@Nullable
-	@Options(fun = AllClasses.class, mapping = TLModelPartMapping.class)
-	@OptionLabels(TLPartScopedResourceProvider.class)
-	@ControlProvider(SelectionControlProvider.class)
-	String getMetaElement();
 
 	/**
 	 * Optional name of the {@link TLClass} which the source object must have to get the given
@@ -124,12 +90,6 @@ public interface RoleRuleConfig extends ConfigurationItem {
 	List<String> getSourceRole();
 
 	/**
-	 * Whether the Rule should also be applied to all sub types of {@link #getMetaElement()}.
-	 */
-	@Name(RoleRuleConfig.XML_ATTRIBUTE_INHERIT)
-	boolean isInherit();
-
-	/**
 	 * Which kind of rule is this?
 	 */
 	@Name(RoleRuleConfig.XML_ATTRIBUTE_TYPE)
@@ -141,4 +101,3 @@ public interface RoleRuleConfig extends ConfigurationItem {
 	@Name(RoleRuleConfig.XML_ATTRIBUTE_RESOURCE_KEY)
 	ResKey getResKey();
 }
-

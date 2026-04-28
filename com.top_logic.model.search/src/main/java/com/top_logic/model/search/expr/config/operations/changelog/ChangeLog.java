9c489bb789b4a6da0154041728ed598b59c25ed9
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2026-04-28 08:40:52 +0200

    Ticket #29236: Added missing changelog parameters.

--- com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/config/operations/changelog/ChangeLog.java
+++ com.top_logic.model.search/src/main/java/com/top_logic/model/search/expr/config/operations/changelog/ChangeLog.java
@@ -6,27 +6,36 @@
 package com.top_logic.model.search.expr.config.operations.changelog;
 
 import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.top_logic.basic.config.ConfigurationException;
 import com.top_logic.basic.config.InstantiationContext;
 import com.top_logic.element.changelog.ChangeLogBuilder;
 import com.top_logic.element.changelog.SubtreeFilter;
 import com.top_logic.element.changelog.model.TlChangelogFactory;
+import com.top_logic.knowledge.service.HistoryManager;
 import com.top_logic.knowledge.service.KnowledgeBase;
 import com.top_logic.knowledge.service.PersistencyLayer;
+import com.top_logic.knowledge.service.Revision;
+import com.top_logic.knowledge.wrap.person.Person;
+import com.top_logic.model.TLModule;
 import com.top_logic.model.TLObject;
 import com.top_logic.model.TLType;
 import com.top_logic.model.search.expr.EvalContext;
 import com.top_logic.model.search.expr.GenericMethod;
+import com.top_logic.model.search.expr.I18NConstants;
 import com.top_logic.model.search.expr.SearchExpression;
 import com.top_logic.model.search.expr.config.dom.Expr;
 import com.top_logic.model.search.expr.config.operations.AbstractSimpleMethodBuilder;
 import com.top_logic.model.search.expr.config.operations.ArgumentDescriptor;
+import com.top_logic.util.error.TopLogicException;
 import com.top_logic.util.model.ModelService;
 
 /**
- * TL-Script function {@code changeLog([obj [, maxEntries [, includeSubtree]]])}.
+ * TL-Script function
+ * {@code changeLog([obj [, maxEntries [, includeSubtree [, startRev [, stopRev [, author [, includeTechnical [, excludedModules]]]]]]]])}.
  *
  * <p>
  * Returns the list of {@code tl.changelog:ChangeSet}s that affect the given object. When
@@ -42,6 +51,29 @@ import com.top_logic.util.model.ModelService;
  * <p>
  * {@code maxEntries} limits the number of returned entries, newest first. A value {@code <= 0}
  * (the default) means unlimited.
+ * </p>
+ *
+ * <p>
+ * {@code startRev} and {@code stopRev} narrow the analyzed revision range. Each may be passed as
+ * either a {@link Revision} object or as a numeric commit number. By default the full history
+ * (revision {@code 1} up to the last revision) is analyzed.
+ * </p>
+ *
+ * <p>
+ * {@code author} restricts the result to changes committed by the given account. The argument may
+ * be either a {@code tl.accounts:Person} or a contact (or any {@link TLObject} carrying an
+ * {@code account} reference to a {@code tl.accounts:Person}). {@code null} (the default) produces
+ * a system-wide change log without any author filter.
+ * </p>
+ *
+ * <p>
+ * {@code includeTechnical} (default {@code false}) controls whether change sets that represent a
+ * purely technical change which does not reflect in the model are also reported.
+ * </p>
+ *
+ * <p>
+ * {@code excludedModules} is a collection of modules whose changes must not be regarded. Each
+ * element may be either a {@link TLModule} reference or a module name as a string.
  * </p>
  */
 public class ChangeLog extends GenericMethod {
@@ -67,10 +99,18 @@ public class ChangeLog extends GenericMethod {
 	protected Object eval(Object[] arguments, EvalContext definitions) {
 		TLObject obj = arguments[0] == null ? null : asTLObject(arguments[0]);
 
-		int maxEntries = arguments.length > 1 && arguments[1] != null ? asInt(arguments[1]) : 0;
-		boolean includeSubtree = arguments.length > 2 && arguments[2] != null ? asBoolean(arguments[2]) : true;
+		int maxEntries = arguments[1] != null ? asInt(arguments[1]) : 0;
+		boolean includeSubtree = arguments[2] != null ? asBoolean(arguments[2]) : true;
+		boolean includeTechnical = arguments[6] != null ? asBoolean(arguments[6]) : false;
 
 		KnowledgeBase kb = obj != null ? obj.tKnowledgeBase() : PersistencyLayer.getKnowledgeBase();
+		HistoryManager hm = kb.getHistoryManager();
+
+		Revision startRev = toRevision(hm, arguments[3]);
+		Revision stopRev = toRevision(hm, arguments[4]);
+		Person author = toAuthor(arguments[5]);
+		Set<String> excludedModules = toModuleNames(arguments[7]);
+
 		ChangeLogBuilder builder = new ChangeLogBuilder(kb, ModelService.getApplicationModel());
 		if (obj != null) {
 			builder.setFilter(new SubtreeFilter(obj, includeSubtree));
@@ -78,7 +118,74 @@ public class ChangeLog extends GenericMethod {
 		if (maxEntries > 0) {
 			builder.setNumberEntries(maxEntries);
 		}
-		return (Collection<?>) builder.build();
+		if (startRev != null) {
+			builder.setStartRev(startRev);
+		}
+		if (stopRev != null) {
+			builder.setStopRev(stopRev);
+		}
+		if (author != null) {
+			builder.setAuthor(author);
+		}
+		builder.setIncludeTechnical(includeTechnical);
+		if (!excludedModules.isEmpty()) {
+			builder.setExcludedModules(excludedModules);
+		}
+		return builder.build();
+	}
+
+	private Revision toRevision(HistoryManager hm, Object value) {
+		if (value == null) {
+			return null;
+		}
+		Object arg = asSingleElement(value);
+		if (arg == null) {
+			return null;
+		}
+		if (arg instanceof Revision) {
+			return (Revision) arg;
+		}
+		return hm.getRevision(asLong(arg));
+	}
+
+	private Person toAuthor(Object value) {
+		if (value == null) {
+			return null;
+		}
+		Object single = asSingleElement(value);
+		if (single == null) {
+			return null;
+		}
+		if (single instanceof Person person) {
+			return person;
+		}
+		throw new TopLogicException(
+			I18NConstants.ERROR_UNEXPECTED_TYPE__EXPECTED_VAL_EXPR.fill(Person.class, value, this));
+	}
+
+	private Set<String> toModuleNames(Object value) {
+		if (value == null) {
+			return Set.of();
+		}
+		Collection<?> coll = asCollection(value);
+		if (coll.isEmpty()) {
+			return Set.of();
+		}
+		Set<String> result = new LinkedHashSet<>();
+		for (Object element : coll) {
+			if (element == null) {
+				continue;
+			}
+			if (element instanceof TLModule module) {
+				result.add(module.getName());
+			} else if (element instanceof CharSequence) {
+				result.add(element.toString());
+			} else {
+				throw new TopLogicException(
+					I18NConstants.ERROR_UNEXPECTED_TYPE__EXPECTED_VAL_EXPR.fill(TLModule.class, element, this));
+			}
+		}
+		return result;
 	}
 
 	/**
@@ -90,6 +197,11 @@ public class ChangeLog extends GenericMethod {
 			.optional("object")
 			.optional("maxEntries")
 			.optional("includeSubtree")
+			.optional("startRev")
+			.optional("stopRev")
+			.optional("author")
+			.optional("includeTechnical")
+			.optional("excludedModules")
 			.build();
 
 		/**

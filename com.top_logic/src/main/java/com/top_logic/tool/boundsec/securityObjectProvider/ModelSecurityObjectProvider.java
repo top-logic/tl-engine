91c8f9f290ca1b6525a46257d381f54d09e13508
Daniel Busche <daniel.busche@top-logic.com>
2026-06-22 15:51:08 +0200

    Ticket #29221: Added configuration option to specify model type.
    The more precisely the model type is specified, the better the security
    roles can be configured.

--- com.top_logic/src/main/java/com/top_logic/tool/boundsec/securityObjectProvider/ModelSecurityObjectProvider.java
+++ com.top_logic/src/main/java/com/top_logic/tool/boundsec/securityObjectProvider/ModelSecurityObjectProvider.java
@@ -5,8 +5,19 @@
  */
 package com.top_logic.tool.boundsec.securityObjectProvider;
 
+import java.util.Collections;
+import java.util.Set;
+
+import com.top_logic.basic.ConfigurationError;
 import com.top_logic.basic.annotation.InApp;
+import com.top_logic.basic.config.AbstractConfiguredInstance;
+import com.top_logic.basic.config.ConfigurationException;
+import com.top_logic.basic.config.InstantiationContext;
+import com.top_logic.basic.config.PolymorphicConfiguration;
 import com.top_logic.basic.config.annotation.Label;
+import com.top_logic.basic.config.annotation.Name;
+import com.top_logic.model.TLClass;
+import com.top_logic.model.util.TLModelPartRef;
 import com.top_logic.tool.boundsec.BoundChecker;
 import com.top_logic.tool.boundsec.BoundCommandGroup;
 import com.top_logic.tool.boundsec.BoundObject;
@@ -19,16 +30,53 @@ import com.top_logic.tool.boundsec.SecurityObjectProvider;
  */
 @InApp
 @Label("Current model")
-public class ModelSecurityObjectProvider implements SecurityObjectProvider {
-
-	/** Singleton {@link ModelSecurityObjectProvider} instance. */
-	public static final ModelSecurityObjectProvider INSTANCE = new ModelSecurityObjectProvider();
+public class ModelSecurityObjectProvider extends AbstractConfiguredInstance<ModelSecurityObjectProvider.Config>
+		implements SecurityObjectProvider {
 
 	/**
-	 * Creates a new {@link ModelSecurityObjectProvider}.
+	 * Typed configuration interface definition for {@link ModelSecurityObjectProvider}.
 	 */
-	protected ModelSecurityObjectProvider() {
-		// singleton instance
+	public static interface Config extends PolymorphicConfiguration<ModelSecurityObjectProvider> {
+
+		/** Configuration name for {@link #getModelType()}. */
+		String MODEL_TYPE = "model-type";
+
+		/**
+		 * The type that the model may have.
+		 *
+		 * <p>
+		 * If the value is <code>null</code> then the model can have each type.
+		 * </p>
+		 */
+		@Name(MODEL_TYPE)
+		TLModelPartRef getModelType();
+
+		/**
+		 * Setter for {@link #getModelType()}.
+		 */
+		void setModelType(TLModelPartRef value);
+
+	}
+
+	/**
+	 * Create a {@link ModelSecurityObjectProvider}.
+	 * 
+	 * @param context
+	 *        the {@link InstantiationContext} to create the new object in
+	 * @param config
+	 *        the configuration object to be used for instantiation
+	 */
+	public ModelSecurityObjectProvider(InstantiationContext context, Config config) {
+		super(context, config);
+		TLModelPartRef modelType = getConfig().getModelType();
+		if (modelType != null) {
+			// Try resolving model type. fails if impossible.
+			try {
+				modelType.resolveClass();
+			} catch (ConfigurationException ex) {
+				context.error("Unable to resolve '" + modelType + "' to TLClass.", ex);
+			}
+		}
 	}
 
 	@Override
@@ -37,6 +85,19 @@ public class ModelSecurityObjectProvider implements SecurityObjectProvider {
 			return (BoundObject) model;
 		}
 		return null;
+	}
+
+	@Override
+	public Set<TLClass> getPossibleSecurityObjectTypes() {
+		TLModelPartRef modelType = getConfig().getModelType();
+		if (modelType == null) {
+			return SecurityObjectProvider.super.getPossibleSecurityObjectTypes();
+		}
+		try {
+			return Collections.singleton(modelType.resolveClass());
+		} catch (ConfigurationException ex) {
+			throw new ConfigurationError(ex);
+		}
 	}
 
 }

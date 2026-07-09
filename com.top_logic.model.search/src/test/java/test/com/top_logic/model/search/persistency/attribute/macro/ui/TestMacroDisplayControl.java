c25a8a5fbd2c3fe1cb874907b7c7986950d0b067
Daniel Busche <daniel.busche@top-logic.com>
2026-07-09 09:08:24 +0200

    Ticket #29371: Added test for macro with model access.

--- com.top_logic.model.search/src/test/java/test/com/top_logic/model/search/persistency/attribute/macro/ui/TestMacroDisplayControl.java
+++ com.top_logic.model.search/src/test/java/test/com/top_logic/model/search/persistency/attribute/macro/ui/TestMacroDisplayControl.java
@@ -5,15 +5,21 @@
  */
 package test.com.top_logic.model.search.persistency.attribute.macro.ui;
 
+import java.util.Collection;
+
 import junit.framework.Test;
 
 import test.com.top_logic.layout.TestControl;
 
 import com.top_logic.layout.form.model.FormFactory;
 import com.top_logic.layout.form.model.HiddenField;
+import com.top_logic.model.TLModel;
+import com.top_logic.model.TLModule;
+import com.top_logic.model.TLObject;
 import com.top_logic.model.search.expr.SearchExpression;
 import com.top_logic.model.search.persistency.attribute.macro.ui.MacroDisplayControl;
 import com.top_logic.model.search.persistency.attribute.tempate.TemplateStorageMapping;
+import com.top_logic.util.model.ModelService;
 
 /**
  * Test case for {@link MacroDisplayControl}.
@@ -30,6 +36,22 @@ import com.top_logic.model.search.persistency.attribute.tempate.TemplateStorageM
  */
 @SuppressWarnings("javadoc")
 public class TestMacroDisplayControl extends TestControl {
+
+	public void testMacroWithModelAccess() {
+		TLModel applicationModel = ModelService.getApplicationModel();
+		Collection<TLModule> modules = applicationModel.getModules();
+		assertTrue(!modules.isEmpty());
+
+		SearchExpression macro = createMacro("<b>name: {$model.get(`tl.model:TLModule#name`)}</b>");
+
+		for (TLModule module : modules) {
+			MacroDisplayControl control = createControl(module, macro);
+
+			String output = writeControl(control);
+			assertContains("<b>name: " + module.getName() + "</b>", output);
+		}
+
+	}
 
 	public void testMacroOutput() {
 		MacroDisplayControl control = createControl("<b>value: {1 + 2}</b>");
@@ -50,9 +72,16 @@ public class TestMacroDisplayControl extends TestControl {
 	}
 
 	private MacroDisplayControl createControl(String macroSource) {
-		SearchExpression macro = TemplateStorageMapping.INSTANCE.getBusinessObject(macroSource);
+		return createControl(null, createMacro(macroSource));
+	}
+
+	private SearchExpression createMacro(String macroSource) {
+		return TemplateStorageMapping.INSTANCE.getBusinessObject(macroSource);
+	}
+
+	private MacroDisplayControl createControl(TLObject self, SearchExpression macro) {
 		HiddenField field = FormFactory.newHiddenField("macro", macro);
-		return new MacroDisplayControl(null, field);
+		return new MacroDisplayControl(self, field);
 	}
 
 	public static Test suite() {

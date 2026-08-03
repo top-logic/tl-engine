db1aba7e05f790b9a1a146faa231fb28cadf62e5
Daniel Busche <daniel.busche@top-logic.com>
2026-08-03 13:05:44 +0200

    Ticket #29447: Security parents are navigation rules, not role rules.
    A security parent is determined by pure navigation: NavigationRulesImporter
    reads the type, the inheritance flag and the path, nothing else. Configuring
    the rules as RoleRuleConfig therefore offered a role, a source role, a source
    type, a rule type and a resource key that were silently ignored.
    
    Declare the rules of a SecurityParentsConfig as NavigationRuleConfig. The id
    moves to NavigationRuleConfig, since it is the key of both rule lists.

--- com.top_logic.element/src/main/java/com/top_logic/element/boundsec/manager/rule/config/RoleRuleConfig.java
+++ com.top_logic.element/src/main/java/com/top_logic/element/boundsec/manager/rule/config/RoleRuleConfig.java
@@ -9,10 +9,8 @@ import java.util.List;
 
 import com.top_logic.basic.config.CommaSeparatedStrings;
 import com.top_logic.basic.config.annotation.Format;
-import com.top_logic.basic.config.annotation.Mandatory;
 import com.top_logic.basic.config.annotation.Name;
 import com.top_logic.basic.config.annotation.Nullable;
-import com.top_logic.basic.config.annotation.ValueInitializer;
 import com.top_logic.basic.config.order.DisplayOrder;
 import com.top_logic.basic.util.ResKey;
 import com.top_logic.element.boundsec.manager.rule.RoleProvider.Type;
@@ -20,7 +18,6 @@ import com.top_logic.layout.form.template.SelectionControlProvider;
 import com.top_logic.layout.form.values.edit.annotation.ControlProvider;
 import com.top_logic.layout.form.values.edit.annotation.OptionLabels;
 import com.top_logic.layout.form.values.edit.annotation.Options;
-import com.top_logic.layout.form.values.edit.initializer.UUIDInitializer;
 import com.top_logic.model.TLClass;
 import com.top_logic.model.config.TLModelPartMapping;
 import com.top_logic.model.resources.TLPartScopedResourceProvider;
@@ -36,7 +33,7 @@ import com.top_logic.tool.boundsec.wrap.RoleNameMapping;
  * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
  */
 @DisplayOrder({
-	RoleRuleConfig.ID,
+	NavigationRuleConfig.ID,
 	RoleRuleConfig.XML_ATTRIBUTE_META_ELEMENT,
 	RoleRuleConfig.XML_ATTRIBUTE_ROLE,
 	RoleRuleConfig.XML_ATTRIBUTE_INHERIT,
@@ -47,9 +44,6 @@ import com.top_logic.tool.boundsec.wrap.RoleNameMapping;
 	RoleRuleConfig.XML_TAG_PATH_ELEMENT,
 })
 public interface RoleRuleConfig extends NavigationRuleConfig {
-
-	/** Name of the value of {@link #getId()} in the configuration. */
-	String ID = "id";
 
 	/** Name of the value of {@link #getSourceMetaElement()} in the configuration. */
 	String XML_ATTRIBUTE_SOURCE_META_ELEMENT = "source-meta-element";
@@ -65,22 +59,6 @@ public interface RoleRuleConfig extends NavigationRuleConfig {
 
 	/** Name of the value of {@link #getType()} in the configuration. */
 	String XML_ATTRIBUTE_TYPE = "type";
-
-	/**
-	 * Stable, unique identifier of this rule within its {@link RoleRulesConfig}.
-	 *
-	 * <p>
-	 * On creation in a form the value is prefilled with a generated id; in a
-	 * configuration file it must be specified explicitly.
-	 * </p>
-	 */
-	@Name(RoleRuleConfig.ID)
-	@Mandatory
-	@ValueInitializer(UUIDInitializer.class)
-	String getId();
-
-	/** @see #getId() */
-	void setId(String id);
 
 	/**
 	 * Optional name of the {@link TLClass} which the source object must have to get the given
